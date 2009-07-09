9bd62a9782088fa3289412468915dc4ceacbb5e8
abstr <jens.nyberg@gmail.com>
2009-07-09 02:44:17 +0200

    Added interpreter. Does not work very good but it is a start

--- src/shell.c
+++ src/shell.c
@@ -3,13 +3,47 @@
 #include <kbd.h>
 #include <shell.h>
 
+uint8_t shellBuffer[256];
+uint16_t shellBufferSize = 0;
+
+void shell_buffer_write(uint8_t c)
+{
+
+	if (shellBufferSize < 256)
+	{
+
+		shellBuffer[shellBufferSize] = c;
+		shellBufferSize++;
+
+	}
+
+}
+
+uint8_t *shell_buffer_read()
+{
+
+	shellBuffer[shellBufferSize] = '\0';
+
+	return shellBuffer;
+
+}
+
+void shell_buffer_clear()
+{
+
+	puts("fudge:/$ ");
+	shellBufferSize = 0;
+
+}
+
 void shell_init()
 {
 
 	screen_clear();
 
 	puts("Welcome to Fudge! Type help for more information.\n\n");
-	puts("fudge:/$ ");
+
+	shell_buffer_clear();
 
 	while(1)
 	{
@@ -19,13 +53,29 @@ void shell_init()
 
 			uint8_t c = kbd_buffer_read();
 
+			shell_buffer_write(c);
 			putc(c);
 
 			if (c == '\n')
 			{
 
-				// Read console buffer and execute
-				puts("fudge:/$ ");
+				uint8_t *command = shell_buffer_read();
+
+				if (strcmp(command, "help"))
+				{
+
+					puts("Help is not available!\n");
+
+				}
+
+				else
+				{
+
+					puts("Command not recognized");
+
+				}
+
+				shell_buffer_clear();
 
 			}
 
