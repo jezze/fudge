eb585833647b90bf14f26f2dfa7f2924cf6cf3c3
abstr <jens.nyberg@gmail.com>
2009-07-10 03:18:17 +0200

    Updated gdt tss

--- src/gdt.c
+++ src/gdt.c
@@ -8,7 +8,7 @@ extern void tss_flush();
 gdt_entry_t gdt[6];
 gdt_ptr_t gdtp;
 
-tss_entry_t tss_entry;
+tss_entry_t tss;
 
 void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
 {
@@ -16,46 +16,33 @@ void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, ui
 	gdt[num].base_low = (base & 0xFFFF);
 	gdt[num].base_middle = (base >> 16) & 0xFF;
 	gdt[num].base_high = (base >> 24) & 0xFF;
-
 	gdt[num].limit_low = (limit & 0xFFFF);
-	gdt[num].granularity = ((limit >> 16) & 0x0F);
-
+	gdt[num].granularity = (limit >> 16) & 0x0F;
 	gdt[num].granularity |= (gran & 0xF0);
 	gdt[num].access = access;	
-
-}
-
-void tss_write(int32_t num, uint16_t ss0, uint32_t esp0)
-{
-
-	uint32_t base = (uint32_t)&tss_entry;
-	uint32_t limit = base + sizeof (tss_entry);
-
-	gdt_set_gate(num, base, limit, 0xE9, 0x00);
-
-	memset(&tss_entry, 0, sizeof (tss_entry));
-
-	tss_entry.ss0 = ss0;
-	tss_entry.esp0 = esp0;
-	tss_entry.cs = 0x0b;
-	tss_entry.ss = tss_entry.ds = tss_entry.es = tss_entry.fs = tss_entry.gs = 0x13;
 
 }
 
 void set_kernel_stack(uint32_t stack)
 {
 
-	tss_entry.esp0 = stack;
+	tss.esp0 = stack;
 
 }
 
 void gdt_init()
 {
 
-	gdtp.limit = (sizeof (gdt_entry_t) * 6) - 1;
 	gdtp.base = (uint32_t)&gdt;
+	gdtp.limit = (sizeof (gdt_entry_t) * 6) - 1;
 
 	memset(&gdt, 0, sizeof (gdt_entry_t) * 6);
+	memset(&tss, 0, sizeof (tss_entry_t));
+
+	tss.ss0 = 0x10;
+	tss.esp0 = 0x0;
+	tss.cs = 0x0b;
+	tss.ss = tss.ds = tss.es = tss.fs = tss.gs = 0x13;
 
 	gdt_set_gate(0, 0, 0, 0, 0);			// Null segment
 	gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);	// Kernel code segment
@@ -63,7 +50,10 @@ void gdt_init()
 	gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF);	// User code segment
 	gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);	// User data segment
 
-	tss_write(5, 0x10, 0x0);
+	uint32_t base = (uint32_t)&tss;
+	uint32_t limit = base + sizeof (tss_entry_t);
+
+	gdt_set_gate(5, base, limit, 0xE9, 0x00);	// TSS
 
 	gdt_flush();
 	tss_flush();
