69a0518d20fe34b877a813ceaa5cefdd18eea5ee
jezze <jens.nyberg@gmail.com>
2010-05-31 01:39:44 +0000

    vfs cleanups

--- include/kernel/vfs.h
+++ include/kernel/vfs.h
@@ -1,13 +1,8 @@
 #ifndef VFS_H
 #define VFS_H
 
-#define VFS_FILE        0x01
-#define VFS_DIRECTORY   0x02
-#define VFS_CHARDEVICE  0x03
-#define VFS_BLOCKDEVICE 0x04
-#define VFS_PIPE        0x05
-#define VFS_SYMLINK     0x06
-#define VFS_MOUNTPOINT  0x08
+#define VFS_FILE      0x01
+#define VFS_DIRECTORY 0x02
 
 struct vfs_node;
 
