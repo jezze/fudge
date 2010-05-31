aee21524d12c2e3bff555ced76b12397d3e7ae86
jezze <jens.nyberg@gmail.com>
2010-05-31 20:43:34 +0000

    another initrd fix to remove heap allocation

--- src/kernel/initrd.c
+++ src/kernel/initrd.c
@@ -7,9 +7,8 @@
 
 initrd_header_t *initrd_header;
 initrd_file_header_t *file_headers;
-vfs_node_t *initrd_root;
-vfs_node_t *root_nodes;
-uint32_t nroot_nodes;
+vfs_node_t initrd_root;
+vfs_node_t root_nodes[32];
 
 static uint32_t initrd_read(vfs_node_t *node, uint32_t offset, uint32_t size, uint32_t *buffer)
 {
@@ -31,7 +30,7 @@ static uint32_t initrd_read(vfs_node_t *node, uint32_t offset, uint32_t size, ui
 static vfs_node_t *initrd_walk(vfs_node_t *node, uint32_t index)
 {
 
-    if (index < nroot_nodes)
+    if (index < initrd_header->nfiles)
         return &root_nodes[index];
     else
         return 0;
@@ -43,7 +42,7 @@ static vfs_node_t *initrd_find(vfs_node_t *node, char *name)
 
     uint32_t i;
 
-    for (i = 0; i < nroot_nodes; i++)
+    for (i = 0; i < initrd_header->nfiles; i++)
     {
 
         if (!string_compare(name, root_nodes[i].name))
@@ -61,20 +60,16 @@ vfs_node_t *initrd_init(uint32_t location)
     initrd_header = (initrd_header_t *)location;
     file_headers = (initrd_file_header_t *)(location + sizeof (initrd_header_t));
 
-    initrd_root = (vfs_node_t *)kmalloc(sizeof (vfs_node_t));
-    string_copy(initrd_root->name, "initrd");
-    initrd_root->inode = 0;
-    initrd_root->length = 0;
-    initrd_root->flags = VFS_DIRECTORY;
-    initrd_root->open = 0;
-    initrd_root->close = 0;
-    initrd_root->read = 0;
-    initrd_root->write = 0;
-    initrd_root->walk = &initrd_walk;
-    initrd_root->find = &initrd_find;
-
-    root_nodes = (vfs_node_t*)kmalloc(sizeof (vfs_node_t) * initrd_header->nfiles);
-    nroot_nodes = initrd_header->nfiles;
+    string_copy(initrd_root.name, "initrd");
+    initrd_root.inode = 0;
+    initrd_root.length = 0;
+    initrd_root.flags = VFS_DIRECTORY;
+    initrd_root.open = 0;
+    initrd_root.close = 0;
+    initrd_root.read = 0;
+    initrd_root.write = 0;
+    initrd_root.walk = &initrd_walk;
+    initrd_root.find = &initrd_find;
 
     uint32_t i;
 
@@ -95,7 +90,7 @@ vfs_node_t *initrd_init(uint32_t location)
 
     }
 
-    return initrd_root;
+    return &initrd_root;
 
 }
 
