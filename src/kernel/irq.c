dde93b7ce27c33bb739c73521c8b2dd6c1a83199
jezze <jens.nyberg@gmail.com>
2010-06-08 22:14:02 +0000

    last fixes on that area

--- src/kernel/irq.c
+++ src/kernel/irq.c
@@ -4,23 +4,6 @@
 #include <kernel/idt.h>
 #include <kernel/irq.h>
 
-extern void irq0();
-extern void irq1();
-extern void irq2();
-extern void irq3();
-extern void irq4();
-extern void irq5();
-extern void irq6();
-extern void irq7();
-extern void irq8();
-extern void irq9();
-extern void irq10();
-extern void irq11();
-extern void irq12();
-extern void irq13();
-extern void irq14();
-extern void irq15();
-
 void *irq_routines[IRQ_ROUTINES_SIZE];
 
 void irq_register_handler(uint8_t num, void (*handler)(registers_t *r))
--- src/kernel/isr.c
+++ src/kernel/isr.c
@@ -4,39 +4,6 @@
 #include <kernel/screen.h>
 #include <kernel/isr.h>
 
-extern void isr0();
-extern void isr1();
-extern void isr2();
-extern void isr3();
-extern void isr4();
-extern void isr5();
-extern void isr6();
-extern void isr7();
-extern void isr8();
-extern void isr9();
-extern void isr10();
-extern void isr11();
-extern void isr12();
-extern void isr13();
-extern void isr14();
-extern void isr15();
-extern void isr16();
-extern void isr17();
-extern void isr18();
-extern void isr19();
-extern void isr20();
-extern void isr21();
-extern void isr22();
-extern void isr23();
-extern void isr24();
-extern void isr25();
-extern void isr26();
-extern void isr27();
-extern void isr28();
-extern void isr29();
-extern void isr30();
-extern void isr31();
-
 void *isr_routines[ISR_ROUTINES_SIZE];
 
 char *exception_messages[] =
