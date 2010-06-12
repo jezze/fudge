9e0dfda466f1a1fac5f8d72a4b1e1a3ee9cb3255
jezze <jens.nyberg@gmail.com>
2010-06-13 00:43:10 +0200

    small cleanup

--- kernel/idt.c
+++ kernel/idt.c
@@ -10,7 +10,6 @@ void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags)
 
     idt[num].base_lo = (base & 0xFFFF);
     idt[num].base_hi = (base >> 16) & 0xFFFF;
-
     idt[num].sel = sel;
     idt[num].always0 = 0;
     idt[num].flags = flags;
