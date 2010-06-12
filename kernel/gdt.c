203bd71b97ac1ffaa5d00210aaebaa22d352083a
jezze <jens.nyberg@gmail.com>
2010-06-13 00:41:55 +0200

    small cleanup

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -4,7 +4,6 @@
 
 gdt_entry_t gdt[GDT_TABLE_SIZE];
 gdt_ptr_t gdtp;
-
 tss_entry_t tss;
 
 void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
@@ -33,6 +32,10 @@ void gdt_init()
     gdtp.base = (uint32_t)&gdt;
     gdtp.limit = (sizeof (gdt_entry_t) * GDT_TABLE_SIZE) - 1;
 
+    gdt_ptr_t tssp;
+    tssp.base = (uint32_t)&tss;
+    tssp.limit = tssp.base + sizeof (tss_entry_t);
+
     memory_set(&gdt, 0, sizeof (gdt_entry_t) * GDT_TABLE_SIZE);
     memory_set(&tss, 0, sizeof (tss_entry_t));
 
@@ -46,11 +49,7 @@ void gdt_init()
     gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Kernel data segment
     gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User code segment
     gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User data segment
-
-    uint32_t base = (uint32_t)&tss;
-    uint32_t limit = base + sizeof (tss_entry_t);
-
-    gdt_set_gate(5, base, limit, 0xE9, 0x00);    // TSS
+    gdt_set_gate(5, tssp.base, tssp.limit, 0xE9, 0x00); // TSS segment
 
     gdt_flush();
     tss_flush();
