07ca31bfe401ddc04276bbadf1f57667d1efe7a0
jezze <jens.nyberg@gmail.com>
2010-06-22 21:34:50 +0200

    some ramdisk changes

--- ramdisk/cat.c
+++ ramdisk/cat.c
@@ -5,29 +5,24 @@
 #include <kernel/screen.h>
 #include <kernel/vfs.h>
 
-void main(uint32_t argc, char *argv[])
+void main()
 {
 
-    if (argc > 1)
-    {
+    vfs_node_t *node = vfs_find(fsRoot, "about.txt");
 
-        vfs_node_t *node = vfs_find(fsRoot, argv[1]);
+    if (!node)
+        return;
 
-        if (!node)
-            return;
+    char buffer[400];
 
-        char buffer[400];
-
-        uint32_t size = vfs_read(node, 0, 400, buffer);
+    uint32_t size = vfs_read(node, 0, 400, buffer);
         
-        uint32_t i;
+    uint32_t i;
 
-        for (i = 0; i < size; i++)
-            screen_putc(buffer[i]);
+    for (i = 0; i < size; i++)
+        screen_putc(buffer[i]);
 
-        screen_putc('\n');
-
-    }
+    screen_putc('\n');
 
 }
 
