f3d9ab394d7d825a32fa6689ee971631b6c0706a
jezze <jens.nyberg@gmail.com>
2010-06-24 01:54:48 +0200

    syscalls on user

--- ramdisk/hello2.c
+++ ramdisk/hello2.c
@@ -1,9 +1,9 @@
 #include <lib/types.h>
-#include <lib/io.h>
+#include <kernel/syscall.h>
 
 void main()
 {
 
-    *((int*)0xb8000)=0x07690748;
+    call_send(SYSCALL_STRING_PUTS, "Hello World!\n");
 
 }
