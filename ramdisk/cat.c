e12120f3b649db4111c51e4d6c844ab8d23c8bb8
jezze <jens.nyberg@gmail.com>
2010-07-04 14:42:46 +0200

    fix cat issue

--- ramdisk/cat.c
+++ ramdisk/cat.c
@@ -12,11 +12,14 @@ void main(int argc, char *argv[])
     if (!node)
         return;
 
-    char buffer[400];
+    char buffer[2000];
 
-    unsigned int size = node->read(node, 0, 400, buffer);
-        
-    buffer[399] = '\0';
+    unsigned int size = node->read(node, 0, 2000, buffer);
+    
+    if (size == 2000)
+        buffer[size - 1] = '\0';
+    else
+        buffer[size] = '\0';
 
     call_puts(buffer);
     call_puts("\n");
