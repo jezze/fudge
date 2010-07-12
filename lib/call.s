534b45e0a95fa2c48bd38c31fad8861556e23cf9
jezze <jens.nyberg@gmail.com>
2010-07-12 20:08:08 +0200

    removed some ebp pushes

--- lib/call.s
+++ lib/call.s
@@ -1,61 +1,43 @@
 global call_puts
 call_puts:
-    push ebp
-    mov ebp, esp
     mov eax, 0x01
-    mov ecx, [ebp + 8]
+    mov ecx, [esp + 4]
     int 0x80
-    pop ebp
     ret
 
 global call_puts_dec
 call_puts_dec:
-    push ebp
-    mov ebp, esp
     mov eax, 0x02
-    mov ecx, [ebp + 8]
+    mov ecx, [esp + 4]
     int 0x80
-    pop ebp
     ret
 
 global call_puts_hex
 call_puts_hex:
-    push ebp
-    mov ebp, esp
     mov eax, 0x03
-    mov ecx, [ebp + 8]
+    mov ecx, [esp + 4]
     int 0x80
-    pop ebp
     ret
 
 global call_vfs_read
 call_vfs_read:
-    push ebp
-    mov ebp, esp
     mov eax, 0x10
-    mov ebx, [ebp + 8]
-    mov edi, [ebp + 12]
+    mov ebx, [esp + 4]
+    mov edi, [esp + 8]
     int 0x80
-    pop ebp
     ret
 
 global call_vfs_walk
 call_vfs_walk:
-    push ebp
-    mov ebp, esp
     mov eax, 0x12
-    mov ecx, [ebp + 8]
+    mov ecx, [esp + 4]
     int 0x80
-    pop ebp
     ret
 
 global call_vfs_find
 call_vfs_find:
-    push ebp
-    mov ebp, esp
     mov eax, 0x13
-    mov ecx, [ebp + 8]
+    mov ecx, [esp + 4]
     int 0x80
-    pop ebp
     ret
 
