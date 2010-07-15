ae03199d402739582885a5b335402228b619cb50
jezze <jens.nyberg@gmail.com>
2010-07-15 23:44:52 +0200

    removed some ebp pushing when not neccessary

--- kernel/arch/i386/mmu.s
+++ kernel/arch/i386/mmu.s
@@ -1,13 +1,10 @@
 global mmu_flush
 mmu_flush:
-    push ebp
-    mov ebp, esp
-    mov eax, [ebp + 8]
+    mov eax, [esp + 4]
     mov cr3, eax
     mov eax, cr0
     or eax, 0x80000000
     mov cr0, eax
-    pop ebp
     ret
 
 global mmu_read_cr0
@@ -27,19 +24,13 @@ mmu_read_cr3:
 
 global mmu_write_cr0
 mmu_write_cr0:
-    push ebp
-    mov ebp, esp
-    mov eax, [ebp + 8]
+    mov eax, [esp + 4]
     mov cr0, eax
-    pop ebp
     ret
 
 global mmu_write_cr3
 mmu_write_cr3:
-    push ebp
-    mov ebp, esp
-    mov eax, [ebp + 8]
+    mov eax, [esp + 4]
     mov cr3, eax
-    pop ebp
     ret
 
