4427ed89f58a9a8fc2d4bc46bb04abd167d55585
jezze <jens.nyberg@gmail.com>
2010-07-15 21:15:53 +0200

    removed byte from push in isr and joined syscall.s to isr.s

--- kernel/arch/i386/isr.s
+++ kernel/arch/i386/isr.s
@@ -1,254 +1,268 @@
 extern isr_handler
+extern syscall_handler
 
 ; Divide By Zero Exception
 global isr00
 isr00:
 	cli
-	push byte 0
-	push byte 0
+	push 0
+	push 0
 	jmp isr_common
 
 ; Debug Exception
 global isr01
 isr01:
 	cli
-	push byte 0
-	push byte 1
+	push 0
+	push 1
 	jmp isr_common
 
 ; Non Maskable Interrupt Exception
 global isr02
 isr02:
 	cli
-	push byte 0
-	push byte 2
+	push 0
+	push 2
 	jmp isr_common
 
 ; Int 3 Exception
 global isr03
 isr03:
 	cli
-	push byte 0
-	push byte 3
+	push 0
+	push 3
 	jmp isr_common
 
 ; INTO Exception
 global isr04
 isr04:
 	cli
-	push byte 0
-	push byte 4
+	push 0
+	push 4
 	jmp isr_common
 
 ; Out of Bounds Exception
 global isr05
 isr05:
 	cli
-	push byte 0
-	push byte 5
+	push 0
+	push 5
 	jmp isr_common
 
 ; Invalid Opcode Exception
 global isr06
 isr06:
 	cli
-	push byte 0
-	push byte 6
+	push 0
+	push 6
 	jmp isr_common
 
 ; Coprocessor Not Available Exception
 global isr07
 isr07:
 	cli
-	push byte 0
-	push byte 7
+	push 0
+	push 7
 	jmp isr_common
 
 ; Double Fault Exception
 global isr08
 isr08:
 	cli
-	push byte 8
+	push 8
 	jmp isr_common
 
 ; Coprocessor Segment Overrun Exception
 global isr09
 isr09:
 	cli
-	push byte 0
-	push byte 9
+	push 0
+	push 9
 	jmp isr_common
 
 ; Bad TSS Exception
 global isr0A
 isr0A:
 	cli
-	push byte 10
+	push 10
 	jmp isr_common
 
 ; Segment Not Present Exception
 global isr0B
 isr0B:
 	cli
-	push byte 11
+	push 11
 	jmp isr_common
 
 ; Stack Fault Exception Exception
 global isr0C
 isr0C:
 	cli
-	push byte 12
+	push 12
 	jmp isr_common
 
 ; General Protection Fault Exception
 global isr0D
 isr0D:
 	cli
-	push byte 13
+	push 13
 	jmp isr_common
 
 ; Page Fault Exception
 global isr0E
 isr0E:
 	cli
-	push byte 14
+	push 14
 	jmp isr_common
 
 ; Reserved Exception Exception
 global isr0F
 isr0F:
 	cli
-	push byte 0
-	push byte 15
+	push 0
+	push 15
 	jmp isr_common
 
 ; Floating Point Exception
 global isr10
 isr10:
 	cli
-	push byte 0
-	push byte 16
+	push 0
+	push 16
 	jmp isr_common
 
 ; Alignment Check Exception
 global isr11
 isr11:
 	cli
-	push byte 0
-	push byte 17
+	push 0
+	push 17
 	jmp isr_common
 
 ; Machine Check Exception
 global isr12
 isr12:
 	cli
-	push byte 0
-	push byte 18
+	push 0
+	push 18
 	jmp isr_common
 
 ; Reserved Exception
 global isr13
 isr13:
 	cli
-	push byte 0
-	push byte 19
+	push 0
+	push 19
 	jmp isr_common
 
 ; Reserved Exception
 global isr14
 isr14:
 	cli
-	push byte 0
-	push byte 20
+	push 0
+	push 20
 	jmp isr_common
 
 ; Reserved Exception
 global isr15
 isr15:
 	cli
-	push byte 0
-	push byte 21
+	push 0
+	push 21
 	jmp isr_common
 
 ; Reserved Exception
 global isr16
 isr16:
 	cli
-	push byte 0
-	push byte 22
+	push 0
+	push 22
 	jmp isr_common
 
 ; Reserved Exception
 global isr17
 isr17:
 	cli
-	push byte 0
-	push byte 23
+	push 0
+	push 23
 	jmp isr_common
 
 ; Reserved Exception
 global isr18
 isr18:
 	cli
-	push byte 0
-	push byte 24
+	push 0
+	push 24
 	jmp isr_common
 
 ; Reserved Exception
 global isr19
 isr19:
 	cli
-	push byte 0
-	push byte 25
+	push 0
+	push 25
 	jmp isr_common
 
 ; Reserved Exception
 global isr1A
 isr1A:
 	cli
-	push byte 0
-	push byte 26
+	push 0
+	push 26
 	jmp isr_common
 
 ; Reserved Exception
 global isr1B
 isr1B:
 	cli
-	push byte 0
-	push byte 27
+	push 0
+	push 27
 	jmp isr_common
 
 ; Reserved Exception
 global isr1C
 isr1C:
 	cli
-	push byte 0
-	push byte 28
+	push 0
+	push 28
 	jmp isr_common
 
 ; Reserved Exception
 global isr1D
 isr1D:
 	cli
-	push byte 0
-	push byte 29
+	push 0
+	push 29
 	jmp isr_common
 
 ; Reserved Exception
 global isr1E
 isr1E:
 	cli
-	push byte 0
-	push byte 30
+	push 0
+	push 30
 	jmp isr_common
 
 ; Reserved Exception
 global isr1F
 isr1F:
 	cli
-	push byte 0
-	push byte 31
+	push 0
+	push 31
 	jmp isr_common
+
+global syscall_interrupt
+syscall_interrupt:
+    cli
+    push edi
+    push esi
+    push edx
+    push ecx
+    push ebx
+    push eax
+    call syscall_handler
+    add esp, 24
+	iret
 
 isr_common:
 	pusha
