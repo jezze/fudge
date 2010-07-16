3e5d94867e30f87eda7655f35e1ad85e94a907ed
jezze <jens.nyberg@gmail.com>
2010-07-16 20:10:35 +0200

    cleanup - removed tabs from source

--- kernel/arch/i386/irq.s
+++ kernel/arch/i386/irq.s
@@ -2,136 +2,136 @@ extern irq_handler
 
 global irq00
 irq00:
-	cli
-	push byte 0
-	push byte 32
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 32
+    jmp irq_common
 
 global irq01
 irq01:
-	cli
-	push byte 0
-	push byte 33
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 33
+    jmp irq_common
 
 global irq02
 irq02:
-	cli
-	push byte 0
-	push byte 34
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 34
+    jmp irq_common
 
 global irq03
 irq03:
-	cli
-	push byte 0
-	push byte 35
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 35
+    jmp irq_common
 
 global irq04
 irq04:
-	cli
-	push byte 0
-	push byte 36
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 36
+    jmp irq_common
 
 global irq05
 irq05:
-	cli
-	push byte 0
-	push byte 37
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 37
+    jmp irq_common
 
 global irq06
 irq06:
-	cli
-	push byte 0
-	push byte 38
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 38
+    jmp irq_common
 
 global irq07
 irq07:
-	cli
-	push byte 0
-	push byte 39
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 39
+    jmp irq_common
 
 global irq08
 irq08:
-	cli
-	push byte 0
-	push byte 40
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 40
+    jmp irq_common
 
 global irq09
 irq09:
-	cli
-	push byte 0
-	push byte 41
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 41
+    jmp irq_common
 
 global irq0A
 irq0A:
-	cli
-	push byte 0
-	push byte 42
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 42
+    jmp irq_common
 
 global irq0B
 irq0B:
-	cli
-	push byte 0
-	push byte 43
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 43
+    jmp irq_common
 
 global irq0C
 irq0C:
-	cli
-	push byte 0
-	push byte 44
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 44
+    jmp irq_common
 
 global irq0D
 irq0D:
-	cli
-	push byte 0
-	push byte 45
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 45
+    jmp irq_common
 
 global irq0E
 irq0E:
-	cli
-	push byte 0
-	push byte 46
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 46
+    jmp irq_common
 
 global irq0F
 irq0F:
-	cli
-	push byte 0
-	push byte 47
-	jmp irq_common
+    cli
+    push byte 0
+    push byte 47
+    jmp irq_common
 
 irq_common:
-	pusha
-	push ds
-	push es
-	push fs
-	push gs
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
+    pusha
+    push ds
+    push es
+    push fs
+    push gs
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
     mov eax, esp
     push eax
     call irq_handler
     pop eax
-	pop gs
-	pop fs
-	pop es
-	pop ds
-	popa
-	add esp, 8
-	iret
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popa
+    add esp, 8
+    iret
 
--- kernel/arch/i386/isr.s
+++ kernel/arch/i386/isr.s
@@ -4,252 +4,252 @@ extern syscall_handler
 ; Divide By Zero Exception
 global isr00
 isr00:
-	cli
-	push 0
-	push 0x00
-	jmp isr_common
+    cli
+    push 0
+    push 0x00
+    jmp isr_common
 
 ; Debug Exception
 global isr01
 isr01:
-	cli
-	push 0
-	push 0x01
-	jmp isr_common
+    cli
+    push 0
+    push 0x01
+    jmp isr_common
 
 ; Non Maskable Interrupt Exception
 global isr02
 isr02:
-	cli
-	push 0
-	push 0x02
-	jmp isr_common
+    cli
+    push 0
+    push 0x02
+    jmp isr_common
 
 ; Int 3 Exception
 global isr03
 isr03:
-	cli
-	push 0
-	push 0x03
-	jmp isr_common
+    cli
+    push 0
+    push 0x03
+    jmp isr_common
 
 ; INTO Exception
 global isr04
 isr04:
-	cli
-	push 0
-	push 0x04
-	jmp isr_common
+    cli
+    push 0
+    push 0x04
+    jmp isr_common
 
 ; Out of Bounds Exception
 global isr05
 isr05:
-	cli
-	push 0
-	push 0x05
-	jmp isr_common
+    cli
+    push 0
+    push 0x05
+    jmp isr_common
 
 ; Invalid Opcode Exception
 global isr06
 isr06:
-	cli
-	push 0
-	push 0x06
-	jmp isr_common
+    cli
+    push 0
+    push 0x06
+    jmp isr_common
 
 ; Coprocessor Not Available Exception
 global isr07
 isr07:
-	cli
-	push 0
-	push 0x07
-	jmp isr_common
+    cli
+    push 0
+    push 0x07
+    jmp isr_common
 
 ; Double Fault Exception
 global isr08
 isr08:
-	cli
-	push 0x08
-	jmp isr_common
+    cli
+    push 0x08
+    jmp isr_common
 
 ; Coprocessor Segment Overrun Exception
 global isr09
 isr09:
-	cli
-	push 0
-	push 0x09
-	jmp isr_common
+    cli
+    push 0
+    push 0x09
+    jmp isr_common
 
 ; Bad TSS Exception
 global isr0A
 isr0A:
-	cli
-	push 0x0A
-	jmp isr_common
+    cli
+    push 0x0A
+    jmp isr_common
 
 ; Segment Not Present Exception
 global isr0B
 isr0B:
-	cli
-	push 0x0B
-	jmp isr_common
+    cli
+    push 0x0B
+    jmp isr_common
 
 ; Stack Fault Exception Exception
 global isr0C
 isr0C:
-	cli
-	push 0x0C
-	jmp isr_common
+    cli
+    push 0x0C
+    jmp isr_common
 
 ; General Protection Fault Exception
 global isr0D
 isr0D:
-	cli
-	push 0x0D
-	jmp isr_common
+    cli
+    push 0x0D
+    jmp isr_common
 
 ; Page Fault Exception
 global isr0E
 isr0E:
-	cli
-	push 0x0E
-	jmp isr_common
+    cli
+    push 0x0E
+    jmp isr_common
 
 ; Reserved Exception Exception
 global isr0F
 isr0F:
-	cli
-	push 0
-	push 0x0F
-	jmp isr_common
+    cli
+    push 0
+    push 0x0F
+    jmp isr_common
 
 ; Floating Point Exception
 global isr10
 isr10:
-	cli
-	push 0
-	push 0x10
-	jmp isr_common
+    cli
+    push 0
+    push 0x10
+    jmp isr_common
 
 ; Alignment Check Exception
 global isr11
 isr11:
-	cli
-	push 0
-	push 0x11
-	jmp isr_common
+    cli
+    push 0
+    push 0x11
+    jmp isr_common
 
 ; Machine Check Exception
 global isr12
 isr12:
-	cli
-	push 0
-	push 0x12
-	jmp isr_common
+    cli
+    push 0
+    push 0x12
+    jmp isr_common
 
 ; Reserved Exception
 global isr13
 isr13:
-	cli
-	push 0
-	push 0x13
-	jmp isr_common
+    cli
+    push 0
+    push 0x13
+    jmp isr_common
 
 ; Reserved Exception
 global isr14
 isr14:
-	cli
-	push 0
-	push 0x14
-	jmp isr_common
+    cli
+    push 0
+    push 0x14
+    jmp isr_common
 
 ; Reserved Exception
 global isr15
 isr15:
-	cli
-	push 0
-	push 0x15
-	jmp isr_common
+    cli
+    push 0
+    push 0x15
+    jmp isr_common
 
 ; Reserved Exception
 global isr16
 isr16:
-	cli
-	push 0
-	push 0x16
-	jmp isr_common
+    cli
+    push 0
+    push 0x16
+    jmp isr_common
 
 ; Reserved Exception
 global isr17
 isr17:
-	cli
-	push 0
-	push 0x17
-	jmp isr_common
+    cli
+    push 0
+    push 0x17
+    jmp isr_common
 
 ; Reserved Exception
 global isr18
 isr18:
-	cli
-	push 0
-	push 0x18
-	jmp isr_common
+    cli
+    push 0
+    push 0x18
+    jmp isr_common
 
 ; Reserved Exception
 global isr19
 isr19:
-	cli
-	push 0
-	push 0x19
-	jmp isr_common
+    cli
+    push 0
+    push 0x19
+    jmp isr_common
 
 ; Reserved Exception
 global isr1A
 isr1A:
-	cli
-	push 0
-	push 0x1A
-	jmp isr_common
+    cli
+    push 0
+    push 0x1A
+    jmp isr_common
 
 ; Reserved Exception
 global isr1B
 isr1B:
-	cli
-	push 0
-	push 0x1B
-	jmp isr_common
+    cli
+    push 0
+    push 0x1B
+    jmp isr_common
 
 ; Reserved Exception
 global isr1C
 isr1C:
-	cli
-	push 0
-	push 0x1C
-	jmp isr_common
+    cli
+    push 0
+    push 0x1C
+    jmp isr_common
 
 ; Reserved Exception
 global isr1D
 isr1D:
-	cli
-	push 0
-	push 0x1D
-	jmp isr_common
+    cli
+    push 0
+    push 0x1D
+    jmp isr_common
 
 ; Reserved Exception
 global isr1E
 isr1E:
-	cli
-	push 0
-	push 0x1E
-	jmp isr_common
+    cli
+    push 0
+    push 0x1E
+    jmp isr_common
 
 ; Reserved Exception
 global isr1F
 isr1F:
-	cli
-	push 0
-	push 0x1F
-	jmp isr_common
+    cli
+    push 0
+    push 0x1F
+    jmp isr_common
 
 global isr_syscall
 isr_syscall:
@@ -260,30 +260,30 @@ isr_syscall:
     call syscall_handler
     pop eax
     popa
-	iret
+    iret
 
 isr_common:
-	pusha
-	push ds
-	push es
-	push fs
-	push gs
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov eax, esp
-	push eax
-	call isr_handler
-	pop eax
-	pop gs
-	pop fs
-	pop es
-	pop ds
-	popa
-	add esp, 8
-	iret
+    pusha
+    push ds
+    push es
+    push fs
+    push gs
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov eax, esp
+    push eax
+    call isr_handler
+    pop eax
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popa
+    add esp, 8
+    iret
 
 global isr_enable
 isr_enable:
