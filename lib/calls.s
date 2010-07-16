c7edc32bb492c941ec370ace0104244c93960962
jezze <jens.nyberg@gmail.com>
2010-07-16 18:58:48 +0200

    added ebp around system calls

--- lib/calls.s
+++ lib/calls.s
@@ -1,26 +1,38 @@
 global call_vfs_walk
 call_vfs_walk:
+    push ebp
+    mov ebp, esp
     mov eax, 0x12
-    mov ecx, [esp + 4]
+    mov ecx, [ebp + 8]
     int 0x80
+    pop ebp
     ret
 
 global call_vfs_find
 call_vfs_find:
+    push ebp
+    mov ebp, esp
     mov eax, 0x13
-    mov ecx, [esp + 4]
+    mov ecx, [ebp + 8]
     int 0x80
+    pop ebp
     ret
 
 global call_halt
 call_halt:
+    push ebp
+    mov ebp, esp
     mov eax, 0x50
     int 0x80
+    pop ebp
     ret
 
 global call_reboot
 call_reboot:
+    push ebp
+    mov ebp, esp
     mov eax, 0x51
     int 0x80
+    pop ebp
     ret
 
