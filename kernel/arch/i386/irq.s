f9e62c2274c94b94366546ca89a9a49ad51c1097
jezze <jens.nyberg@gmail.com>
2010-07-16 20:18:06 +0200

    added hex numbers to irq.s

--- kernel/arch/i386/irq.s
+++ kernel/arch/i386/irq.s
@@ -4,112 +4,112 @@ global irq00
 irq00:
     cli
     push byte 0
-    push byte 32
+    push byte 0x20
     jmp irq_common
 
 global irq01
 irq01:
     cli
     push byte 0
-    push byte 33
+    push byte 0x21
     jmp irq_common
 
 global irq02
 irq02:
     cli
     push byte 0
-    push byte 34
+    push byte 0x22
     jmp irq_common
 
 global irq03
 irq03:
     cli
     push byte 0
-    push byte 35
+    push byte 0x23
     jmp irq_common
 
 global irq04
 irq04:
     cli
     push byte 0
-    push byte 36
+    push byte 0x24
     jmp irq_common
 
 global irq05
 irq05:
     cli
     push byte 0
-    push byte 37
+    push byte 0x25
     jmp irq_common
 
 global irq06
 irq06:
     cli
     push byte 0
-    push byte 38
+    push byte 0x26
     jmp irq_common
 
 global irq07
 irq07:
     cli
     push byte 0
-    push byte 39
+    push byte 0x27
     jmp irq_common
 
 global irq08
 irq08:
     cli
     push byte 0
-    push byte 40
+    push byte 0x28
     jmp irq_common
 
 global irq09
 irq09:
     cli
     push byte 0
-    push byte 41
+    push byte 0x29
     jmp irq_common
 
 global irq0A
 irq0A:
     cli
     push byte 0
-    push byte 42
+    push byte 0x2A
     jmp irq_common
 
 global irq0B
 irq0B:
     cli
     push byte 0
-    push byte 43
+    push byte 0x2B
     jmp irq_common
 
 global irq0C
 irq0C:
     cli
     push byte 0
-    push byte 44
+    push byte 0x2C
     jmp irq_common
 
 global irq0D
 irq0D:
     cli
     push byte 0
-    push byte 45
+    push byte 0x2D
     jmp irq_common
 
 global irq0E
 irq0E:
     cli
     push byte 0
-    push byte 46
+    push byte 0x2E
     jmp irq_common
 
 global irq0F
 irq0F:
     cli
     push byte 0
-    push byte 47
+    push byte 0x2F
     jmp irq_common
 
 irq_common:
