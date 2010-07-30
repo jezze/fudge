47ee2f27ac0d595292879e9202d2dc4607e21c78
jezze <jens.nyberg@gmail.com>
2010-07-30 22:13:34 +0200

    added fundamental arm vga support. it was hell.

--- kernel/arch/arm/arch.c
+++ kernel/arch/arm/arch.c
@@ -55,10 +55,61 @@ void arch_disable_interrupts()
 
 }
 
+void arch_put_pixel(volatile unsigned short *address, unsigned short value)
+{
+
+    *address = value;
+
+}
+
 void arch_init()
 {
 
     arch_puts("Hello World!\n");
+
+    volatile unsigned int *mem;
+
+    mem = (volatile unsigned int *)0x10000014;
+    *mem = 0xA05F;
+
+    mem = (volatile unsigned int *)0x1000001C;
+    *mem = 0x12C11;
+
+    mem = (volatile unsigned int *)0xC0000000;
+    *mem = 0x3F1F3F9C;
+
+    mem = (volatile unsigned int *)0xC0000004;
+    *mem = 0x080B61DF;
+
+    mem = (volatile unsigned int *)0xC0000008;
+    *mem = 0x067F3800;
+
+    mem = (volatile unsigned int *)0xC0000010;
+    *mem = 0x00200000;
+
+    mem = (volatile unsigned int *)0xC000001C;
+    *mem = 0x1829;
+
+    mem = (volatile unsigned int *)0x1000000C;
+    *mem = 0x3E005;
+
+    volatile unsigned short *video;
+
+    video = (volatile unsigned short *)0x00200000;
+    *video = 0xFFFF;
+
+    video = (volatile unsigned short *)0x00200002;
+    *video = 0xFC00;
+
+    video = (volatile unsigned short *)0x00200004;
+    *video = 0x83E0;
+    
+    video = (volatile unsigned short *)0x00200006;
+    *video = 0x801F;
+
+    video = (volatile unsigned short *)0x00200008;
+
+    arch_put_pixel(video, (unsigned short)0xFFFF);
 
 }
 
