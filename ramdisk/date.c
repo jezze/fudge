b1d69940e33c556d7c7c4a8a905d54ca6e7a690e
jezze <jens.nyberg@gmail.com>
2010-08-02 22:50:03 +0200

    fixed date

--- ramdisk/date.c
+++ ramdisk/date.c
@@ -4,5 +4,45 @@
 void main(int argc, char *argv[])
 {
 
+    struct vfs_node *node = call_vfs_find("dev/rtc");
+
+    unsigned char buffer;
+
+    call_puts("20");
+    vfs_read(node, 0, 1, &buffer);
+    call_puts_bcd(buffer);
+
+    call_puts("-");
+
+    vfs_read(node, 1, 1, &buffer);
+    call_puts_bcd(buffer);
+
+    call_puts("-");
+
+    vfs_read(node, 2, 1, &buffer);
+    call_puts_bcd(buffer);
+
+    call_puts(" ");
+
+    vfs_read(node, 3, 1, &buffer);
+    call_puts_bcd(buffer);
+
+    call_puts(":");
+
+    vfs_read(node, 4, 1, &buffer);
+    call_puts_bcd(buffer);
+
+    call_puts(":");
+
+    vfs_read(node, 5, 1, &buffer);
+    call_puts_bcd(buffer);
+
+    vfs_read(node, 3, 1, &buffer);
+
+    if (buffer >> 4)
+        call_puts("PM\n");
+    else
+        call_puts("AM\n");
+
 }
 
