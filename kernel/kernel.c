d37d174e6a10193dd3d17148ef1c2a856f5cd3b8
jezze <jens.nyberg@gmail.com>
2010-08-02 20:05:36 +0200

    more cleanups

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,37 +6,19 @@
 #include <kernel/mboot.h>
 #include <kernel/shell.h>
 
-struct vfs_node vfsRoot;
-
-static void kernel_init_vfs()
-{
-
-    string_copy(vfsRoot.name, "root");
-    vfsRoot.inode = 0;
-    vfsRoot.length = 0;
-    vfsRoot.open = 0;
-    vfsRoot.close = 0;
-    vfsRoot.read = 0;
-    vfsRoot.write = 0;
-    vfsRoot.walk = 0;
-
-}
+struct vfs_node *vfsRoot;
 
 struct vfs_node *kernel_get_vfs_root()
 {
 
-    return &vfsRoot;
+    return vfsRoot;
 
 }
 
 void kernel_main(struct mboot_info *header, unsigned int magic)
 {
 
-    kernel_init_vfs();
-
-    struct vfs_node *initrd = initrd_init(*((unsigned int *)header->modulesAddresses));
-
-    vfsRoot.walk = initrd->walk;
+    vfsRoot = initrd_init(*((unsigned int *)header->modulesAddresses));
 
     ASSERT(magic == MBOOT_MAGIC);
     ASSERT(header->modulesCount);
