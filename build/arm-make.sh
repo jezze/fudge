5f7c70aef29b8d14f8352b7c7e09e1cf5bd5b711
jezze <jens.nyberg@gmail.com>
2010-09-25 00:26:28 +0200

    arm script cleanup

--- build/arm-make.sh
+++ build/arm-make.sh
@@ -4,12 +4,12 @@ DIR_KERNEL="../kernel"
 DIR_KERNEL_ARCH="../kernel/arch/arm"
 DIR_LIB="../lib"
 
-ASM=arm-elf-as
+ASM="arm-elf-as"
 ASMFLAGS=
-GCC=arm-elf-gcc
-GCCFLAGS=-c -I../include -Wall -Wextra -ffreestanding -nostdlib -nostartfiles -std=gnu99 -mhard-float
-LD=arm-elf-ld
-LDFLAGS=-T./linker-arm.ld
+GCC="arm-elf-gcc"
+GCCFLAGS="-c -I../include -Wall -Wextra -ffreestanding -nostdlib -nostartfiles -std=gnu99 -mhard-float"
+LD="arm-elf-ld"
+LDFLAGS="-T./linker-arm.ld"
 
 $GCC $GCCFLAGS $DIR_KERNEL/assert.c -o $DIR_KERNEL/assert.o
 $GCC $GCCFLAGS $DIR_KERNEL/kernel.c -o $DIR_KERNEL/kernel.o
@@ -19,11 +19,11 @@ $GCC $GCCFLAGS $DIR_LIB/string.c -o $DIR_LIB/string.o
 $GCC $GCCFLAGS $DIR_LIB/vfs.c -o $DIR_LIB/vfs.o
 
 $LD $LDFLAGS \
-    ../kernel/assert.o \
-    ../kernel/kernel.o \
-    ../kernel/arch/arm/arch.o \
-    ../kernel/arch/arm/init.o \
-    ../lib/string.o \
-    ../lib/vfs.o \
+    $DIR_KERNEL/assert.o \
+    $DIR_KERNEL/kernel.o \
+    $DIR_KERNEL_ARCH/arch.o \
+    $DIR_KERNEL_ARCH/init.o \
+    $DIR_LIB/string.o \
+    $DIR_LIB/vfs.o \
     -o root/boot/kernel
 
