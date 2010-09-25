a445bfb4fc071c4130de993280b0c9577b7ee46a
jezze <jens.nyberg@gmail.com>
2010-09-26 00:32:27 +0200

    reorder functions in cr.s

--- kernel/arch/x86/cr.s
+++ kernel/arch/x86/cr.s
@@ -3,25 +3,15 @@ cr0_read:
     mov eax, cr0
     ret
 
-global cr2_read
-cr2_read:
-    mov eax, cr2
-    ret
-
-global cr3_read
-cr3_read:
-    mov eax, cr3
-    ret
-
-global cr4_read
-cr4_read:
-    mov eax, cr4
-    ret
-
 global cr0_write
 cr0_write:
     mov eax, [esp + 4]
     mov cr0, eax
+    ret
+
+global cr2_read
+cr2_read:
+    mov eax, cr2
     ret
 
 global cr2_write
@@ -30,10 +20,20 @@ cr2_write:
     mov cr2, eax
     ret
 
+global cr3_read
+cr3_read:
+    mov eax, cr3
+    ret
+
 global cr3_write
 cr3_write:
     mov eax, [esp + 4]
     mov cr3, eax
+    ret
+
+global cr4_read
+cr4_read:
+    mov eax, cr4
     ret
 
 global cr4_write
