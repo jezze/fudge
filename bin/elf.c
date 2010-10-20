19c73ed3e2ff70e22ea7e51f37a5cad3884a830d
jezze <jens.nyberg@gmail.com>
2010-10-20 22:20:10 +0200

    fixed elf.c

--- bin/elf.c
+++ bin/elf.c
@@ -10,7 +10,7 @@ void main(int argc, char *argv[])
     if (argc != 2)
         return;
 
-    struct vfs_node *node = call_open(argv[1]);
+    struct vfs_node *node = vfs_find(session_get_location(), argv[1]);
 
     if (!node)
         return;
