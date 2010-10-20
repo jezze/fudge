207653259344ed5b4ee3fbc6240fcae1f89e8c73
jezze <jens.nyberg@gmail.com>
2010-10-20 20:35:33 +0200

    some clear.c cleanups

--- bin/clear.c
+++ bin/clear.c
@@ -1,33 +1,23 @@
 #include <call.h>
 #include <file.h>
+#include <session.h>
 #include <vfs.h>
 
 void main(int argc, char *argv[])
 {
 
-    struct vfs_node *vgaCursor = call_open("/dev/vga_fb_cursor");
-    struct vfs_node *node = call_open("/dev/vga_fb");
-
-    unsigned offset;
-
-    vfs_read(vgaCursor, 0, 1, &offset);
- 
-    char row[80];
-
-    vfs_read(node, 80 * (offset / 80), 80, row);
+    struct vfs_node *vgaFb = call_open("/dev/vga_fb");
+    struct vfs_node *vgaFbCursor = call_open("/dev/vga_fb_cursor");
 
     char c = ' ';
     unsigned int i;
 
     for (i = 0; i < 2000; i++)
-        vfs_write(node, i, 1, &c);
+        vfs_write(vgaFb, i, 1, &c);
 
-    vfs_write(node, 0, 80, row);
+    i = 0;
 
-    offset %= 80;
-
-    vfs_write(vgaCursor, 0, 1, &offset);
-
+    vfs_write(vgaFbCursor, 0, 1, &i);
 
 }
 
