f9e57738a6c46318cf06507ceb9a5aaefb089b4b
jezze <jens.nyberg@gmail.com>
2010-10-20 14:47:11 +0200

    added missing bin folder

--- build/clean.sh
+++ build/clean.sh
@@ -2,7 +2,17 @@
 
 rm -f fudge.img
 rm -f fudge.iso
-rm -f root/bin/*
+rm -f root/bin/cat
+rm -f root/bin/cd
+rm -f root/bin/clear
+rm -f root/bin/cpu
+rm -f root/bin/date
+rm -f root/bin/echo
+rm -f root/bin/elf
+rm -f root/bin/hello
+rm -f root/bin/ls
+rm -f root/bin/reboot
+rm -f root/bin/timer
 rm -f root/boot/kernel
 rm -f root/boot/initrd
 rm -f ../kernel/*.o
