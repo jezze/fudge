3bf71ad04b4f44b0e055ef52574fa5942fcb3702
jezze <jens.nyberg@gmail.com>
2010-12-16 00:37:13 +0100

    changed name of mmu flags

--- include/arch/x86/kernel/mmu.h
+++ include/arch/x86/kernel/mmu.h
@@ -7,18 +7,7 @@
 #define MMU_ERROR_RESERVED 1 << 3
 #define MMU_ERROR_FETCH    1 << 4
 
-#define MMU_PAGE_SIZE 0x1000
-
-#define MMU_DIRECTORY_ADDRESS           0x196000
-#define MMU_DIRECTORY_SIZE              1024
-#define MMU_DIRECTORY_FLAG_PRESENT      1 << 0
-#define MMU_DIRECTORY_FLAG_WRITEABLE    1 << 1
-#define MMU_DIRECTORY_FLAG_USERMODE     1 << 2
-#define MMU_DIRECTORY_FLAG_CACHEWRITE   1 << 3
-#define MMU_DIRECTORY_FLAG_CACHEDISABLE 1 << 4
-#define MMU_DIRECTORY_FLAG_ACCESSED     1 << 5
-#define MMU_DIRECTORY_FLAG_LARGE        1 << 6
-#define MMU_DIRECTORY_FLAG_IGNORED      1 << 7
+#define MMU_DIRECTORY_SIZE    1024
 
 #define MMU_TABLE_ADDRESS           0x197000
 #define MMU_TABLE_SIZE              1024
@@ -28,8 +17,17 @@
 #define MMU_TABLE_FLAG_CACHEWRITE   1 << 3
 #define MMU_TABLE_FLAG_CACHEDISABLE 1 << 4
 #define MMU_TABLE_FLAG_ACCESSED     1 << 5
-#define MMU_TABLE_FLAG_DIRTY        1 << 6
-#define MMU_TABLE_FLAG_GLOBAL       1 << 7
+#define MMU_TABLE_FLAG_LARGE        1 << 6
+#define MMU_TABLE_FLAG_IGNORED      1 << 7
+
+#define MMU_PAGE_FLAG_PRESENT      1 << 0
+#define MMU_PAGE_FLAG_WRITEABLE    1 << 1
+#define MMU_PAGE_FLAG_USERMODE     1 << 2
+#define MMU_PAGE_FLAG_CACHEWRITE   1 << 3
+#define MMU_PAGE_FLAG_CACHEDISABLE 1 << 4
+#define MMU_PAGE_FLAG_ACCESSED     1 << 5
+#define MMU_PAGE_FLAG_DIRTY        1 << 6
+#define MMU_PAGE_FLAG_GLOBAL       1 << 7
 
 struct isr_registers;
 
