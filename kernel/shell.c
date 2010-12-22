bebc2a0da8e7f2c5ade3179b23adf9966274db77
jezze <jens.nyberg@gmail.com>
2010-12-22 01:31:04 +0100

    changed dynamic memory area address

--- kernel/shell.c
+++ kernel/shell.c
@@ -40,8 +40,6 @@ static void shell_execute_elf(struct vfs_node *node, void *address, int argc, ch
     struct elf_header *header = (struct elf_header *)address;
     struct elf_program_header *programHeader = (struct elf_program_header *)(address + header->programHeaderOffset);
 
-    file_read(node, programHeader->offset, programHeader->memorySize, (void *)0x00400000);
-
     mmu_clear_directory(&shellProgramDirectory);
     mmu_clear_table(&shellProgramTables[0]);
     mmu_clear_table(&shellProgramTables[1]);
@@ -54,7 +52,7 @@ static void shell_execute_elf(struct vfs_node *node, void *address, int argc, ch
     mmu_add_table(&shellProgramDirectory, index + 1, &shellProgramTables[2], MMU_TABLE_FLAG_PRESENT | MMU_TABLE_FLAG_WRITEABLE);
 
     mmu_map(&shellProgramDirectory, 0x00000000, 0x00000000, 0x00400000, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE);
-    mmu_map(&shellProgramDirectory, programHeader->virtualAddress, 0x00400000, programHeader->memorySize, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE);
+    mmu_map(&shellProgramDirectory, programHeader->virtualAddress, (unsigned int)address, programHeader->memorySize, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE);
 
     mmu_set_directory(&shellProgramDirectory);
 
@@ -69,7 +67,7 @@ static void shell_execute_elf(struct vfs_node *node, void *address, int argc, ch
 static void shell_call(struct vfs_node *node, int argc, char *argv[])
 {
 
-    void *buffer = (void *)0x280000;
+    void *buffer = (void *)0x00300000;
     file_read(node, 0, node->length, buffer);
 
     if (elf_check(buffer))
