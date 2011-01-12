47e4d0d3f5b2ecdcf4069f08e1baed1f8dbe628e
jezze <jens.nyberg@gmail.com>
2011-01-13 00:50:28 +0100

    removed old function from kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,16 +1,9 @@
-#include <lib/cbuffer.h>
 #include <kernel/dev.h>
 #include <kernel/initrd.h>
 #include <kernel/kernel.h>
 #include <kernel/shell.h>
 #include <kernel/tty.h>
 #include <kernel/vfs.h>
-#include <arch/x86/kernel/io.h>
-#include <arch/x86/modules/pit/pit.h>
-#include <arch/x86/modules/rtc/rtc.h>
-#include <arch/x86/modules/ata/ata.h>
-#include <arch/x86/modules/kbd/kbd.h>
-#include <arch/x86/modules/vga/vga.h>
 
 unsigned int kernelInitrdAddress;
 
@@ -21,33 +14,12 @@ void kernel_set_initrd(unsigned int initrdAddress)
 
 }
 
-static void kernel_init_devices()
-{
-
-    dev_init();
-    io_init();
-    vga_init();
-    pit_init();
-    kbd_init();
-    rtc_init();
-    ata_init();
-
-}
-
-static void kernel_init_filesystem()
+void kernel_init()
 {
 
     vfs_init();
     initrd_init(kernelInitrdAddress);
-
-}
-
-void kernel_init()
-{
-
-    kernel_init_filesystem();
-    kernel_init_devices();
-
+    dev_init();
     tty_init();
     shell_init();
 
