40f4e7a2974f052d59f77ccfd5b8700fc65e5c9b
jezze <jens.nyberg@gmail.com>
2011-01-17 03:13:15 +0100

    updated arm build files

--- build/arm-make.sh
+++ build/arm-make.sh
@@ -12,32 +12,25 @@ GCCFLAGS="-c -I../include -Wall -Wextra -ffreestanding -nostdlib -nostartfiles -
 LD="arm-elf-ld"
 LDFLAGS="-T./linker-arm.ld"
 
-$GCC $GCCFLAGS $DIR_KERNEL/assert.c -o $DIR_KERNEL/assert.o
 $GCC $GCCFLAGS $DIR_KERNEL/dev.c -o $DIR_KERNEL/dev.o
 $GCC $GCCFLAGS $DIR_KERNEL/kernel.c -o $DIR_KERNEL/kernel.o
 $GCC $GCCFLAGS $DIR_KERNEL/vfs.c -o $DIR_KERNEL/vfs.o
 $GCC $GCCFLAGS $DIR_ARCH_KERNEL/arch.c -o $DIR_ARCH_KERNEL/arch.o
 $ASM $ASMFLAGS $DIR_ARCH_KERNEL/init.s -o $DIR_ARCH_KERNEL/init.o
-$GCC $GCCFLAGS $DIR_LIB/call.c -o $DIR_LIB/call.o
 $GCC $GCCFLAGS $DIR_LIB/memory.c -o $DIR_LIB/memory.o
-$GCC $GCCFLAGS $DIR_LIB/stdin.c -o $DIR_LIB/stdin.o
-$GCC $GCCFLAGS $DIR_LIB/stdout.c -o $DIR_LIB/stdout.o
+$GCC $GCCFLAGS $DIR_LIB/file.c -o $DIR_LIB/file.o
 $GCC $GCCFLAGS $DIR_LIB/string.c -o $DIR_LIB/string.o
-$GCC $GCCFLAGS $DIR_LIB/vfs.c -o $DIR_LIB/vfs.o
 $GCC $GCCFLAGS $DIR_ARCH_LIB/call.c -o $DIR_ARCH_LIB/call.o
 
 $LD $LDFLAGS \
-    $DIR_KERNEL/assert.o \
     $DIR_KERNEL/dev.o \
     $DIR_KERNEL/kernel.o \
     $DIR_KERNEL/vfs.o \
     $DIR_ARCH_KERNEL/arch.o \
     $DIR_ARCH_KERNEL/init.o \
+    $DIR_LIB/file.o \
     $DIR_LIB/memory.o \
-    $DIR_LIB/stdin.o \
-    $DIR_LIB/stdout.o \
     $DIR_LIB/string.o \
-    $DIR_LIB/vfs.o \
     $DIR_ARCH_LIB/call.o \
     -o root/boot/kernel
 
