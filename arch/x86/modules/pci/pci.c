9b1cad1866118ba637e539d1c8835566c9d861ea
Jens Nyberg <jens.nyberg@gmail.com>
2011-02-25 04:21:38 +0100

    made initial pci functions

--- arch/x86/modules/pci/pci.c
+++ arch/x86/modules/pci/pci.c
@@ -1,4 +1,40 @@
+#include <arch/x86/kernel/io.h>
 #include <arch/x86/modules/pci/pci.h>
+
+unsigned short pci_read(unsigned short bus, unsigned short slot, unsigned short func, unsigned short offset)
+{
+
+    unsigned int lbus = (unsigned int)bus;
+    unsigned int lslot = (unsigned int)slot;
+    unsigned int lfunc = (unsigned int)func;
+    unsigned int tmp = 0;
+
+    unsigned int address = (unsigned int)((lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xFC) | ((unsigned int)0x80000000));
+
+    io_outd(PCI_ADDRESS, address);
+
+    tmp = (unsigned short)((io_ind(PCI_DATA) >> ((offset & 2) * 8)) & 0xFFFF);
+
+    return tmp;
+
+}
+
+unsigned short pci_check_vendor(unsigned short bus, unsigned short slot)
+{
+
+    unsigned short vendor;
+    unsigned short device;
+
+    if ((vendor == pci_read(bus, slot, 0, 0)) != 0xFFFF)
+    {
+
+        device = pci_read(bus, slot, 0, 2);
+
+    }
+
+    return vendor;
+
+}
 
 void pci_init()
 {
--- include/arch/x86/modules/pci/pci.h
+++ include/arch/x86/modules/pci/pci.h
@@ -1,6 +1,9 @@
 #ifndef ARCH_PCI_H
 #define ARCH_PCI_H
 
+#define PCI_ADDRESS 0xCF8
+#define PCI_DATA    0xCFC
+
 extern void pci_init();
 
 #endif
