00808aa555bc6646aca6a7c517f650fb07d28d0d
jezze <jens.nyberg@gmail.com>
2011-03-09 00:40:09 +0100

    removed the last of old initrd fs. now tarfs only

--- include/kernel/initrd.h
+++ include/kernel/initrd.h
@@ -1,23 +1,6 @@
 #ifndef KERNEL_INITRD_H
 #define KERNEL_INITRD_H
 
-struct initrd_header
-{
-
-    unsigned int nfiles;
-
-};
-
-struct initrd_file_header
-{
-
-    unsigned char magic;
-    char name[64];
-    unsigned int offset;
-    unsigned int length;
-
-};
-
 extern void initrd_init(unsigned int address);
 
 #endif
--- kernel/initrd.c
+++ kernel/initrd.c
@@ -28,24 +28,26 @@ static unsigned int initrd_file_read(struct vfs_node *node, unsigned int offset,
     return count;
 
 }
-/*
+
 static unsigned int initrd_file_write(struct vfs_node *node, unsigned int offset, unsigned int count, void *buffer)
 {
 
-    struct initrd_file_header header = initrdFileHeaders[node->inode];
+    struct tar_header *header = initrdFileHeaders[node->inode];
 
-    if (offset > header.length)
+    if (offset > node->length)
         return 0;
 
-    if (offset + count > header.length)
-        count = header.length - offset;
+    if (offset + count > node->length)
+        count = node->length - offset;
 
-    memory_copy((unsigned char *)(header.offset + offset), buffer, count);
+    unsigned int address = (unsigned int)header + 512;
+
+    memory_copy((unsigned char *)(address + offset), buffer, count);
 
     return count;
 
 }
-*/
+
 static struct vfs_node *initrd_node_walk(struct vfs_node *node, unsigned int index)
 {
 
@@ -62,39 +64,6 @@ static unsigned int initrd_node_write(struct vfs_node *node, unsigned int offset
     return count;
 
 }
-
-/*
-void initrd_init(unsigned int address)
-{
-
-    struct vfs_node *initrdNode = vfs_add_node("initrd", 0);
-    initrdNode->walk = initrd_node_walk;
-    initrdNode->write = initrd_node_write;
-
-    initrdHeader = (struct initrd_header*)address;
-    initrdFileHeaders = (struct initrd_file_header *)(address + sizeof (struct initrd_header));
-
-    unsigned int i;
-
-    for (i = 0; i < initrdHeader->nfiles; i++)
-    {
-
-        initrdFileHeaders[i].offset += address;
-
-        struct vfs_node *initrdFileNode = vfs_add_node(initrdFileHeaders[i].name, initrdFileHeaders[i].length);
-        initrdFileNode->inode = i;
-        initrdFileNode->read = initrd_file_read;
-        initrdFileNode->write = initrd_file_write;
-
-        file_write(initrdNode, initrdNode->length, 1, initrdFileNode);
-
-    }
-
-    struct vfs_node *rootNode = call_open("/");
-    file_write(rootNode, rootNode->length, 1, initrdNode);
-
-}
-*/
 
 void initrd_init(unsigned int address)
 {
@@ -126,7 +95,7 @@ void initrd_init(unsigned int address)
         struct vfs_node *initrdFileNode = vfs_add_node(header->name + 9, size);
         initrdFileNode->inode = inode;
         initrdFileNode->read = initrd_file_read;
-//        initrdFileNode->write = initrd_file_write;
+        initrdFileNode->write = initrd_file_write;
 
         file_write(initrdNode, initrdNode->length, 1, initrdFileNode);
 
@@ -143,3 +112,4 @@ void initrd_init(unsigned int address)
     file_write(rootNode, rootNode->length, 1, initrdNode);
 
 }
+
