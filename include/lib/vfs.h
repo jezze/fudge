dcb4faecd0141332a45128f2eb7ee3cc127b4614
jezze <jens.nyberg@gmail.com>
2011-03-27 00:07:50 +0100

    removed some typedefs

--- include/lib/vfs.h
+++ include/lib/vfs.h
@@ -1,25 +1,17 @@
 #ifndef LIB_VFS_H
 #define LIB_VFS_H
 
-struct vfs_node;
-
-typedef void (*vfs_open_t)(struct vfs_node*);
-typedef void (*vfs_close_t)(struct vfs_node*);
-typedef unsigned int (*vfs_read_t)(struct vfs_node*, unsigned int offset, unsigned int count, void *buffer);
-typedef unsigned int (*vfs_write_t)(struct vfs_node*, unsigned int offset, unsigned int count, void *buffer);
-typedef struct vfs_node * (*vfs_walk_t)(struct vfs_node*, unsigned int index);
-
 struct vfs_node
 {
 
     char name[128];
     unsigned int inode;
     unsigned int length;
-    vfs_open_t open;
-    vfs_close_t close;
-    vfs_read_t read;
-    vfs_write_t write;
-    vfs_walk_t walk;
+    void (*open)(struct vfs_node *node);
+    void (*close)(struct vfs_node *node);
+    unsigned int (*read)(struct vfs_node *node, unsigned int offset, unsigned int count, void *buffer);
+    unsigned int (*write)(struct vfs_node *node, unsigned int offset, unsigned int count, void *buffer);
+    struct vfs_node *(*walk)(struct vfs_node *node, unsigned int index);
 
 };
 
