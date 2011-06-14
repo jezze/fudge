234a592d37d2e2e0a5474ad3c6578411d2166e72
Jens Nyberg <jens.nyberg@gmail.com>
2011-06-14 23:04:34 +0200

    added prototypes for new driver interface

--- include/kernel/modules.h
+++ include/kernel/modules.h
@@ -1,6 +1,11 @@
 #ifndef KERNEL_MODULES_H
 #define KERNEL_MODULES_H
 
+#define MODULES_BUS_TYPE_ATA 1
+#define MODULES_BUS_TYPE_COM 2 
+#define MODULES_BUS_TYPE_PCI 3
+#define MODULES_BUS_TYPE_USB 4
+
 #define MODULES_TYPE_BASE       0
 #define MODULES_TYPE_TIMER      1
 #define MODULES_TYPE_KEYBOARD   2
@@ -23,11 +28,34 @@ struct modules_device
 {
 
     struct modules_module module;
-    struct file_node node;
+    char name[32];
+    struct file_node node; // REMOVE
 
 };
 
-extern void modules_register(unsigned int type, struct modules_module *module);
+struct modules_driver
+{
+
+    struct modules_module module;
+    char name[32];
+    struct modules_device *devices[32];
+
+};
+
+struct modules_bus
+{
+
+    struct modules_module module;
+    char name[32];
+    struct modules_device *devices[32];
+    struct modules_driver *drivers[32];
+
+};
+
+extern void modules_register(unsigned int type, struct modules_module *module); // REMOVE
+extern void modules_register_bus(unsigned int type, struct modules_bus *bus);
+extern void modules_register_device(unsigned int type, struct modules_device *device);
+extern void modules_register_driver(unsigned int type, struct modules_driver *driver);
 extern struct modules_module *modules_find(unsigned int type);
 extern void modules_init();
 
