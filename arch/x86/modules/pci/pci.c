1e4466625738aab3a6cf60f009358f2b6ffb7908
Jens Nyberg <jens.nyberg@gmail.com>
2011-07-03 02:53:50 +0200

    more pci updates

--- arch/x86/modules/pci/pci.c
+++ arch/x86/modules/pci/pci.c
@@ -8,6 +8,91 @@ static struct modules_bus pciBus;
 static struct pci_device pciDevices[256];
 static unsigned int pciDevicesCount;
 
+static struct pci_device *pci_get_device(struct vfs_node *node)
+{
+
+    unsigned int i;
+
+    for (i = 0; i < pciDevicesCount; i++)
+    {
+
+        if (&pciDevices[i].base.node == node)
+            return &pciDevices[i];
+
+    }
+
+    return 0;
+
+}
+
+unsigned int pci_write_num(char *out, unsigned int num, unsigned int base)
+{
+
+    if (!num)
+    {
+
+        out[0] = '0';
+
+        return 1;
+
+    }
+
+    char buffer[32] = {0};
+
+    int i;
+
+    for (i = 30; num && i; --i, num /= base)
+        buffer[i] = "0123456789abcdef"[num % base];
+
+    string_copy(out, buffer + i + 1);
+
+    return 32 - i;
+
+}
+
+
+
+static unsigned int pci_device_read(struct vfs_node *node, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    struct pci_device *device = pci_get_device(node);
+
+    if (!device)
+        return 0;
+
+    char num[32];
+
+    string_copy(buffer, "Vendor: 0x");
+    pci_write_num(num, device->vendor, 16);
+    string_concat(buffer, num);
+    string_concat(buffer, "\n");
+
+    string_concat(buffer, "Device: 0x");
+    pci_write_num(num, device->device, 16);
+    string_concat(buffer, num);
+    string_concat(buffer, "\n");
+
+    string_concat(buffer, "Class: 0x");
+    pci_write_num(num, device->classcode, 16);
+    string_concat(buffer, num);
+    string_concat(buffer, "\n");
+
+    string_concat(buffer, "Sublass: 0x");
+    pci_write_num(num, device->subclass, 16);
+    string_concat(buffer, num);
+    string_concat(buffer, "\n");
+
+    string_concat(buffer, "Interface: 0x");
+    pci_write_num(num, device->interface, 16);
+    string_concat(buffer, num);
+    string_concat(buffer, "\n");
+
+
+
+    return string_length(buffer);
+
+}
+
 static unsigned short pci_read(unsigned short bus, unsigned short slot, unsigned short func, unsigned short offset)
 {
 
@@ -36,10 +121,19 @@ static void pci_check_vendor(unsigned short bus, unsigned short slot)
 
     struct pci_device *device = &pciDevices[pciDevicesCount];
 
-    string_copy(device->base.name, "pci:0:0");
+    string_copy(device->base.name, "pci:");
+    pci_write_num(device->base.name + 4, bus, 10);
+    string_concat(device->base.name, ":");
+    pci_write_num(device->base.name + 6, slot, 10);
+
     device->base.node.length = 0;
+    device->base.node.operations.read = pci_device_read;
     device->vendor = vendor;
     device->device = pci_read(bus, slot, 0, 2);
+    device->interface = (pci_read(bus, slot, 0, 8) >> 8);
+    device->revision = (pci_read(bus, slot, 0, 8) & 0xFF);
+    device->classcode = (pci_read(bus, slot, 0, 10) >> 8);
+    device->subclass = (pci_read(bus, slot, 0, 10) & 0xFF);
 
     modules_register_device(MODULES_DEVICE_TYPE_PCI, &device->base);
 
@@ -50,17 +144,13 @@ static void pci_check_vendor(unsigned short bus, unsigned short slot)
 void pci_init()
 {
 
-    string_copy(pciBus.name, "pci");
+    string_copy(pciBus.name, "pci:0");
     modules_register_bus(MODULES_BUS_TYPE_PCI, &pciBus);
 
     pci_check_vendor(0, 0);
     pci_check_vendor(0, 1);
     pci_check_vendor(0, 2);
     pci_check_vendor(0, 3);
-    pci_check_vendor(0, 4);
-    pci_check_vendor(0, 5);
-    pci_check_vendor(0, 6);
-    pci_check_vendor(0, 7);
 
 }
 
--- include/arch/x86/modules/pci/pci.h
+++ include/arch/x86/modules/pci/pci.h
@@ -4,6 +4,26 @@
 #define PCI_ADDRESS 0xCF8
 #define PCI_DATA    0xCFC
 
+#define PCI_CLASS_UNKNOWN   0x00
+#define PCI_CLASS_STORAGE   0x01
+#define PCI_CLASS_NETWORK   0x02
+#define PCI_CLASS_DISPLAY   0x03
+#define PCI_CLASS_MEDIA     0x04
+#define PCI_CLASS_MEMORY    0x05
+#define PCI_CLASS_BRIDGE    0x06
+#define PCI_CLASS_SIMPLE    0x07
+#define PCI_CLASS_BASE      0x08
+#define PCI_CLASS_INPUT     0x09
+#define PCI_CLASS_DOCK      0x0A
+#define PCI_CLASS_PROCESSOR 0x0B
+#define PCI_CLASS_SERIAL    0x0C
+#define PCI_CLASS_WIRELESS  0x0D
+#define PCI_CLASS_IO        0x0E
+#define PCI_CLASS_SATELLITE 0x0F
+#define PCI_CLASS_CRYPT     0x10
+#define PCI_CLASS_SIGNAL    0x11
+#define PCI_CLASS_OTHER     0xFF
+
 struct pci_device
 {
 
