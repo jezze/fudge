7e437d2eb3f4d81cc8a9b0a08c3f2e812080b887
Jens Nyberg <jens.nyberg@gmail.com>
2011-07-09 16:18:58 +0200

    small arch cleanup

--- arch/x86/kernel/arch.c
+++ arch/x86/kernel/arch.c
@@ -14,17 +14,24 @@
 
 static struct kernel_arch arch;
 
-static void arch_interrupts_disable()
+static void arch_disable_interrupts()
 {
 
     cpu_interrupts_off();
 
 }
 
-static void arch_interrupts_enable()
+static void arch_enable_interrupts()
 {
 
     cpu_interrupts_on();
+
+}
+
+static void arch_enable_usermode(unsigned int address)
+{
+
+    cpu_usermode(address);
 
 }
 
@@ -51,13 +58,6 @@ static void arch_set_stack(unsigned int address)
 
 }
 
-static void arch_usermode(unsigned int address)
-{
-
-    cpu_usermode(address);
-
-}
-
 static void arch_init_base()
 {
 
@@ -71,24 +71,24 @@ static void arch_init_base()
     mmu_init();
     syscall_init();
 
-    cpu_interrupts_on();
+    arch.enable_interrupts();
 
 }
 
 void arch_init(struct mboot_info *header, unsigned int magic, unsigned int stack)
 {
 
+    arch.disable_interrupts = arch_disable_interrupts;
+    arch.enable_interrupts = arch_enable_interrupts;
+    arch.enable_usermode = arch_enable_usermode;
+    arch.set_stack = arch_set_stack;
+    arch.stackAddress = stack;
+    arch.initrdAddress = (unsigned int *)header->modulesAddresses;
+
     arch_init_base();
 
     kernel_assert(magic == MBOOT_MAGIC, "MBOOT_MAGIC is not correct", __FILE__, __LINE__);
     kernel_assert(header->modulesCount, "Module does not exist", __FILE__, __LINE__);
-
-    arch.enable_interrupts = arch_interrupts_enable;
-    arch.enable_usermode = arch_usermode;
-    arch.disable_interrupts = arch_interrupts_disable;
-    arch.stackAddress = stack;
-    arch.set_stack = arch_set_stack;
-    arch.initrdAddress = (unsigned int *)header->modulesAddresses;
 
     kernel_init(&arch);
 
