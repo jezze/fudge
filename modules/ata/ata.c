c4709bdb6b3d56e950fccba82a4de86785414083
jezze <jens.nyberg@gmail.com>
2011-10-07 00:35:26 +0200

    cleaned up ata

--- modules/ata/ata.c
+++ modules/ata/ata.c
@@ -27,40 +27,13 @@ static struct ata_device *ata_get_device(struct vfs_node *node)
 
 }
 
-static unsigned int ata_write_num(char *out, unsigned int num, unsigned int base)
-{
-
-    if (!num)
-    {
-
-        out[0] = '0';
-
-        return 1;
-
-    }
-
-    char buffer[32] = {0};
-
-    int i;
-
-    for (i = 30; num && i; --i, num /= base)
-        buffer[i] = "0123456789abcdef"[num % base];
-
-    string_copy(out, buffer + i + 1);
-
-    return 32 - i;
-
-}
-
-static unsigned int ata_device_read(struct vfs_node *node, unsigned int count, void *buffer)
+static unsigned int ata_device_node_read(struct vfs_node *node, unsigned int count, void *buffer)
 {
 
     struct ata_device *device = ata_get_device(node);
 
     if (!device)
         return 0;
-
-    char num[32];
 
     string_copy(buffer, "Type: ");
 
@@ -243,7 +216,7 @@ void ata_init_devices()
     ataDevices[0].base.module.type = MODULES_TYPE_DEVICE;
     ataDevices[0].base.type = MODULES_DEVICE_TYPE_ATA;
     string_copy(ataDevices[0].base.name, "ata:0:0");
-    ataDevices[0].base.node.operations.read = ata_device_read;
+    ataDevices[0].base.node.operations.read = ata_device_node_read;
     ataDevices[0].control = ATA_PRIMARY_MASTER_CONTROL;
     ataDevices[0].data = ATA_PRIMARY_MASTER_DATA;
 
@@ -253,7 +226,7 @@ void ata_init_devices()
     ataDevices[1].base.module.type = MODULES_TYPE_DEVICE;
     ataDevices[1].base.type = MODULES_DEVICE_TYPE_ATA;
     string_copy(ataDevices[1].base.name, "ata:0:1");
-    ataDevices[1].base.node.operations.read = ata_device_read;
+    ataDevices[1].base.node.operations.read = ata_device_node_read;
     ataDevices[1].control = ATA_PRIMARY_SLAVE_CONTROL;
     ataDevices[1].data = ATA_PRIMARY_SLAVE_DATA;
 
@@ -263,7 +236,7 @@ void ata_init_devices()
     ataDevices[2].base.module.type = MODULES_TYPE_DEVICE;
     ataDevices[2].base.type = MODULES_DEVICE_TYPE_ATA;
     string_copy(ataDevices[2].base.name, "ata:1:0");
-    ataDevices[2].base.node.operations.read = ata_device_read;
+    ataDevices[2].base.node.operations.read = ata_device_node_read;
     ataDevices[2].control = ATA_SECONDARY_MASTER_CONTROL;
     ataDevices[2].data = ATA_SECONDARY_MASTER_DATA;
 
@@ -273,7 +246,7 @@ void ata_init_devices()
     ataDevices[3].base.module.type = MODULES_TYPE_DEVICE;
     ataDevices[3].base.type = MODULES_DEVICE_TYPE_ATA;
     string_copy(ataDevices[3].base.name, "ata:1:1");
-    ataDevices[3].base.node.operations.read = ata_device_read;
+    ataDevices[3].base.node.operations.read = ata_device_node_read;
     ataDevices[3].control = ATA_SECONDARY_SLAVE_CONTROL;
     ataDevices[3].data = ATA_SECONDARY_SLAVE_DATA;
 
