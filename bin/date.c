7da02852fe9f746855b32967cd8e08ecf85b94c7
jezze <jens.nyberg@gmail.com>
2011-10-09 17:35:39 +0200

    fixed date command

--- bin/date.c
+++ bin/date.c
@@ -1,56 +1,13 @@
 #include <fudge.h>
 
-void write_bcd(unsigned int fd, unsigned char num)
-{
-
-    file_write_format(fd, "%d", num >> 4);
-    file_write_format(fd, "%d", num & 0x0F);
-
-}
-
 void main(int argc, char *argv[])
 {
 
+    char buffer[64];
+
     int fd = file_open("/rtc");
-
-    unsigned char buffer;
-
-    file_write_format(FILE_STDOUT, "20");
-    file_read(fd, 1, &buffer);
-    write_bcd(FILE_STDOUT, buffer);
-
-    file_write_format(FILE_STDOUT, "-");
-
-    file_read(fd, 1, &buffer);
-    write_bcd(FILE_STDOUT, buffer);
-
-    file_write_format(FILE_STDOUT, "-");
-
-    file_read(fd, 1, &buffer);
-    write_bcd(FILE_STDOUT, buffer);
-
-    file_write_format(FILE_STDOUT, " ");
-
-    file_read(fd, 1, &buffer);
-    write_bcd(FILE_STDOUT, buffer);
-
-    file_write_format(FILE_STDOUT, ":");
-
-    file_read(fd, 1, &buffer);
-    write_bcd(FILE_STDOUT, buffer);
-
-    file_write_format(FILE_STDOUT, ":");
-
-    file_read(fd, 1, &buffer);
-    write_bcd(FILE_STDOUT, buffer);
-
-    file_read(fd, 1, &buffer);
-
-    if (buffer >> 4)
-        file_write_format(FILE_STDOUT, "PM\n");
-    else
-        file_write_format(FILE_STDOUT, "AM\n");
-
+    file_read(fd, 64, buffer);
+    file_write_format(FILE_STDOUT, "Date: %s\n", buffer);
     file_close(fd);
 
 }
