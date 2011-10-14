37d1cda35d8304ab5f6bbce8d8220ce3cba055b6
jezze <jens.nyberg@gmail.com>
2011-10-14 05:11:15 +0200

    further progress on execute syscall

--- kernel/arch/x86/syscall.c
+++ kernel/arch/x86/syscall.c
@@ -122,6 +122,8 @@ static void syscall_info(struct syscall_registers *registers)
 
 }
 
+int num = 1;
+
 static void syscall_map(struct syscall_registers *registers)
 {
 
@@ -156,22 +158,38 @@ static void syscall_map(struct syscall_registers *registers)
 
     struct mmu_header *pHeader = mmu_get_program_header(address);
 
-    mmu_map_header(pHeader, programHeader->virtualAddress, (unsigned int)address, 0x10000, MMU_TABLE_FLAG_PRESENT | MMU_TABLE_FLAG_WRITEABLE | MMU_TABLE_FLAG_USERMODE, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE | MMU_PAGE_FLAG_USERMODE);
-    mmu_set_directory(&pHeader->directory);
-
     // This is for testing purposes
     if (registers->eip > 0x00800000)
     {
 
-        memory_set(address + 0x7995, 0, 10);
+        //file_write_format(2, "Address: 0x%x\n", address);
+        //file_write_format(2, "Entry: 0x%x\n", header->entry);
+        //file_write_format(2, "ESP: 0x%x\n", registers->useresp);
+        //file_write_format(2, "EIP: 0x%x\n", registers->eip);
+
+        memory_set(address + 0x7FFC, 0, 4); // second argument
+        memory_set(address + 0x7FF8, 0, 4); // first argument argc
+        memory_set(address + 0x7FF8, 1, 1); // first argument argc
+        memory_set(address + 0x7FF4, 0, 4); // ebp
+
+        // need to set ebp to a function that reloads previous task and continues
+
         registers->eip = header->entry;
-        //registers->ebp = address + 0x8000;
-        registers->useresp = (unsigned int)address + 0x8000;
+        registers->useresp = (unsigned int)(address + 0x7FF4); // set esp to ebp
         registers->eax = 0;
+
+        //file_write_format(2, "ESP: 0x%x\n", registers->useresp);
+        //file_write_format(2, "EIP: 0x%x\n", registers->eip);
+
+        mmu_map_header(pHeader, programHeader->virtualAddress, (unsigned int)address, 0x10000, MMU_TABLE_FLAG_PRESENT | MMU_TABLE_FLAG_WRITEABLE | MMU_TABLE_FLAG_USERMODE, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE | MMU_PAGE_FLAG_USERMODE);
+        mmu_set_directory(&pHeader->directory);
 
         return;
 
     }
+
+    mmu_map_header(pHeader, programHeader->virtualAddress, (unsigned int)address, 0x10000, MMU_TABLE_FLAG_PRESENT | MMU_TABLE_FLAG_WRITEABLE | MMU_TABLE_FLAG_USERMODE, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE | MMU_PAGE_FLAG_USERMODE);
+    mmu_set_directory(&pHeader->directory);
 
     registers->eax = header->entry;
 
