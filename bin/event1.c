03fc05fa9b8d2be452819c54d26d859f3a346c43
jezze <jens.nyberg@gmail.com>
2011-10-31 01:49:19 +0100

    added an event example

---
+++ bin/event1.c
@@ -0,0 +1,20 @@
+#include <fudge.h>
+
+void explain()
+{
+
+    file_write_format(FILE_STDOUT, "You executed a program it seems...\n");
+
+    call_exit();
+
+}
+
+void main(int argc, char *argv[])
+{
+
+    call_attach(0x03, explain);
+
+    call_wait();
+
+}
+
