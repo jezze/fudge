9cdd599d29a9982883ae85391895266745b03388
jezze <jens.nyberg@gmail.com>
2011-11-03 22:40:27 +0100

    cleanups

--- kernel/arch/x86/gdt.c
+++ kernel/arch/x86/gdt.c
@@ -1,26 +1,26 @@
 #include <lib/memory.h>
 #include <kernel/arch/x86/gdt.h>
 
-static struct gdt_entry gdt[GDT_TABLE_SLOTS];
+static struct gdt_entry gdtEntries[GDT_TABLE_SLOTS];
 static struct gdt_pointer gdtPointer;
 
 void gdt_set_gate(unsigned char index, unsigned int base, unsigned int limit, unsigned char access, unsigned char granularity)
 {
 
-    gdt[index].baseLow = (base & 0xFFFF);
-    gdt[index].baseMiddle = (base >> 16) & 0xFF;
-    gdt[index].baseHigh = (base >> 24) & 0xFF;
-    gdt[index].limitLow = (limit & 0xFFFF);
-    gdt[index].granularity = (limit >> 16) & 0x0F;
-    gdt[index].granularity |= (granularity & 0xF0);
-    gdt[index].access = access;
+    gdtEntries[index].baseLow = (base & 0xFFFF);
+    gdtEntries[index].baseMiddle = (base >> 16) & 0xFF;
+    gdtEntries[index].baseHigh = (base >> 24) & 0xFF;
+    gdtEntries[index].limitLow = (limit & 0xFFFF);
+    gdtEntries[index].granularity = (limit >> 16) & 0x0F;
+    gdtEntries[index].granularity |= (granularity & 0xF0);
+    gdtEntries[index].access = access;
 
 }
 
 void gdt_init()
 {
 
-    memory_set(&gdt, 0, sizeof (struct gdt_entry) * GDT_TABLE_SLOTS);
+    memory_set(&gdtEntries, 0, sizeof (struct gdt_entry) * GDT_TABLE_SLOTS);
 
     gdt_set_gate(0x00, 0x00000000, 0x00000000, 0x00, 0x00); // Null segment
     gdt_set_gate(0x01, 0x00000000, 0xFFFFFFFF, 0x9A, 0xCF); // Kernel code segment
@@ -28,7 +28,7 @@ void gdt_init()
     gdt_set_gate(0x03, 0x00000000, 0xFFFFFFFF, 0xFA, 0xCF); // User code segment
     gdt_set_gate(0x04, 0x00000000, 0xFFFFFFFF, 0xF2, 0xCF); // User data segment
 
-    gdtPointer.base = gdt;
+    gdtPointer.base = gdtEntries;
     gdtPointer.limit = (sizeof (struct gdt_entry) * GDT_TABLE_SLOTS) - 1;
     gdt_flush(&gdtPointer);
 
--- kernel/arch/x86/idt.c
+++ kernel/arch/x86/idt.c
@@ -1,7 +1,7 @@
 #include <lib/memory.h>
 #include <kernel/arch/x86/idt.h>
 
-static struct idt_entry idt[IDT_TABLE_SLOTS];
+static struct idt_entry idtEntries[IDT_TABLE_SLOTS];
 static struct idt_pointer idtPointer;
 
 void idt_set_gate(unsigned char index, void *callback, unsigned short selector, unsigned char flags)
@@ -9,20 +9,20 @@ void idt_set_gate(unsigned char index, void *callback, unsigned short selector, 
 
     unsigned int base = (unsigned int)callback;
 
-    idt[index].baseLow = (base & 0xFFFF);
-    idt[index].baseHigh = (base >> 16) & 0xFFFF;
-    idt[index].selector = selector;
-    idt[index].zero = 0;
-    idt[index].flags = flags | 0x60;
+    idtEntries[index].baseLow = (base & 0xFFFF);
+    idtEntries[index].baseHigh = (base >> 16) & 0xFFFF;
+    idtEntries[index].selector = selector;
+    idtEntries[index].zero = 0;
+    idtEntries[index].flags = flags | 0x60;
 
 }
 
 void idt_init()
 {
 
-    memory_set(&idt, 0, sizeof (struct idt_entry) * IDT_TABLE_SLOTS);
+    memory_set(&idtEntries, 0, sizeof (struct idt_entry) * IDT_TABLE_SLOTS);
 
-    idtPointer.base = idt;
+    idtPointer.base = idtEntries;
     idtPointer.limit = (sizeof (struct idt_entry) * IDT_TABLE_SLOTS) - 1;
     idt_flush(&idtPointer);
 
