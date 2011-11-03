a5389e404300b64682a439365df14acdd2e4879b
jezze <jens.nyberg@gmail.com>
2011-11-03 23:05:01 +0100

    changed tss name for better consistency

--- kernel/arch/x86/tss.c
+++ kernel/arch/x86/tss.c
@@ -2,26 +2,26 @@
 #include <kernel/arch/x86/gdt.h>
 #include <kernel/arch/x86/tss.h>
 
-static struct tss_entry tss;
+static struct tss_entry tssEntry;
 
 void tss_set_stack(void *address)
 {
 
-    tss.esp0 = (unsigned int)address;
+    tssEntry.esp0 = (unsigned int)address;
 
 }
 
 void tss_init()
 {
 
-    memory_set(&tss, 0, sizeof (struct tss_entry));
+    memory_set(&tssEntry, 0, sizeof (struct tss_entry));
 
-    tss.ss0 = 0x10;
-    tss.esp0 = 0x00;
-    tss.cs = 0x0B;
-    tss.ss = tss.ds = tss.es = tss.fs = tss.gs = 0x13;
+    tssEntry.ss0 = 0x10;
+    tssEntry.esp0 = 0x00;
+    tssEntry.cs = 0x0B;
+    tssEntry.ss = tssEntry.ds = tssEntry.es = tssEntry.fs = tssEntry.gs = 0x13;
 
-    unsigned int base = (unsigned int)&tss;
+    unsigned int base = (unsigned int)&tssEntry;
     unsigned int limit = base + sizeof (struct tss_entry);
 
     gdt_set_gate(0x05, base, limit, 0xE9, 0x00);
