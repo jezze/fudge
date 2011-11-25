20938be576ef43ea0c11ce28a76e2b64fb57bf45
jezze <jens.nyberg@gmail.com>
2011-11-25 19:18:33 +0100

    made some cleanups

--- modules/rtl8139/rtl8139.c
+++ modules/rtl8139/rtl8139.c
@@ -7,21 +7,21 @@
 #include <modules/pci/pci.h>
 #include <modules/rtl8139/rtl8139.h>
 
-static struct rtl8139_driver rtl8139Driver;
+static struct rtl8139_driver driver;
 
-static void rtl8139_handle_irq()
+static void handle_irq()
 {
 
 }
 
-void rtl8139_poweron(struct rtl8139_driver *driver)
+static void poweron(struct rtl8139_driver *driver)
 {
 
     io_outb(driver->io + RTL8139_REGISTER_CONFIG1, 0x00);
 
 }
 
-void rtl8139_reset(struct rtl8139_driver *driver)
+static void reset(struct rtl8139_driver *driver)
 {
 
     io_outb(driver->io + RTL8139_REGISTER_CR, 0x10);
@@ -30,7 +30,7 @@ void rtl8139_reset(struct rtl8139_driver *driver)
 
 }
 
-void rtl8139_set_rx(struct rtl8139_driver *driver, unsigned int rcr)
+static void set_rx(struct rtl8139_driver *driver, unsigned int rcr)
 {
 
     io_outd(driver->io + RTL8139_REGISTER_RBSTART, (unsigned int)driver->rx);
@@ -38,38 +38,55 @@ void rtl8139_set_rx(struct rtl8139_driver *driver, unsigned int rcr)
 
 }
 
-void rtl8139_set_interrupt_flags(struct rtl8139_driver *driver, unsigned short flags)
+static void set_interrupt_flags(struct rtl8139_driver *driver, unsigned short flags)
 {
 
     io_outw(driver->io + RTL8139_REGISTER_IMR, flags);
 
 }
 
-void rtl8139_enable(struct rtl8139_driver *driver)
+static void enable(struct rtl8139_driver *driver)
 {
 
     io_outw(driver->io + RTL8139_REGISTER_CR, 0x0C);
 
 }
 
-void rtl8139_get_mac(struct rtl8139_driver *driver)
+static void get_mac(struct rtl8139_driver *driver)
 {
 
-    rtl8139Driver.mac[0] = io_inb(driver->io + RTL8139_REGISTER_IDR0);
-    rtl8139Driver.mac[1] = io_inb(driver->io + RTL8139_REGISTER_IDR1);
-    rtl8139Driver.mac[2] = io_inb(driver->io + RTL8139_REGISTER_IDR2);
-    rtl8139Driver.mac[3] = io_inb(driver->io + RTL8139_REGISTER_IDR3);
-    rtl8139Driver.mac[4] = io_inb(driver->io + RTL8139_REGISTER_IDR4);
-    rtl8139Driver.mac[5] = io_inb(driver->io + RTL8139_REGISTER_IDR5);
+    driver->mac[0] = io_inb(driver->io + RTL8139_REGISTER_IDR0);
+    driver->mac[1] = io_inb(driver->io + RTL8139_REGISTER_IDR1);
+    driver->mac[2] = io_inb(driver->io + RTL8139_REGISTER_IDR2);
+    driver->mac[3] = io_inb(driver->io + RTL8139_REGISTER_IDR3);
+    driver->mac[4] = io_inb(driver->io + RTL8139_REGISTER_IDR4);
+    driver->mac[5] = io_inb(driver->io + RTL8139_REGISTER_IDR5);
 
 }
 
 static unsigned int rtl8139_macs_read(struct vfs_node *self, unsigned int count, void *buffer)
 {
 
-    string_write_format(buffer, "%x:%x:%x:%x:%x:%x", rtl8139Driver.mac[0], rtl8139Driver.mac[1], rtl8139Driver.mac[2], rtl8139Driver.mac[3], rtl8139Driver.mac[4], rtl8139Driver.mac[5]);
+    string_write_format(buffer, "%x:%x:%x:%x:%x:%x", driver.mac[0], driver.mac[1], driver.mac[2], driver.mac[3], driver.mac[4], driver.mac[5]);
 
     return string_length(buffer);
+
+}
+
+void rtl8139_driver_init(struct rtl8139_driver *driver, struct pci_device *device)
+{
+
+    driver->base.device = &device->base;
+    driver->io = (device->configuration.bar0 & ~1);
+
+    poweron(driver);
+    reset(driver);
+    get_mac(driver);
+    set_rx(driver, 0x0F);
+    set_interrupt_flags(driver, 0x05);
+    enable(driver);
+
+    stream_device_init(&driver->macs, "mac", rtl8139_macs_read, 0);
 
 }
 
@@ -86,21 +103,21 @@ void init()
     if (!device)
         return;
 
-    rtl8139Driver.base.device = &device->base;
-    rtl8139Driver.io = (device->configuration.bar0 & ~1);
+    rtl8139_driver_init(&driver, device);
 
-    kernel_register_irq(device->configuration.interruptline, rtl8139_handle_irq);
+    kernel_register_irq(device->configuration.interruptline, handle_irq);
 
-    rtl8139_poweron(&rtl8139Driver);
-    rtl8139_reset(&rtl8139Driver);
-    rtl8139_get_mac(&rtl8139Driver);
-    rtl8139_set_rx(&rtl8139Driver, 0x0F);
-    rtl8139_set_interrupt_flags(&rtl8139Driver, 0x05);
-    rtl8139_enable(&rtl8139Driver);
-
-    stream_device_init(&rtl8139Driver.macs, "mac", rtl8139_macs_read, 0);
-
-    modules_register_device(&rtl8139Driver.macs.base);
+    modules_register_device(&driver.macs.base);
 
 }
 
+void destroy()
+{
+
+    struct pci_device *device = (struct pci_device *)driver.base.device;
+
+    kernel_unregister_irq(device->configuration.interruptline);
+
+    modules_unregister_device(&driver.macs.base);
+
+}
