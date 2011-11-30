5dafc864a34db35e55571a9abb8f5694e68d0db0
jezze <jens.nyberg@gmail.com>
2011-11-30 03:34:22 +0100

    added some code to print to console

--- kernel/arch/arm/arch.c
+++ kernel/arch/arm/arch.c
@@ -3,8 +3,31 @@
 
 static struct arch_arm arm;
 
+#define SERIAL_BASE 0x16000000
+#define SERIAL_FLAG_REGISTER 0x18
+#define SERIAL_BUFFER_FULL (1 << 5)
+ 
+void putc(char c)
+{
+
+    while (*(volatile unsigned long*)(SERIAL_BASE + SERIAL_FLAG_REGISTER) & (SERIAL_BUFFER_FULL));
+
+    *(volatile unsigned long*)SERIAL_BASE = c;
+
+}
+                                                         
+void puts(const char *s)
+{
+
+    while (*s)
+        putc(*s++);
+
+}
+
 void arch_init()
 {
+
+    puts("Fudge Console Text\n\n");
 
     for (;;);
 
