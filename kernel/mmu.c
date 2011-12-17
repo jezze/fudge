072fee0e1bd300d9fa8618d594cd17a87c11013a
jezze <jens.nyberg@gmail.com>
2011-12-17 14:19:38 +0100

    changed a name in mmu

--- kernel/mmu.c
+++ kernel/mmu.c
@@ -1,25 +1,25 @@
 #include <kernel/mmu.h>
 
-static struct mmu_unit *mmuUnit;
+static struct mmu_unit *primary;
 
 void *mmu_get_task_memory(unsigned int id)
 {
 
-    return mmuUnit->get_task_memory(id);
+    return primary->get_task_memory(id);
 
 }
 
 void mmu_load_task_memory(void *paddress)
 {
 
-    mmuUnit->load_task_memory(paddress);
+    primary->load_task_memory(paddress);
 
 }
 
 void mmu_map_task_memory(void *paddress, void *vaddress, unsigned int size, unsigned int tflags, unsigned int pflags)
 {
 
-    mmuUnit->map_task_memory(paddress, vaddress, size, tflags, pflags);
+    primary->map_task_memory(paddress, vaddress, size, tflags, pflags);
 
 }
 
@@ -40,7 +40,7 @@ void mmu_unit_init(struct mmu_unit *unit, void (*setup)(), void (*enable)(), voi
 void mmu_register_unit(struct mmu_unit *unit)
 {
 
-    mmuUnit = unit;
+    primary = unit;
 
 }
 
