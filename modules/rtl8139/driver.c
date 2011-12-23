29e966ab30080aeb4824035c46b755dd1eeeb15e
jezze <jens.nyberg@gmail.com>
2011-12-23 04:29:13 +0100

    seperated rtl8139 driver files

--- modules/rtl8139/driver.c
+++ modules/rtl8139/driver.c
@@ -6,8 +6,6 @@
 #include <modules/pci/pci.h>
 #include <modules/rtl8139/rtl8139.h>
 
-static struct rtl8139_driver driver;
-
 static void poweron(struct rtl8139_driver *driver)
 {
 
@@ -71,47 +69,6 @@ void rtl8139_driver_init(struct rtl8139_driver *driver, struct pci_device *devic
     set_rx(driver, 0x0F);
     set_interrupt_flags(driver, 0x05);
     enable(driver);
-
-}
-
-static void handle_irq()
-{
-
-}
-
-void init()
-{
-
-    unsigned int i;
-    struct pci_bus *bus;
-
-    for (i = 0; (bus = (struct pci_bus *)modules_get_bus(PCI_BUS_TYPE, i)); i++)
-    {
-
-        struct pci_device *device = bus->find_device_by_id(bus, 0x10EC, 0x8139, 0);
-
-        if (device)
-        {
-
-            rtl8139_driver_init(&driver, device);
-            kernel_register_irq(device->configuration.interruptline, handle_irq);
-            modules_register_driver(&driver.base);
-
-            break;
-
-        }
-
-    }
-
-}
-
-void destroy()
-{
-
-    struct pci_device *device = (struct pci_device *)driver.base.device;
-
-    kernel_unregister_irq(device->configuration.interruptline);
-    modules_unregister_driver(&driver.base);
 
 }
 
