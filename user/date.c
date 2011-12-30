a5d40e8e6dff21ccb6a6ffa5ff1c8a0e7c886402
jezze <jens.nyberg@gmail.com>
2011-12-30 14:46:10 +0100

    some safety added

--- user/date.c
+++ user/date.c
@@ -6,6 +6,16 @@ void main(int argc, char *argv[])
     char buffer[64];
 
     int fd = file_open("rtc", "date");
+
+    if (!fd)
+    {
+
+        file_write_format(FILE_STDOUT, "Could not read rtc/date\n");
+
+        return;
+
+    }
+
     file_read(fd, 64, buffer);
     file_write_format(FILE_STDOUT, "Date: %s\n", buffer);
     file_close(fd);
--- user/timer.c
+++ user/timer.c
@@ -6,6 +6,16 @@ void main(int argc, char *argv[])
     char buffer[32];
 
     int fd = file_open("pit", "jiffies");
+
+    if (!fd)
+    {
+
+        file_write_format(FILE_STDOUT, "Could not read pit/jiffies\n");
+
+        return;
+
+    }
+
     file_read(fd, 32, buffer);
     file_write_format(FILE_STDOUT, "Timer: %s\n", buffer);
     file_close(fd);
