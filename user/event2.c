8d5912f9a1264e2fd9a2dac4efaf377a8d7b093c
jezze <jens.nyberg@gmail.com>
2012-01-03 10:54:58 +0100

    fixed event2

--- user/event2.c
+++ user/event2.c
@@ -1,26 +1,29 @@
 #include <fudge.h>
 
-unsigned int counter;
+static unsigned int counter;
+static unsigned int ticks;
 
 void explain()
 {
 
-    if (counter > 10)
-    {
+    ticks++;
 
-        call_detach(0x00);
-        call_exit();
+    if (ticks < 10)
+        call_wait();
 
-    }
+    ticks = 0;
+    counter++;
 
     if (counter & 1)
         file_write_format(FILE_STDOUT, "Odd:    %d\n", counter);
     else
         file_write_format(FILE_STDOUT, "Even:   %d\n", counter);
 
-    counter++;
+    if (counter < 10)
+        call_wait();
 
-    call_wait();
+    call_detach(0x00);
+    call_exit();
 
 }
 
@@ -28,9 +31,9 @@ void main(int argc, char *argv[])
 {
 
     counter = 0;
+    ticks = 0;
 
     call_attach(0x00, explain);
-
     call_wait();
 
 }
