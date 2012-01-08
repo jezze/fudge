c1b424aa398d606900ccb8953c3f1e2ec2856481
jezze <jens.nyberg@gmail.com>
2012-01-08 07:30:36 +0100

    small changes

--- user/test.c
+++ user/test.c
@@ -5,7 +5,15 @@
 int main(int argc, char *argv[])
 {
 
-    return write(2, "test\n", 5);
+    write(STDOUT_FILENO, "test1\n", 6);
+
+//    fputs("test2\n", stdout);
+
+//    FILE *out = fopen("stdout", "w");
+//    fputs("test3\n", out);
+//    fclose(out);
+
+    return 0;
 
 }
 
