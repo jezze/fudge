c89d0d0ca8cd0ec902e673d5e5e5361958b004d4
jezze <jens.nyberg@gmail.com>
2012-01-22 14:13:17 +0100

    some acpi memory mapping

--- modules/acpi/main.c
+++ modules/acpi/main.c
@@ -1,11 +1,28 @@
 #include <lib/memory.h>
 #include <kernel/log.h>
+#include <kernel/mmu.h>
 #include <modules/acpi/acpi.h>
+
+static struct mmu_memory acpiMemory;
 
 static void write_sdt(struct acpi_sdth *sdt)
 {
 
-    //log_write("[acpi] Sdt: %s\n", sdt->signature);
+    struct mmu_memory *memory = &acpiMemory;
+
+    memory->paddress = (void *)((unsigned int)sdt & 0xFFFFF000);
+    memory->vaddress = (void *)((unsigned int)sdt & 0xFFFFF000);
+    memory->size = 0x4000;
+
+    mmu_map_kernel_memory(memory);
+    mmu_load_memory(0);
+
+    char signature[5];
+    signature[4] = '\0';
+
+    memory_copy(signature, sdt->signature, 4);
+
+    log_write("[acpi] Sdt: %s\n", signature);
 
 }
 
@@ -37,7 +54,7 @@ void init()
     log_write("[acpi] RSDP Address: 0x%x\n", rsdp);
     log_write("[acpi] RSDP Checksum: 0x%x\n", rsdp->checksum);
     log_write("[acpi] RSDP Revision: %d.0\n", rsdp->revision + 1);
-    log_write("[acpi] RSDP OEM: %s\n", rsdp->oem);
+//    log_write("[acpi] RSDP OEM: %s\n", rsdp->oem);
     log_write("[acpi] RSDT Address: 0x%x\n", rsdp->rsdt);
 
     write_sdt((struct acpi_sdth *)rsdp->rsdt);
