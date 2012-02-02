d8b7ff6480d74783ccd9d80361008bc1b1c1bb43
jezze <jens.nyberg@gmail.com>
2012-02-03 00:13:09 +0100

    more rtl work

--- modules/rtl8139/driver.c
+++ modules/rtl8139/driver.c
@@ -1,5 +1,6 @@
 #include <kernel/arch/x86/io.h>
 #include <kernel/irq.h>
+#include <kernel/log.h>
 #include <kernel/modules.h>
 #include <modules/pci/pci.h>
 #include <modules/rtl8139/rtl8139.h>
@@ -57,7 +58,25 @@ static void get_mac(struct rtl8139_driver *driver)
 static void handle_irq(struct modules_device *self)
 {
 
-    log_write("[rtl8139] IRQ handler\n");
+    struct rtl8139_driver *driver = (struct rtl8139_driver *)self->driver;
+
+    unsigned int status = io_inw(driver->io + RTL8139_REGISTER_ISR);
+
+    if (status & RTL8139_ISR_FLAG_ROK)
+    {
+
+        log_write("%x:%x:%x:%x\n", driver->rx[0], driver->rx[1], driver->rx[2], driver->rx[3]);
+
+        io_outw(driver->io + RTL8139_REGISTER_ISR, RTL8139_ISR_FLAG_ROK);
+
+    }
+
+    if (status & RTL8139_ISR_FLAG_TOK)
+    {
+
+        io_outw(driver->io + RTL8139_REGISTER_ISR, RTL8139_ISR_FLAG_TOK);
+
+    }
 
 }
 
@@ -70,7 +89,7 @@ static void rtl8139_driver_start(struct modules_driver *self)
     reset(driver);
     get_mac(driver);
     set_rx(driver, 0x0F);
-    set_interrupt_flags(driver, 0x05);
+    set_interrupt_flags(driver, RTL8139_ISR_FLAG_ROK | RTL8139_ISR_FLAG_TOK);
     enable(driver);
 
     log_write("[rtl8139] Mac: %x:%x:%x:%x:%x:%x\n", driver->mac[0], driver->mac[1], driver->mac[2], driver->mac[3], driver->mac[4], driver->mac[5]);
