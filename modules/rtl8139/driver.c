992138f73ab3cbc6ce4529c91ec959543ed5982c
jezze <jens.nyberg@gmail.com>
2012-02-03 19:15:45 +0100

    added more info

--- modules/rtl8139/driver.c
+++ modules/rtl8139/driver.c
@@ -56,6 +56,27 @@ static void get_mac(struct rtl8139_driver *driver)
 
 }
 
+static void read_datagram_ipv6(unsigned char *data)
+{
+
+    log_write("- IPv6 Version: %x\n", data[0] >> 4);
+    log_write("- IPv6 Length:  %x\n", data[4]);
+    log_write("- IPv6 Source:  %x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x\n", data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22], data[23]);
+    log_write("- IPv6 Dest:    %x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x\n", data[24], data[25], data[26], data[27], data[28], data[29], data[30], data[31], data[32], data[33], data[34], data[35], data[36], data[37], data[38], data[39]);
+
+}
+
+static void read_frame(unsigned char *data)
+{
+
+    log_write("- Frame Dest:   %x:%x:%x:%x:%x:%x\n", data[0], data[1], data[2], data[3], data[4], data[5]);
+    log_write("- Frame Source: %x:%x:%x:%x:%x:%x\n", data[6], data[7], data[8], data[9], data[10], data[11]);
+    log_write("- Frame Type:   %x:%x\n", data[12], data[13]);
+
+    read_datagram_ipv6(data + 14);
+
+}
+
 unsigned short last;
 
 static void read(struct rtl8139_driver *driver)
@@ -71,13 +92,9 @@ static void read(struct rtl8139_driver *driver)
         unsigned short header = *(unsigned short *)&driver->rx[current];
         unsigned short length = *(unsigned short *)&driver->rx[current + 2]; 
 
-        log_write("[rtl8139] Off:0x%x H:0x%x L:0x%x\n", current, header, length);
+        log_write("[rtl8139]   0x%x H:0x%x L:0x%x\n", current, header, length);
 
-        unsigned char *data = driver->rx + current + 4;
-
-        log_write("- Dest: %x:%x:%x:%x:%x:%x\n", data[0], data[1], data[2], data[3], data[4], data[5]);
-        log_write("- Source: %x:%x:%x:%x:%x:%x\n", data[6], data[7], data[8], data[9], data[10], data[11]);
-        log_write("- Type: %x:%x\n", data[12], data[13]);
+        read_frame(driver->rx + current + 4);
 
         current += length + 4;
         current = (current + 3) & ~3;
