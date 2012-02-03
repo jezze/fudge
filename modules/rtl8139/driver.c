61c73de720a8303ff031cfc98cf0df7dcc7a4f67
jezze <jens.nyberg@gmail.com>
2012-02-03 06:17:21 +0100

    more rtl work

--- modules/rtl8139/driver.c
+++ modules/rtl8139/driver.c
@@ -56,34 +56,29 @@ static void get_mac(struct rtl8139_driver *driver)
 
 }
 
-unsigned short cbr_old;
+unsigned short last;
 
 static void read(struct rtl8139_driver *driver)
 {
 
-    unsigned short end_ofs = io_inw(driver->io + RTL8139_REGISTER_CBR);
-    unsigned short read_ofs = cbr_old;
+    unsigned short start = io_inw(driver->io + RTL8139_REGISTER_CAPR);
+    unsigned short end = io_inw(driver->io + RTL8139_REGISTER_CBR);
+    unsigned short current = last;
 
-    while (read_ofs < end_ofs)
+    while (current < end)
     {
 
-        unsigned short hdr = *(unsigned short *)&driver->rx[read_ofs];
-        unsigned short len = *(unsigned short *)&driver->rx[read_ofs + 2]; 
+        unsigned short header = *(unsigned short *)&driver->rx[current];
+        unsigned short length = *(unsigned short *)&driver->rx[current + 2]; 
 
-        log_write("[rtl8139] Off:0x%x Head:0x%x Len:0x%x\n", read_ofs, hdr, len);
+        log_write("[rtl8139] Off:0x%x H:0x%x L:0x%x\n", current, header, length);
 
-        read_ofs += len + 4;
-        read_ofs = (read_ofs + 3) & ~3;
+        current += length + 4;
+        current = (current + 3) & ~3;
 
     }
 
-    cbr_old = read_ofs;
-
-    unsigned short read_ofs2 = io_inw(driver->io + RTL8139_REGISTER_CAPR);
-
-    read_ofs2 = (read_ofs2 + 0x10) & 0xFFFF;
-
-    unsigned short len2 = *(unsigned short *)&driver->rx[read_ofs2 + 2];
+    last = current;
 
 }
 
@@ -126,7 +121,7 @@ static void rtl8139_driver_start(struct modules_driver *self)
 
     struct rtl8139_driver *driver = (struct rtl8139_driver *)self;
 
-    cbr_old = 0;
+    last = 0;
 
     poweron(driver);
     reset(driver);
