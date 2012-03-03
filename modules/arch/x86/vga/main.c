13d329f1d85bb7de78e439a4ca8dd7fbfe857c35
jezze <jens.nyberg@gmail.com>
2012-03-03 22:40:49 +0100

    added vga console node

--- modules/arch/x86/vga/main.c
+++ modules/arch/x86/vga/main.c
@@ -1,13 +1,27 @@
 #include <kernel/modules.h>
+#include <kernel/vfs.h>
+#include <modules/nodefs/nodefs.h>
 #include <modules/vga/vga.h>
 
 static struct vga_driver driver;
+static struct nodefs_node console;
 
 void init()
 {
 
     vga_driver_init(&driver);
     modules_register_driver(&driver.base);
+
+    struct nodefs_driver *nodefs = (struct nodefs_driver *)modules_get_driver(NODEFS_DRIVER_TYPE);
+
+    if (!nodefs)
+        return;
+
+    console.name = "vga/console";
+    console.read = 0;
+    console.write = 0;
+
+    nodefs->register_node(nodefs, &console);
 
 }
 
