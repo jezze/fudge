dffe2f7211773e5f5f0914e5ad937ec507dc2dfb
jezze <jens.nyberg@gmail.com>
2012-04-10 22:50:07 +0200

    started making proper wm

--- user/wm.c
+++ user/wm.c
@@ -1,9 +1,6 @@
 #include <fudge.h>
 #include <ppm.h>
-
-#define XRES 800
-#define YRES 600
-#define BPP 32
+#include "wm.h"
 
 static unsigned int fdlfb;
 static unsigned int fdmx;
@@ -15,7 +12,7 @@ static int my;
 static void draw_pixel(unsigned int x, unsigned int y, unsigned int color)
 {
 
-    unsigned int offset = (y * XRES + x) * 4;
+    unsigned int offset = (y * SCREEN_WIDTH + x) * 4;
 
     file_write(fdlfb, offset, 4, &color);
 
@@ -24,9 +21,33 @@ static void draw_pixel(unsigned int x, unsigned int y, unsigned int color)
 static void draw_buffer(unsigned int x, unsigned int y, unsigned int count, void *buffer)
 {
 
-    unsigned int offset = (y * XRES + x) * 4;
+    unsigned int offset = (y * SCREEN_WIDTH + x) * 4;
 
     file_write(fdlfb, offset, count, buffer);
+
+}
+
+static void draw_stroke(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, unsigned int color)
+{
+
+    unsigned char buffer[0xC80];
+    unsigned int width = x2 - x1 + 1;
+    unsigned int i;
+
+    for (i = 0; i <= width; i++)
+        memory_copy(buffer + i * 4, &color, 4);
+
+    draw_buffer(x1, y1, width * 4, &buffer);
+
+    for (i = y1; i <= y2; i++)
+    {
+
+        draw_pixel(x1, i, color);
+        draw_pixel(x2, i, color);
+
+    }
+
+    draw_buffer(x1, y2, width * 4, &buffer);
 
 }
 
@@ -34,16 +55,13 @@ static void draw_fill(unsigned int x1, unsigned int y1, unsigned int x2, unsigne
 {
 
     unsigned char buffer[0xC80];
-
-    unsigned int width = x2 - x1;
-    unsigned int height = y2 - y1;
-
+    unsigned int width = x2 - x1 + 1;
     unsigned int i;
 
-    for (i = 0; i < width; i++)
+    for (i = 0; i <= width; i++)
         memory_copy(buffer + i * 4, &color, 4);
 
-    for (i = y1; i < y1 + height; i++)
+    for (i = y1; i <= y2; i++)
         draw_buffer(x1, i, width * 4, &buffer);
 
 }
@@ -136,11 +154,11 @@ void set_mouse_coords(unsigned int x, unsigned int y)
     mx = x;
     my = y;
 
-    if (mx > XRES)
-        mx = XRES;
+    if (mx > SCREEN_WIDTH)
+        mx = SCREEN_WIDTH;
 
-    if (my > YRES)
-        my = YRES;
+    if (my > SCREEN_HEIGHT)
+        my = SCREEN_HEIGHT;
 
 }
 
@@ -161,6 +179,36 @@ void mouse_event()
 
 }
 
+void draw_frame(unsigned int x, unsigned int y, unsigned int w, unsigned int h)
+{
+
+    draw_fill(x, y, w, h, FRAME_BACKGROUND);
+    draw_stroke(x + 0, y + 0, w - 0, h - 0, FRAME_BORDER_DARK);
+    draw_stroke(x + 1, y + 1, w - 1, h - 1, FRAME_BORDER_LIGHT);
+    draw_stroke(x + 5, y + 35, w - 5, h - 5, FRAME_BORDER_DARK);
+    draw_stroke(x + 4, y + 34, w - 4, h - 4, FRAME_BORDER_LIGHT);
+
+}
+
+void draw_panel(unsigned int x, unsigned int y, unsigned int w, unsigned int h)
+{
+
+    draw_fill(x, y, w, h, PANEL_BACKGROUND);
+    draw_stroke(x, y, w, h, PANEL_BORDER_DARK);
+    draw_stroke(x + 1, y + 1, w - 1, h - 1, PANEL_BORDER_LIGHT);
+
+}
+
+void draw_window(unsigned int x, unsigned int y, unsigned int w, unsigned int h)
+{
+
+    draw_frame(x, y, w, h);
+    draw_fill(x + 6, y + 36, w - 6, h - 6, WINDOW_BACKGROUND);
+    draw_panel(x + 24, y + 60, w - 24, y + 92);
+    draw_panel(x + 24, y + 92, w - 24, h - 24);
+
+}
+
 void main(int argc, char *argv[])
 {
 
@@ -172,14 +220,15 @@ void main(int argc, char *argv[])
     fdmx = file_open("/module/ps2/mx");
     fdmy = file_open("/module/ps2/my");
 
-    set_xres(XRES);
-    set_yres(YRES);
-    set_bpp(BPP);
+    set_xres(SCREEN_WIDTH);
+    set_yres(SCREEN_HEIGHT);
+    set_bpp(SCREEN_BPP);
     enable();
 
-    set_mouse_coords(XRES / 2, YRES / 2);
+    set_mouse_coords(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
-    draw_fill(0, 0, XRES, YRES, 0x00FFFFFF);
+    draw_fill(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BACKGROUND);
+    draw_window(200, 100, 600, 500);
 
     call_attach(0x0C, mouse_event);
     call_wait();
