b0e24b97da309d775520fea890cb46afa315cf45
jezze <jens.nyberg@gmail.com>
2012-05-06 19:01:27 +0200

    some init improvements

--- user/init.c
+++ user/init.c
@@ -1,10 +1,7 @@
 #include <fudge.h>
 
-void main(int argc, char *argv[])
+void load_modules()
 {
-
-    call_close(FILE_STDIN);
-    call_close(FILE_STDOUT);
 
     unsigned int id = call_open(3, "/ramdisk/bin/load");
 
@@ -35,14 +32,19 @@ void main(int argc, char *argv[])
     call_open(FILE_STDIN, "/ramdisk/mod/bga.ko");
     call_execute(id, 0, 0);
 
+    call_close(FILE_STDIN);
     call_close(id);
 
-    call_close(FILE_STDIN);
-    call_close(FILE_STDOUT);
+}
 
-    unsigned int id2 = call_open(3, "/ramdisk/bin/shell");
-    call_execute(id2, 0, 0);
-    call_close(id2);
+void main(int argc, char *argv[])
+{
+
+    load_modules();
+
+    unsigned int id = call_open(3, "/ramdisk/bin/shell");
+    call_execute(id, 0, 0);
+    call_close(id);
 
     for (;;);
 
