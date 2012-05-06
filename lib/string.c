45cb4e9e22c93c6bac5e403070801caff3783cbc
jezze <jens.nyberg@gmail.com>
2012-05-07 01:14:50 +0200

    some optimizations

--- lib/string.c
+++ lib/string.c
@@ -18,14 +18,14 @@ unsigned int string_read_num(const char *in, unsigned int base)
     const char *ip = in;
     int num = 0;
 
-    while ((*ip >= '0' && *ip <= '9') || (*ip >= 'a' && *ip <= 'f'))
+    while ((*ip >= '0' && *ip <= '9') || (base > 10 && (*ip >= 'a' && *ip <= 'f')))
     {
 
-        if ((*ip >= '0' && *ip <= '9'))
-            num = num * base + *ip++ - '0';
+        unsigned int offset = (*ip > '9') ? *ip - 'a' + 10 : *ip - '0';
 
-        if ((*ip >= 'a' && *ip <= 'f'))
-            num = num * base + *ip++ - 'a' + 10;
+        num = num * base + offset;
+
+        ip++;
 
     }
 
