907403bf55d06335a52a5bb6e3bf8ffe8f886c70
jezze <jens.nyberg@gmail.com>
2012-05-06 18:19:25 +0200

    modified init script to execute load

--- user/init.c
+++ user/init.c
@@ -4,49 +4,54 @@ void main(int argc, char *argv[])
 {
 
     unsigned int id;
+    unsigned int id2;
+
+    call_close(FILE_STDIN);
+    call_close(FILE_STDOUT);
+
+    id = call_open("/ramdisk/bin/load");
+    id2 = call_open("/ramdisk/bin/load");
+    call_close(id);
 
     id = call_open("/ramdisk/mod/nodefs.ko");
-    call_load(id);
+    call_execute(id2, 0, 0);
     call_close(id);
 
     id = call_open("/ramdisk/mod/uart.ko");
-    call_load(id);
+    call_execute(id2, 0, 0);
     call_close(id);
 
     id = call_open("/ramdisk/mod/vga.ko");
-    call_load(id);
+    call_execute(id2, 0, 0);
     call_close(id);
 
     id = call_open("/ramdisk/mod/pci.ko");
-    call_load(id);
+    call_execute(id2, 0, 0);
     call_close(id);
 
     id = call_open("/ramdisk/mod/pit.ko");
-    call_load(id);
+    call_execute(id2, 0, 0);
     call_close(id);
 
     id = call_open("/ramdisk/mod/rtc.ko");
-    call_load(id);
+    call_execute(id2, 0, 0);
     call_close(id);
 
     id = call_open("/ramdisk/mod/ps2.ko");
-    call_load(id);
+    call_execute(id2, 0, 0);
     call_close(id);
 
     id = call_open("/ramdisk/mod/tty.ko");
-    call_load(id);
+    call_execute(id2, 0, 0);
+    call_close(id);
+
+    id = call_open("/ramdisk/mod/bga.ko");
+    call_execute(id2, 0, 0);
     call_close(id);
 
     call_close(FILE_STDIN);
     call_close(FILE_STDOUT);
-
-    call_open("/ramdisk/mod/bga.ko");
-    id = call_open("/ramdisk/bin/load");
-    call_execute(id, 0, 0);
-    call_close(id);
-
-    call_close(FILE_STDIN);
-    call_close(FILE_STDOUT);
+    call_close(id2);
 
     id = call_open("/ramdisk/bin/shell");
     call_execute(id, 0, 0);
