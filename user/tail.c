087b0799d829fe1f19eccf3d25361dce04724ea1
jezze <jens.nyberg@gmail.com>
2012-05-07 01:49:52 +0200

    some minor code

--- user/tail.c
+++ user/tail.c
@@ -3,5 +3,19 @@
 void main(int argc, char *argv[])
 {
 
+    char buffer[128];
+    unsigned int count = 0;
+
+    while (count < 8)
+    {
+
+        unsigned int length = call_read(FILE_STDIN, 0, 8, buffer + count);
+
+        call_write(FILE_STDOUT, 0, length, buffer + count);
+
+        count += length;
+
+    }
+
 }
 
