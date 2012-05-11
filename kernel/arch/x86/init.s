820fe94d8df7ddcf3cc907a1e7c48fb9889c8d83
jezze <jens.nyberg@gmail.com>
2012-05-12 00:39:27 +0200

    some init cleanups

--- kernel/arch/x86/init.s
+++ kernel/arch/x86/init.s
@@ -1,8 +1,5 @@
-INIT_MBOOT_PAGE_ALIGN   equ 1 << 0
-INIT_MBOOT_MEMORY_INFO  equ 1 << 1
-INIT_MBOOT_VIDEO_INFO   equ 1 << 2
 INIT_MBOOT_HEADER_MAGIC equ 0x1BADB002
-INIT_MBOOT_HEADER_FLAGS equ INIT_MBOOT_PAGE_ALIGN | INIT_MBOOT_MEMORY_INFO | INIT_MBOOT_VIDEO_INFO
+INIT_MBOOT_HEADER_FLAGS equ 0x00000001
 INIT_MBOOT_CHECKSUM     equ - (INIT_MBOOT_HEADER_MAGIC + INIT_MBOOT_HEADER_FLAGS)
 INIT_STACK_SIZE         equ 0x8000
 
@@ -13,19 +10,9 @@ extern kernel_init
 section .mboot
 align 4
 
-init_mboot:
-    dd INIT_MBOOT_HEADER_MAGIC
-    dd INIT_MBOOT_HEADER_FLAGS
-    dd INIT_MBOOT_CHECKSUM
-    dd 0
-    dd 0
-    dd 0
-    dd 0
-    dd 0
-    dd 1
-    dd 320
-    dd 200
-    dd 8
+dd INIT_MBOOT_HEADER_MAGIC
+dd INIT_MBOOT_HEADER_FLAGS
+dd INIT_MBOOT_CHECKSUM
 
 section .text
 align 4
@@ -33,7 +20,7 @@ align 4
 global init
 init:
     cli
-    mov esp, init_stack + INIT_STACK_SIZE
+    mov esp, stack + INIT_STACK_SIZE
     push esp
     push eax
     push ebx
@@ -46,6 +33,5 @@ init:
 section .bss
 align 4
 
-global init_stack
-init_stack:
+stack:
     resb INIT_STACK_SIZE
