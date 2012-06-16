e2df03a38ac341ded2773a2bee1f2fa8069cea19
jezze <jens.nyberg@gmail.com>
2012-06-16 17:14:36 +0200

    finally solved the order problem

--- kernel/rules.mk
+++ kernel/rules.mk
@@ -1,11 +1,11 @@
 include kernel/arch/$(ARCH)/rules.mk
 include kernel/vfs/rules.mk
 
-KERNELOBJ+=kernel/error.o kernel/event.o kernel/ramdisk.o kernel/irq.o kernel/kernel.o kernel/mmu.o kernel/modules.o kernel/runtime.o kernel/syscall.o
+KERNELOBJ+=kernel/error.o kernel/event.o kernel/irq.o kernel/kernel.o kernel/mmu.o kernel/modules.o kernel/ramdisk.o kernel/runtime.o kernel/syscall.o
 CLEAN+=$(KERNELOBJ) kernel/fudge
 
 kernel/%.o: GCCFLAGS+=-Iinclude
 kernel/%: LDFLAGS+=-Tkernel/arch/$(ARCH)/linker.ld
 
 kernel/fudge: $(KERNELOBJ) lib/libfudge.a
-	$(LD) $(LDFLAGS) -o $@ kernel/*.o kernel/arch/$(ARCH)/*.o kernel/vfs/*.o lib/libfudge.a
+	$(LD) $(LDFLAGS) -o $@ $^
