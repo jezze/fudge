02894d34125fd6524acb3c9a9b470d8b86c68d76
jezze <jens.nyberg@gmail.com>
2012-06-17 22:49:33 +0200

    more fixes

--- packages/base/load.c
+++ packages/base/load.c
@@ -4,17 +4,16 @@
 unsigned int find(char *name)
 {
 
-    char headerBuffer[0x1000];
-    char sectionBuffer[0x1000];
+    struct elf_header header;
+    char sectionBuffer[0x320];
     char symbolBuffer[0x2000];
-    char stringTable[0x2000];
-    struct elf_header *header = (struct elf_header *)headerBuffer;
+    char stringTable[0x1000];
     struct elf_section_header *sectionHeader = (struct elf_section_header *)sectionBuffer;
     struct elf_symbol *symbolTable = (struct elf_symbol *)symbolBuffer;
     unsigned int id = call_open(FILE_NEW, "/ramdisk/boot/fudge");
 
-    call_read(id, 0, sizeof (struct elf_header), headerBuffer);
-    call_read(id, header->shoffset, header->shsize * header->shcount, sectionBuffer);
+    call_read(id, 0, sizeof (struct elf_header), &header);
+    call_read(id, header.shoffset, header.shsize * header.shcount, sectionBuffer);
     call_read(id, sectionHeader[7].offset, sectionHeader[7].size, symbolBuffer);
     call_read(id, sectionHeader[sectionHeader[7].link].offset, sectionHeader[sectionHeader[7].link].size, stringTable);
     call_close(id);
