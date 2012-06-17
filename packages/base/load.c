7747fe6cab75ceaadd3ca7477bcdb1fabd5ca883
jezze <jens.nyberg@gmail.com>
2012-06-17 22:10:58 +0200

    some fixes

--- packages/base/load.c
+++ packages/base/load.c
@@ -4,34 +4,34 @@
 #define BUFFER_SIZE 0x8000
 
 static char buffer[BUFFER_SIZE];
-static char symbolBuffer[0x10000];
+static char symbolBuffer[0x2000];
+static char symBuffer[0x2000];
+static char strBuffer[0x2000];
 
 unsigned int find(char *name)
 {
 
     struct elf_header *header = (struct elf_header *)symbolBuffer;
-    unsigned int address = (unsigned int)header;
-    struct elf_section_header *sheader = (struct elf_section_header *)(address + header->shoffset);
-    struct elf_section_header *symHeader = &sheader[7];
-    struct elf_symbol *symTable = (struct elf_symbol *)(address + symHeader->offset);
-    char *strTable = (char *)(address + sheader[symHeader->link].offset);
+    struct elf_section_header *sheader = (struct elf_section_header *)symbolBuffer;
+    struct elf_symbol *symTable = (struct elf_symbol *)symBuffer;
+    unsigned int id = call_open(FILE_NEW, "/ramdisk/boot/fudge");
 
-    return elf_search_table(symTable, symHeader->size / symHeader->esize, strTable, name);
+    call_read(id, 0, 0x2000, symbolBuffer);
+    call_read(id, header->shoffset, 0x2000, symbolBuffer);
+    call_read(id, sheader[7].offset, 0x2000, symBuffer);
+    call_read(id, sheader[sheader[7].link].offset, 0x2000, strBuffer);
+    call_close(id);
+
+    return elf_search_table(symTable, sheader[7].size / sheader[7].esize, strBuffer, name);
 
 }
 
 void main()
 {
 
-    unsigned int id;
-    unsigned int count;
     struct elf_header *header = (struct elf_header *)buffer;
+    unsigned int count = call_read(FILE_STDIN, 0, BUFFER_SIZE, buffer);
 
-    id = call_open(FILE_NEW, "/ramdisk/boot/fudge");
-    call_read(id, 0, 0x10000, symbolBuffer);
-    call_close(id);
-
-    count = call_read(FILE_STDIN, 0, BUFFER_SIZE, buffer);
     elf_symbolize(header, find);
     call_write(FILE_STDIN, 0, count, buffer);
     call_load(FILE_STDIN);
