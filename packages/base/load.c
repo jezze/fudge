9ef7250c83701f20ff4d30869a940406af53cdd9
jezze <jens.nyberg@gmail.com>
2012-06-17 07:09:46 +0200

    some cleanups

--- packages/base/load.c
+++ packages/base/load.c
@@ -5,7 +5,6 @@
 
 static char buffer[BUFFER_SIZE];
 static char symbolBuffer[0x10000];
-static unsigned int symbolCount;
 
 unsigned int find(char *name)
 {
@@ -16,24 +15,16 @@ unsigned int find(char *name)
 
 }
 
-void parse()
-{
-
-    unsigned int id;
-
-    id = call_open(FILE_NEW, "/ramdisk/boot/fudge");
-    symbolCount = call_read(id, 0, 0x10000, symbolBuffer);
-    call_close(id);
-
-}
-
 void main()
 {
 
+    unsigned int id;
     unsigned int count;
     struct elf_header *header = (struct elf_header *)buffer;
 
-    parse();
+    id = call_open(FILE_NEW, "/ramdisk/boot/fudge");
+    call_read(id, 0, 0x10000, symbolBuffer);
+    call_close(id);
 
     count = call_read(FILE_STDIN, 0, BUFFER_SIZE, buffer);
     elf_symbolize(header, find);
