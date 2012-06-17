5b2420eddf9a5c0e3b7068f0d6ffb58970bd9a1f
jezze <jens.nyberg@gmail.com>
2012-06-17 22:27:18 +0200

    minor fixes

--- packages/base/load.c
+++ packages/base/load.c
@@ -1,39 +1,36 @@
 #include <fudge.h>
 #include <elf.h>
 
-#define BUFFER_SIZE 0x8000
-
-static char buffer[BUFFER_SIZE];
-static char symbolBuffer[0x1000];
-static char symBuffer[0x2000];
-static char strBuffer[0x2000];
-
 unsigned int find(char *name)
 {
 
-    struct elf_header *header = (struct elf_header *)symbolBuffer;
-    struct elf_section_header *sheader = (struct elf_section_header *)symbolBuffer;
-    struct elf_symbol *symTable = (struct elf_symbol *)symBuffer;
+    char sectionBuffer[0x1000];
+    char symbolBuffer[0x2000];
+    char strTable[0x2000];
+    struct elf_header *header = (struct elf_header *)sectionBuffer;
+    struct elf_section_header *sectionHeader = (struct elf_section_header *)sectionBuffer;
+    struct elf_symbol *symTable = (struct elf_symbol *)symbolBuffer;
     unsigned int id = call_open(FILE_NEW, "/ramdisk/boot/fudge");
 
-    call_read(id, 0, sizeof (struct elf_header), symbolBuffer);
-    call_read(id, header->shoffset, header->shsize * header->shcount, symbolBuffer);
-    call_read(id, sheader[7].offset, sheader[7].size, symBuffer);
-    call_read(id, sheader[sheader[7].link].offset, sheader[sheader[7].link].size, strBuffer);
+    call_read(id, 0, sizeof (struct elf_header), sectionBuffer);
+    call_read(id, header->shoffset, header->shsize * header->shcount, sectionBuffer);
+    call_read(id, sectionHeader[7].offset, sectionHeader[7].size, symbolBuffer);
+    call_read(id, sectionHeader[sectionHeader[7].link].offset, sectionHeader[sectionHeader[7].link].size, strTable);
     call_close(id);
 
-    return elf_search_table(symTable, sheader[7].size / sheader[7].esize, strBuffer, name);
+    return elf_search_table(symTable, sectionHeader[7].size / sectionHeader[7].esize, strTable, name);
 
 }
 
 void main()
 {
 
-    struct elf_header *header = (struct elf_header *)buffer;
-    unsigned int count = call_read(FILE_STDIN, 0, BUFFER_SIZE, buffer);
+    char moduleBuffer[0x8000];
+    struct elf_header *header = (struct elf_header *)moduleBuffer;
+    unsigned int count = call_read(FILE_STDIN, 0, 0x8000, moduleBuffer);
 
     elf_symbolize(header, find);
-    call_write(FILE_STDIN, 0, count, buffer);
+    call_write(FILE_STDIN, 0, count, moduleBuffer);
     call_load(FILE_STDIN);
 
 }
