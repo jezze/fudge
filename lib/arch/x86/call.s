f45b8fb71336a9e21bf7cc1c31eb9b35bbd24bd8
jezze <jens.nyberg@gmail.com>
2012-06-25 02:56:00 +0200

    added some defines in call

--- lib/arch/x86/call.s
+++ lib/arch/x86/call.s
@@ -1,68 +1,81 @@
 .intel_syntax noprefix
 
+.set CALL_INTERRUPT,     0x80
+.set CALL_INDEX_OPEN,    0x01
+.set CALL_INDEX_CLOSE,   0x02
+.set CALL_INDEX_READ,    0x03
+.set CALL_INDEX_WRITE,   0x04
+.set CALL_INDEX_EXECUTE, 0x05
+.set CALL_INDEX_EXIT,    0x06
+.set CALL_INDEX_WAIT,    0x07
+.set CALL_INDEX_LOAD,    0x08
+.set CALL_INDEX_UNLOAD,  0x09
+.set CALL_INDEX_ATTACH,  0x0A
+.set CALL_INDEX_DETACH,  0x0B
+
 .global call_open
 call_open:
-    mov eax, 0x01
-    int 0x80
+    mov eax, CALL_INDEX_OPEN
+    int CALL_INTERRUPT
     ret
 
 .global call_close
 call_close:
-    mov eax, 0x02
-    int 0x80
+    mov eax, CALL_INDEX_CLOSE
+    int CALL_INTERRUPT
     ret
 
 .global call_read
 call_read:
-    mov eax, 0x03
-    int 0x80
+    mov eax, CALL_INDEX_READ
+    int CALL_INTERRUPT
     ret
 
 .global call_write
 call_write:
-    mov eax, 0x04
-    int 0x80
+    mov eax, CALL_INDEX_WRITE
+    int CALL_INTERRUPT
     ret
 
 .global call_execute
 call_execute:
-    mov eax, 0x05
-    int 0x80
+    mov eax, CALL_INDEX_EXECUTE
+    int CALL_INTERRUPT
     ret
 
 .global call_exit
 call_exit:
-    mov eax, 0x06
-    int 0x80
+    mov eax, CALL_INDEX_EXIT
+    int CALL_INTERRUPT
     ret
 
 .global call_wait
 call_wait:
-    mov eax, 0x07
-    int 0x80
+    mov eax, CALL_INDEX_WAIT
+    int CALL_INTERRUPT
     ret
 
 .global call_load
 call_load:
-    mov eax, 0x08
-    int 0x80
+    mov eax, CALL_INDEX_LOAD
+    int CALL_INTERRUPT
     ret
 
 .global call_unload
 call_unload:
-    mov eax, 0x09
-    int 0x80
+    mov eax, CALL_INDEX_UNLOAD
+    int CALL_INTERRUPT
     ret
 
 .global call_attach
 call_attach:
-    mov eax, 0x0A
-    int 0x80
+    mov eax, CALL_INDEX_ATTACH
+    int CALL_INTERRUPT
     ret
 
 .global call_detach
 call_detach:
-    mov eax, 0x0B
-    int 0x80
+    mov eax, CALL_INDEX_DETACH
+    int CALL_INTERRUPT
     ret
 
