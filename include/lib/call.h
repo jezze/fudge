9f75341bd78051e20fb47e03e795d4404a6e5661
jezze <jens.nyberg@gmail.com>
2012-06-25 02:50:13 +0200

    forgot to remove some stuff

--- include/lib/call.h
+++ include/lib/call.h
@@ -6,14 +6,12 @@ void call_close(unsigned int id);
 unsigned int call_read(unsigned int id, unsigned int offset, unsigned int count, void *buffer);
 unsigned int call_write(unsigned int id, unsigned int offset, unsigned int count, void *buffer);
 unsigned int call_execute(unsigned int id);
-unsigned int call_load(unsigned int id);
-unsigned int call_unload(unsigned int id);
 unsigned int call_exit();
 unsigned int call_wait();
+unsigned int call_load(unsigned int id);
+unsigned int call_unload(unsigned int id);
 unsigned int call_attach(unsigned int id, void (*callback)());
 unsigned int call_detach(unsigned int id);
-void call_halt();
-void call_reboot();
 
 #endif
 
