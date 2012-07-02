da7375c0693af6b2f7828ee19bef7fba551c21e6
jezze <jens.nyberg@gmail.com>
2012-07-03 01:26:47 +0200

    minor cleanups

--- kernel/rules.mk
+++ kernel/rules.mk
@@ -2,6 +2,7 @@ include kernel/src/arch/$(ARCH).mk
 
 KERNELOBJ+=kernel/src/error.o kernel/src/event.o kernel/src/kernel.o kernel/src/mmu.o kernel/src/modules.o kernel/src/ramdisk.o kernel/src/runtime.o kernel/src/syscall.o
 KERNELOBJ+=kernel/src/vfs/ramdisk.o kernel/src/vfs/root.o kernel/src/vfs/sys.o
+
 CLEAN+=$(KERNELOBJ) kernel/fudge
 
 kernel/%.o: CCFLAGS+=-Ilib/include -Ikernel/include
--- lib/rules.mk
+++ lib/rules.mk
@@ -1,6 +1,7 @@
 include lib/src/arch/$(ARCH).mk
 
 LIBOBJ+=lib/src/elf.o lib/src/flake.o lib/src/memory.o lib/src/string.o
+
 CLEAN+=$(LIBOBJ) lib/libfudge.a
 
 lib/%.o: CCFLAGS+=-Ilib/include
