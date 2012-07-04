bdf92a7afcbcae513344eeaa0615e96bd50decbf
jezze <jens.nyberg@gmail.com>
2012-07-04 22:06:29 +0200

    added volatile on io

--- lib/include/io.h
+++ lib/include/io.h
@@ -1,9 +1,9 @@
 #ifndef LIB_IO_H
 #define LIB_IO_H
 
-unsigned char io_inb(unsigned short port);
-unsigned short io_inw(unsigned short port);
-unsigned int io_ind(unsigned short port);
+volatile unsigned char io_inb(unsigned short port);
+volatile unsigned short io_inw(unsigned short port);
+volatile unsigned int io_ind(unsigned short port);
 void io_outb(unsigned short port, unsigned char data);
 void io_outw(unsigned short port, unsigned short data);
 void io_outd(unsigned short port, unsigned int data);
