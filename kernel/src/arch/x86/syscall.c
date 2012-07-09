ed6a5470126d3f71a2e4fb2d83dbd300bf81ba73
jezze <jens.nyberg@gmail.com>
2012-07-09 22:24:10 +0200

    removed some stuff

--- kernel/src/arch/x86/syscall.c
+++ kernel/src/arch/x86/syscall.c
@@ -4,108 +4,9 @@
 #include <arch/x86/isr.h>
 #include <arch/x86/syscall.h>
 
-static unsigned int (*routines[SYSCALL_TABLE_SLOTS])(struct runtime_task *task);
+static unsigned int (*routines[SYSCALL_TABLE_SLOTS])(struct runtime_task *task, void *stack);
 
-static unsigned int handle_attach(struct runtime_task *task)
-{
-
-    struct syscall_attach_args *args = (struct syscall_attach_args *)task->registers.sp;
-
-    return syscall_attach(task, args);
-
-}
-
-static unsigned int handle_close(struct runtime_task *task)
-{
-
-    struct syscall_close_args *args = (struct syscall_close_args *)task->registers.sp;
-
-    return syscall_close(task, args);
-
-}
-
-static unsigned int handle_detach(struct runtime_task *task)
-{
-
-    struct syscall_detach_args *args = (struct syscall_detach_args *)task->registers.sp;
-
-    return syscall_detach(task, args);
-
-}
-
-static unsigned int handle_execute(struct runtime_task *task)
-{
-
-    struct syscall_execute_args *args = (struct syscall_execute_args *)task->registers.sp;
-
-    return syscall_execute(task, args);
-
-}
-
-static unsigned int handle_exit(struct runtime_task *task)
-{
-
-    struct syscall_exit_args *args = (struct syscall_exit_args *)task->registers.sp;
-
-    return syscall_exit(task, args);
-
-}
-
-static unsigned int handle_load(struct runtime_task *task)
-{
-
-    struct syscall_load_args *args = (struct syscall_load_args *)task->registers.sp;
-
-    return syscall_load(task, args);
-
-}
-
-static unsigned int handle_open(struct runtime_task *task)
-{
-
-    struct syscall_open_args *args = (struct syscall_open_args *)task->registers.sp;
-
-    return syscall_open(task, args);
-
-}
-
-static unsigned int handle_read(struct runtime_task *task)
-{
-
-    struct syscall_read_args *args = (struct syscall_read_args *)task->registers.sp;
-
-    return syscall_read(task, args);
-
-}
-
-static unsigned int handle_unload(struct runtime_task *task)
-{
-
-    struct syscall_unload_args *args = (struct syscall_unload_args *)task->registers.sp;
-
-    return syscall_unload(task, args);
-
-}
-
-static unsigned int handle_wait(struct runtime_task *task)
-{
-
-    struct syscall_wait_args *args = (struct syscall_wait_args *)task->registers.sp;
-
-    return syscall_wait(task, args);
-
-}
-
-static unsigned int handle_write(struct runtime_task *task)
-{
-
-    struct syscall_write_args *args = (struct syscall_write_args *)task->registers.sp;
-
-    return syscall_write(task, args);
-
-}
-
-static void register_routine(unsigned char index, unsigned int (*routine)(struct runtime_task *task))
+static void register_routine(unsigned int index, unsigned int (*routine)(struct runtime_task *task, void *stack))
 {
 
     routines[index] = routine;
@@ -124,24 +25,24 @@ static void handle_interrupt(struct runtime_task *task, struct isr_cpu_registers
 
     }
 
-    registers->general.eax = routines[registers->error](task);
+    registers->general.eax = routines[registers->error](task, (void *)task->registers.sp);
 
 }
 
 void syscall_setup()
 {
 
-    register_routine(SYSCALL_INDEX_OPEN, handle_open);
-    register_routine(SYSCALL_INDEX_CLOSE, handle_close);
-    register_routine(SYSCALL_INDEX_READ, handle_read);
-    register_routine(SYSCALL_INDEX_WRITE, handle_write);
-    register_routine(SYSCALL_INDEX_EXECUTE, handle_execute);
-    register_routine(SYSCALL_INDEX_EXIT, handle_exit);
-    register_routine(SYSCALL_INDEX_WAIT, handle_wait);
-    register_routine(SYSCALL_INDEX_LOAD, handle_load);
-    register_routine(SYSCALL_INDEX_UNLOAD, handle_unload);
-    register_routine(SYSCALL_INDEX_ATTACH, handle_attach);
-    register_routine(SYSCALL_INDEX_DETACH, handle_detach);
+    register_routine(SYSCALL_INDEX_OPEN, syscall_open);
+    register_routine(SYSCALL_INDEX_CLOSE, syscall_close);
+    register_routine(SYSCALL_INDEX_READ, syscall_read);
+    register_routine(SYSCALL_INDEX_WRITE, syscall_write);
+    register_routine(SYSCALL_INDEX_EXECUTE, syscall_execute);
+    register_routine(SYSCALL_INDEX_EXIT, syscall_exit);
+    register_routine(SYSCALL_INDEX_WAIT, syscall_wait);
+    register_routine(SYSCALL_INDEX_LOAD, syscall_load);
+    register_routine(SYSCALL_INDEX_UNLOAD, syscall_unload);
+    register_routine(SYSCALL_INDEX_ATTACH, syscall_attach);
+    register_routine(SYSCALL_INDEX_DETACH, syscall_detach);
 
     isr_register_routine(ISR_INDEX_SYSCALL, handle_interrupt);
 
