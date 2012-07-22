d70dd50d19facf50f04faaa1c76b552f328fdc4c
Jezze <jens.nyberg@gmail.com>
2012-07-22 02:36:57 +0200

    some module makefile fixes

--- modules/rules.mk
+++ modules/rules.mk
@@ -1,32 +1,36 @@
 include modules/src/arch/rules.$(ARCH).mk
 
-MODULES+=modules/src/ext2/ext2.ko
-MODULES+=modules/src/log/log.ko
-MODULES+=modules/src/mbr/mbr.ko
-MODULES+=modules/src/nodefs/nodefs.ko
-MODULES+=modules/src/tty/tty.ko
+MODULE_EXT2=modules/src/ext2/ext2.ko
+MODULE_LOG=modules/src/log/log.ko
+MODULE_MBR=modules/src/mbr/mbr.ko
+MODULE_NODEFS=modules/src/nodefs/nodefs.ko
+MODULE_TTY=modules/src/tty/tty.ko
 
-MODULESOBJ+=modules/src/ext2/main.o modules/src/ext2/driver.o modules/src/ext2/filesystem.o
-MODULESOBJ+=modules/src/log/main.o modules/src/log/driver.o
-MODULESOBJ+=modules/src/mbr/main.o modules/src/mbr/driver.o modules/src/mbr/device.o
-MODULESOBJ+=modules/src/nodefs/main.o modules/src/nodefs/driver.o
-MODULESOBJ+=modules/src/tty/main.o modules/src/tty/driver.o
+MODULES+=$(MODULE_EXT2) $(MODULE_LOG) $(MODULE_MBR) $(MODULE_NODEFS) $(MODULE_TTY)
 
-CLEAN+=$(MODULESOBJ) $(MODULES)
+OBJECTS_EXT2=modules/src/ext2/main.o modules/src/ext2/driver.o modules/src/ext2/filesystem.o
+OBJECTS_LOG=modules/src/log/main.o modules/src/log/driver.o
+OBJECTS_MBR=modules/src/mbr/main.o modules/src/mbr/driver.o modules/src/mbr/device.o
+OBJECTS_NODEFS=modules/src/nodefs/main.o modules/src/nodefs/driver.o
+OBJECTS_TTY=modules/src/tty/main.o modules/src/tty/driver.o
 
-modules/src/log/log.ko: lib/src/memory.o lib/src/string.o modules/src/log/main.o modules/src/log/driver.o
+OBJECTS+=$(OBJECTS_EXT2) $(OBJECTS_LOG) $(OBJECTS_MBR) $(OBJECTS_NODEFS) $(OBJECTS_TTY)
+
+CLEAN+=$(MODULES) $(OBJECTS)
+
+modules/src/log/log.ko: lib/src/memory.o lib/src/string.o $(OBJECTS_LOG)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/ext2/ext2.ko: lib/src/memory.o lib/src/string.o modules/src/ext2/main.o modules/src/ext2/driver.o modules/src/ext2/filesystem.o
+modules/src/ext2/ext2.ko: lib/src/memory.o lib/src/string.o $(OBJECTS_EXT2)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/mbr/mbr.ko: lib/src/memory.o lib/src/string.o modules/src/mbr/main.o modules/src/mbr/driver.o modules/src/mbr/device.o
+modules/src/mbr/mbr.ko: lib/src/memory.o lib/src/string.o $(OBJECTS_MBR)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/nodefs/nodefs.ko: lib/src/memory.o lib/src/string.o modules/src/nodefs/main.o modules/src/nodefs/driver.o
+modules/src/nodefs/nodefs.ko: lib/src/memory.o lib/src/string.o $(OBJECTS_NODEFS)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/tty/tty.ko: lib/src/memory.o lib/src/string.o modules/src/tty/main.o modules/src/tty/driver.o
+modules/src/tty/tty.ko: lib/src/memory.o lib/src/string.o $(OBJECTS_TTY)
 	$(LD) $(LDFLAGS) -o $@ $^
 
 modules/%.ko: CCFLAGS+=-Ilib/include -Ikernel/include -Imodules/include
--- modules/src/arch/rules.x86.mk
+++ modules/src/arch/rules.x86.mk
@@ -1,81 +1,85 @@
-MODULES+=modules/src/arch/x86/acpi/acpi.ko
-MODULES+=modules/src/arch/x86/apic/apic.ko
-MODULES+=modules/src/arch/x86/ata/ata.ko
-MODULES+=modules/src/arch/x86/bga/bga.ko
-MODULES+=modules/src/arch/x86/cpuid/cpuid.ko
-MODULES+=modules/src/arch/x86/fpu/fpu.ko
-MODULES+=modules/src/arch/x86/i915/i915.ko
-MODULES+=modules/src/arch/x86/msr/msr.ko
-MODULES+=modules/src/arch/x86/pci/pci.ko
-MODULES+=modules/src/arch/x86/pit/pit.ko
-MODULES+=modules/src/arch/x86/ps2/ps2.ko
-MODULES+=modules/src/arch/x86/rtc/rtc.ko
-MODULES+=modules/src/arch/x86/rtl8139/rtl8139.ko
-MODULES+=modules/src/arch/x86/smp/smp.ko
-MODULES+=modules/src/arch/x86/uart/uart.ko
-MODULES+=modules/src/arch/x86/vga/vga.ko
+MODULE_ACPI=modules/src/arch/x86/acpi/acpi.ko
+MODULE_APIC=modules/src/arch/x86/apic/apic.ko
+MODULE_ATA=modules/src/arch/x86/ata/ata.ko
+MODULE_BGA=modules/src/arch/x86/bga/bga.ko
+MODULE_CPUID=modules/src/arch/x86/cpuid/cpuid.ko
+MODULE_FPU=modules/src/arch/x86/fpu/fpu.ko
+MODULE_I915=modules/src/arch/x86/i915/i915.ko
+MODULE_MSR=modules/src/arch/x86/msr/msr.ko
+MODULE_PCI=modules/src/arch/x86/pci/pci.ko
+MODULE_PIT=modules/src/arch/x86/pit/pit.ko
+MODULE_PS2=modules/src/arch/x86/ps2/ps2.ko
+MODULE_RTC=modules/src/arch/x86/rtc/rtc.ko
+MODULE_RTL8139=modules/src/arch/x86/rtl8139/rtl8139.ko
+MODULE_SMP=modules/src/arch/x86/smp/smp.ko
+MODULE_UART=modules/src/arch/x86/uart/uart.ko
+MODULE_VGA=modules/src/arch/x86/vga/vga.ko
 
-MODULESOBJ+=modules/src/arch/x86/acpi/main.o modules/src/arch/x86/acpi/driver.o
-MODULESOBJ+=modules/src/arch/x86/apic/main.o modules/src/arch/x86/apic/driver.o
-MODULESOBJ+=modules/src/arch/x86/ata/main.o modules/src/arch/x86/ata/bus.o modules/src/arch/x86/ata/device.o
-MODULESOBJ+=modules/src/arch/x86/bga/main.o modules/src/arch/x86/bga/driver.o
-MODULESOBJ+=modules/src/arch/x86/cpuid/main.o modules/src/arch/x86/cpuid/driver.o modules/src/arch/x86/cpuid/cpuid.o
-MODULESOBJ+=modules/src/arch/x86/fpu/main.o modules/src/arch/x86/fpu/driver.o modules/src/arch/x86/fpu/fpu.o
-MODULESOBJ+=modules/src/arch/x86/i915/main.o modules/src/arch/x86/i915/driver.o
-MODULESOBJ+=modules/src/arch/x86/msr/main.o modules/src/arch/x86/msr/driver.o modules/src/arch/x86/msr/msr.o
-MODULESOBJ+=modules/src/arch/x86/pci/main.o modules/src/arch/x86/pci/bus.o modules/src/arch/x86/pci/device.o
-MODULESOBJ+=modules/src/arch/x86/pit/main.o modules/src/arch/x86/pit/driver.o modules/src/arch/x86/pit/device.o
-MODULESOBJ+=modules/src/arch/x86/ps2/main.o modules/src/arch/x86/ps2/kbd_driver.o modules/src/arch/x86/ps2/kbd_device.o modules/src/arch/x86/ps2/mouse_driver.o modules/src/arch/x86/ps2/mouse_device.o
-MODULESOBJ+=modules/src/arch/x86/rtc/main.o modules/src/arch/x86/rtc/device.o
-MODULESOBJ+=modules/src/arch/x86/rtl8139/main.o modules/src/arch/x86/rtl8139/driver.o
-MODULESOBJ+=modules/src/arch/x86/smp/main.o modules/src/arch/x86/smp/driver.o
-MODULESOBJ+=modules/src/arch/x86/uart/main.o modules/src/arch/x86/uart/driver.o modules/src/arch/x86/uart/device.o
-MODULESOBJ+=modules/src/arch/x86/vga/main.o modules/src/arch/x86/vga/driver.o
+MODULES+=$(MODULE_ACPI) $(MODULE_APIC) $(MODULE_ATA) $(MODULE_BGA) $(MODULE_CPUID) $(MODULE_FPU) $(MODULE_I915) $(MODULE_MSR) $(MODULE_PCI) $(MODULE_PIT) $(MODULE_PS2) $(MODULE_RTC) $(MODULE_RTL8139) $(MODULE_SMP) $(MODULE_UART) $(MODULE_VGA)
 
-modules/src/arch/x86/acpi/acpi.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/acpi/main.o modules/src/arch/x86/acpi/driver.o
+OBJECTS_ACPI=modules/src/arch/x86/acpi/main.o modules/src/arch/x86/acpi/driver.o
+OBJECTS_APIC=modules/src/arch/x86/apic/main.o modules/src/arch/x86/apic/driver.o
+OBJECTS_ATA=modules/src/arch/x86/ata/main.o modules/src/arch/x86/ata/bus.o modules/src/arch/x86/ata/device.o
+OBJECTS_BGA=modules/src/arch/x86/bga/main.o modules/src/arch/x86/bga/driver.o
+OBJECTS_CPUID=modules/src/arch/x86/cpuid/main.o modules/src/arch/x86/cpuid/driver.o modules/src/arch/x86/cpuid/cpuid.o
+OBJECTS_FPU=modules/src/arch/x86/fpu/main.o modules/src/arch/x86/fpu/driver.o modules/src/arch/x86/fpu/fpu.o
+OBJECTS_I915=modules/src/arch/x86/i915/main.o modules/src/arch/x86/i915/driver.o
+OBJECTS_MSR=modules/src/arch/x86/msr/main.o modules/src/arch/x86/msr/driver.o modules/src/arch/x86/msr/msr.o
+OBJECTS_PCI=modules/src/arch/x86/pci/main.o modules/src/arch/x86/pci/bus.o modules/src/arch/x86/pci/device.o
+OBJECTS_PIT=modules/src/arch/x86/pit/main.o modules/src/arch/x86/pit/driver.o modules/src/arch/x86/pit/device.o
+OBJECTS_PS2=modules/src/arch/x86/ps2/main.o modules/src/arch/x86/ps2/kbd_driver.o modules/src/arch/x86/ps2/kbd_device.o modules/src/arch/x86/ps2/mouse_driver.o modules/src/arch/x86/ps2/mouse_device.o
+OBJECTS_RTC=modules/src/arch/x86/rtc/main.o modules/src/arch/x86/rtc/device.o
+OBJECTS_RTL8139=modules/src/arch/x86/rtl8139/main.o modules/src/arch/x86/rtl8139/driver.o
+OBJECTS_SMP=modules/src/arch/x86/smp/main.o modules/src/arch/x86/smp/driver.o
+OBJECTS_UART=modules/src/arch/x86/uart/main.o modules/src/arch/x86/uart/driver.o modules/src/arch/x86/uart/device.o
+OBJECTS_VGA=modules/src/arch/x86/vga/main.o modules/src/arch/x86/vga/driver.o
+
+OBJECTS+=$(OBJECTS_ACPI) $(OBJECTS_APIC) $(OBJECTS_ATA) $(OBJECTS_BGA) $(OBJECTS_CPUID) $(OBJECTS_FPU) $(OBJECTS_I915) $(OBJECTS_MSR) $(OBJECTS_PCI) $(OBJECTS_PIT) $(OBJECTS_PS2) $(OBJECTS_RTC) $(OBJECTS_RTL8139) $(OBJECTS_SMP) $(OBJECTS_UART) $(OBJECTS_VGA)
+
+modules/src/arch/x86/acpi/acpi.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_ACPI)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/apic/apic.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/apic/main.o modules/src/arch/x86/apic/driver.o
+modules/src/arch/x86/apic/apic.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_APIC) 
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/ata/ata.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/ata/main.o modules/src/arch/x86/ata/bus.o modules/src/arch/x86/ata/device.o
+modules/src/arch/x86/ata/ata.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_ATA)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/bga/bga.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/bga/main.o modules/src/arch/x86/bga/driver.o
+modules/src/arch/x86/bga/bga.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_BGA)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/cpuid/cpuid.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/cpuid/main.o modules/src/arch/x86/cpuid/driver.o modules/src/arch/x86/cpuid/cpuid.o
+modules/src/arch/x86/cpuid/cpuid.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_CPUID)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/fpu/fpu.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/fpu/main.o modules/src/arch/x86/fpu/driver.o modules/src/arch/x86/fpu/fpu.o
+modules/src/arch/x86/fpu/fpu.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_FPU)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/i915/i915.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/i915/main.o modules/src/arch/x86/i915/driver.o
+modules/src/arch/x86/i915/i915.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_I915)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/msr/msr.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/msr/main.o modules/src/arch/x86/msr/driver.o modules/src/arch/x86/msr/msr.o
+modules/src/arch/x86/msr/msr.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_MSR)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/pci/pci.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/pci/main.o modules/src/arch/x86/pci/bus.o modules/src/arch/x86/pci/device.o
+modules/src/arch/x86/pci/pci.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_PCI)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/pit/pit.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/pit/main.o modules/src/arch/x86/pit/driver.o modules/src/arch/x86/pit/device.o
+modules/src/arch/x86/pit/pit.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_PIT)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/ps2/ps2.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/ps2/main.o modules/src/arch/x86/ps2/kbd_driver.o modules/src/arch/x86/ps2/kbd_device.o modules/src/arch/x86/ps2/mouse_driver.o modules/src/arch/x86/ps2/mouse_device.o
+modules/src/arch/x86/ps2/ps2.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_PS2)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/rtc/rtc.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/rtc/main.o modules/src/arch/x86/rtc/device.o
+modules/src/arch/x86/rtc/rtc.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_RTC)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/rtl8139/rtl8139.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/rtl8139/main.o modules/src/arch/x86/rtl8139/driver.o
+modules/src/arch/x86/rtl8139/rtl8139.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_RTL8139)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/smp/smp.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/smp/main.o modules/src/arch/x86/smp/driver.o
+modules/src/arch/x86/smp/smp.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_SMP)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/uart/uart.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/uart/main.o modules/src/arch/x86/uart/driver.o modules/src/arch/x86/uart/device.o
+modules/src/arch/x86/uart/uart.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_UART)
 	$(LD) $(LDFLAGS) -o $@ $^
 
-modules/src/arch/x86/vga/vga.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o modules/src/arch/x86/vga/main.o modules/src/arch/x86/vga/driver.o
+modules/src/arch/x86/vga/vga.ko: lib/src/memory.o lib/src/string.o lib/src/arch/x86/io.o $(OBJECTS_VGA)
 	$(LD) $(LDFLAGS) -o $@ $^
