2546c29a0d3118c982c7c17aec8667c32fc988f0
Jezze <jens.nyberg@gmail.com>
2012-08-12 02:11:36 +0200

    added build date and fixed help file

--- packages/base/init/init.c
+++ packages/base/init/init.c
@@ -112,6 +112,11 @@ void start_shell()
     call_open(FILE_STDOUT, 11, "/tty/stdout");
 
     call_write(FILE_STDOUT, 0, 23, "Fudge operating system\n");
+    call_write(FILE_STDOUT, 0, 12, "Build date: ");
+    call_write(FILE_STDOUT, 0, 11, __DATE__);
+    call_write(FILE_STDOUT, 0, 1, " ");
+    call_write(FILE_STDOUT, 0, 8, __TIME__);
+    call_write(FILE_STDOUT, 0, 2, "\n\n");
     call_write(FILE_STDOUT, 0, 53, "Write `echo <help.txt` for a short list if commands\n\n");
 
     call_open(3, 18, "/ramdisk/bin/shell");
