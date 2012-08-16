17aa3b17c6ee1e27ee50b20e34413465b291b641
Jezze <jens.nyberg@gmail.com>
2012-08-16 22:20:48 +0200

    minor fix

--- packages/base/init/init.c
+++ packages/base/init/init.c
@@ -42,7 +42,7 @@ void load_modules_core(unsigned int id)
     call_open(FILE_STDIN, 19, "/ramdisk/mod/tty.ko");
     call_execute(id);
 
-    call_mount(FILE_STDIN, 5, 5, "/tty/");
+    call_mount(FILE_STDIN, 6, 5, "/tty/");
 
 }
 
