efdc58ce466e34b3c1839990132377774d0e13d3
Jezze <jens.nyberg@gmail.com>
2012-08-19 22:00:02 +0200

    modified io module

--- modules/include/arch/x86/io/io.h
+++ modules/include/arch/x86/io/io.h
@@ -1,22 +1,12 @@
 #ifndef MODULES_IO_H
 #define MODULES_IO_H
 
-#define IO_DRIVER_TYPE 0x2812
-
-struct io_driver
-{
-
-    struct modules_driver base;
-
-};
-
 volatile unsigned char io_inb(unsigned short port);
 volatile unsigned short io_inw(unsigned short port);
 volatile unsigned int io_ind(unsigned short port);
 void io_outb(unsigned short port, unsigned char data);
 void io_outw(unsigned short port, unsigned short data);
 void io_outd(unsigned short port, unsigned int data);
-void io_driver_init(struct io_driver *driver);
 
 #endif
 
--- modules/src/arch/x86/io/main.c
+++ modules/src/arch/x86/io/main.c
@@ -1,20 +1,13 @@
 #include <modules.h>
 #include <arch/x86/io/io.h>
 
-static struct io_driver driver;
-
 void init()
 {
-
-    io_driver_init(&driver);
-    modules_register_driver(&driver.base);
 
 }
 
 void destroy()
 {
-
-    modules_unregister_driver(&driver.base);
 
 }
 
