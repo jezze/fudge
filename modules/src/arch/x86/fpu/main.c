ce0439212fa19bc0b18789607776458cd9ec10c2
Jezze <jens.nyberg@gmail.com>
2012-08-19 21:57:38 +0200

    modified fpu module

--- modules/src/arch/x86/fpu/main.c
+++ modules/src/arch/x86/fpu/main.c
@@ -1,20 +1,23 @@
 #include <modules.h>
+#include <arch/x86/cpuid/cpuid.h>
 #include <arch/x86/fpu/fpu.h>
-
-static struct fpu_driver driver;
 
 void init()
 {
 
-    fpu_driver_init(&driver);
-    modules_register_driver(&driver.base);
+    struct cpuid_data data;
+
+    cpuid_get(CPUID_INSTRUCTION_FEATURES0, &data);
+
+    if (!(data.edx & CPUID_FEATURES0_EDX_FLAG_FPU))
+        return;
+
+    fpu_enable();
 
 }
 
 void destroy()
 {
-
-    modules_unregister_driver(&driver.base);
 
 }
 
