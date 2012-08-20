984b001da4858af3241b933743090c1561c12822
Jezze <jens.nyberg@gmail.com>
2012-08-21 01:10:14 +0200

    moved code to correct place

--- modules/src/arch/x86/ps2/mouse_device.c
+++ modules/src/arch/x86/ps2/mouse_device.c
@@ -1,66 +1,15 @@
 #include <memory.h>
 #include <modules.h>
-#include <arch/x86/io/io.h>
 #include <arch/x86/ps2/ps2.h>
-
-static void wait_read()
-{
-
-    while ((io_inb(PS2_REGISTER_COMMAND) & 1) != 1);
-
-}
-
-static void wait_write()
-{
-
-    while ((io_inb(PS2_REGISTER_COMMAND) & 2) != 0);
-
-}
-
-static void write(unsigned char value)
-{
-
-    wait_write();
-    io_outb(PS2_REGISTER_COMMAND, 0xD4);
-    wait_write();
-    io_outb(PS2_REGISTER_DATA, value);
-
-}
-
-static unsigned char read()
-{
-
-    wait_read();
-
-    return io_inb(PS2_REGISTER_DATA);
-
-}
 
 void ps2_mouse_device_init(struct ps2_mouse_device *device, unsigned int irq)
 {
-
-    unsigned char status;
 
     memory_clear(device, sizeof (struct ps2_mouse_device));
 
     modules_device_init(&device->base, PS2_MOUSE_DEVICE_TYPE, "mouse");
 
     device->irq = irq;
-
-    wait_write();
-    io_outb(PS2_REGISTER_COMMAND, 0xA8);
-    wait_write();
-    io_outb(PS2_REGISTER_COMMAND, 0x20);
-    wait_read();
-    status = (io_inb(PS2_REGISTER_DATA) | 2);
-    wait_write();
-    io_outb(PS2_REGISTER_COMMAND, 0x60);
-    wait_write();
-    io_outb(PS2_REGISTER_DATA, status);
-    write(0xF6);
-    read();
-    write(0xF4);
-    read();
 
 }
 
--- modules/src/arch/x86/ps2/mouse_driver.c
+++ modules/src/arch/x86/ps2/mouse_driver.c
@@ -4,6 +4,39 @@
 #include <arch/x86/io/io.h>
 #include <arch/x86/ps2/ps2.h>
 
+static void wait_read()
+{
+
+    while ((io_inb(PS2_REGISTER_COMMAND) & 1) != 1);
+
+}
+
+static void wait_write()
+{
+
+    while ((io_inb(PS2_REGISTER_COMMAND) & 2) != 0);
+
+}
+
+static void write(unsigned char value)
+{
+
+    wait_write();
+    io_outb(PS2_REGISTER_COMMAND, 0xD4);
+    wait_write();
+    io_outb(PS2_REGISTER_DATA, value);
+
+}
+
+static unsigned char read()
+{
+
+    wait_read();
+
+    return io_inb(PS2_REGISTER_DATA);
+
+}
+
 static void handle_irq(struct modules_device *self)
 {
 
@@ -37,6 +70,28 @@ static void handle_irq(struct modules_device *self)
 
 }
 
+static void start(struct modules_driver *self)
+{
+
+    unsigned int status;
+
+    wait_write();
+    io_outb(PS2_REGISTER_COMMAND, 0xA8);
+    wait_write();
+    io_outb(PS2_REGISTER_COMMAND, 0x20);
+    wait_read();
+    status = (io_inb(PS2_REGISTER_DATA) | 2);
+    wait_write();
+    io_outb(PS2_REGISTER_COMMAND, 0x60);
+    wait_write();
+    io_outb(PS2_REGISTER_DATA, status);
+    write(0xF6);
+    read();
+    write(0xF4);
+    read();
+
+}
+
 static void attach(struct modules_device *device)
 {
 
@@ -53,12 +108,13 @@ static unsigned int check(struct modules_driver *self, struct modules_device *de
 
 }
 
+
 void ps2_mouse_driver_init(struct ps2_mouse_driver *driver)
 {
 
     memory_clear(driver, sizeof (struct ps2_mouse_driver));
 
-    modules_driver_init(&driver->base, PS2_MOUSE_DRIVER_TYPE, "mouse", 0, check, attach);
+    modules_driver_init(&driver->base, PS2_MOUSE_DRIVER_TYPE, "mouse", start, check, attach);
 
     driver->cycle = 2;
 
