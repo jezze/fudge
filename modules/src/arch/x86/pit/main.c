ffb7ecaa5ab9c22835bf90ebbfb3ee5cd371a3df
Jezze <jens.nyberg@gmail.com>
2012-08-25 01:32:39 +0200

    removed nodefs use in pit module

--- modules/src/arch/x86/pit/main.c
+++ modules/src/arch/x86/pit/main.c
@@ -1,21 +1,9 @@
 #include <memory.h>
-#include <vfs.h>
 #include <modules/modules.h>
-#include <nodefs/nodefs.h>
 #include <arch/x86/pit/pit.h>
 
 static struct pit_device device;
 static struct pit_driver driver;
-static struct nodefs_node jiffies;
-
-static unsigned int jiffies_read(struct nodefs_node *self, unsigned int offset, unsigned int count, void *buffer)
-{
-
-    memory_copy(buffer, &device.jiffies, 4);
-
-    return 4;
-
-}
 
 void init()
 {
@@ -26,13 +14,12 @@ void init()
     pit_driver_init(&driver);
     modules_register_driver(&driver.base);
 
-    nodefs_register_node(&jiffies, "pit_jiffies", &driver.base.base, jiffies_read, 0);
-
 }
 
 void destroy()
 {
 
+    modules_unregister_driver(&driver.base);
     modules_unregister_device(&device.base);
 
 }
