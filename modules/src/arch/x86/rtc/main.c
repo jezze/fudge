eb5f1d5a60fb87606faeb29a48d5091883be300c
Jezze <jens.nyberg@gmail.com>
2012-08-25 01:34:12 +0200

    removed nodefs use in rtc

--- modules/src/arch/x86/rtc/main.c
+++ modules/src/arch/x86/rtc/main.c
@@ -1,28 +1,14 @@
 #include <memory.h>
-#include <vfs.h>
 #include <modules/modules.h>
-#include <nodefs/nodefs.h>
 #include <arch/x86/rtc/rtc.h>
 
 static struct rtc_device device;
-static struct nodefs_node timestamp;
-
-static unsigned int timestamp_read(struct nodefs_node *self, unsigned int offset, unsigned int count, void *buffer)
-{
-
-    memory_copy(buffer, 0, 0);
-
-    return 0;
-
-}
 
 void init()
 {
 
     rtc_device_init(&device, RTC_IRQ);
     modules_register_device(&device.base);
-
-    nodefs_register_node(&timestamp, "rtc_timestamp", &device.base.base, timestamp_read, 0);
 
 }
 
