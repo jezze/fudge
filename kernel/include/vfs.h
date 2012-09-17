cc28becc1be3dcf712859e95ee6420d6deed88d2
Jezze <jens.nyberg@gmail.com>
2012-09-17 19:49:04 +0200

    more work

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -15,12 +15,8 @@ struct vfs_interface
 
 };
 
-unsigned int vfs_read8(char value, unsigned int offset, unsigned int count, void *buffer);
-unsigned int vfs_read16(short value, unsigned int offset, unsigned int count, void *buffer);
-unsigned int vfs_read32(int value, unsigned int offset, unsigned int count, void *buffer);
-unsigned int vfs_write8(char value, unsigned int offset, unsigned int count, void *buffer);
-unsigned int vfs_write16(short value, unsigned int offset, unsigned int count, void *buffer);
-unsigned int vfs_write32(int value, unsigned int offset, unsigned int count, void *buffer);
+unsigned int vfs_read(void *value, unsigned int length, unsigned int offset, unsigned int count, void *buffer);
+unsigned int vfs_write(void *value, unsigned int length, unsigned int offset, unsigned int count, void *buffer);
 void vfs_init_interface(struct vfs_interface *interface, unsigned int rootid, char *name, void (*open)(struct vfs_interface *self, unsigned int id), void (*close)(struct vfs_interface *self, unsigned int id), unsigned int (*read)(struct vfs_interface *self, unsigned int id, unsigned int offset, unsigned int count, void *buffer), unsigned int (*write)(struct vfs_interface *self, unsigned int id, unsigned int offset, unsigned int count, void *buffer), unsigned int (*parent)(struct vfs_interface *self, unsigned int id), unsigned int (*walk)(struct vfs_interface *self, unsigned int id, unsigned int count, char *path), unsigned int (*get_physical)(struct vfs_interface *self, unsigned int id));
 
 #endif
--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -1,92 +1,31 @@
 #include <memory.h>
 #include <vfs.h>
 
-unsigned int vfs_read8(char value, unsigned int offset, unsigned int count, void *buffer)
+unsigned int vfs_read(void *value, unsigned int length, unsigned int offset, unsigned int count, void *buffer)
 {
 
-    if (offset >= 1)
+    if (offset >= length)
         return 0;
 
-    if (count + offset >= 1)
-        count = 1 - offset;
+    if (count + offset >= length)
+        count = length - offset;
 
-    memory_copy(((char *)&value) + offset, buffer, count);
+    memory_copy(buffer, ((char *)value) + offset, count);
 
     return count;
 
 }
 
-
-unsigned int vfs_read16(short value, unsigned int offset, unsigned int count, void *buffer)
+unsigned int vfs_write(void *value, unsigned int length, unsigned int offset, unsigned int count, void *buffer)
 {
 
-    if (offset >= 2)
+    if (offset >= length)
         return 0;
 
-    if (count + offset >= 2)
-        count = 2 - offset;
+    if (count + offset >= length)
+        count = length - offset;
 
-    memory_copy(((char *)&value) + offset, buffer, count);
-
-    return count;
-
-}
-
-unsigned int vfs_read32(int value, unsigned int offset, unsigned int count, void *buffer)
-{
-
-    if (offset >= 4)
-        return 0;
-
-    if (count + offset >= 4)
-        count = 4 - offset;
-
-    memory_copy(((char *)&value) + offset, buffer, count);
-
-    return count;
-
-}
-
-unsigned int vfs_write8(char value, unsigned int offset, unsigned int count, void *buffer)
-{
-
-    if (offset >= 1)
-        return 0;
-
-    if (count + offset >= 1)
-        count = 1 - offset;
-
-    memory_copy(buffer, ((char *)&value) + offset, count);
-
-    return count;
-
-}
-
-unsigned int vfs_write16(short value, unsigned int offset, unsigned int count, void *buffer)
-{
-
-    if (offset >= 2)
-        return 0;
-
-    if (count + offset >= 2)
-        count = 2 - offset;
-
-    memory_copy(buffer, ((char *)&value) + offset, count);
-
-    return count;
-
-}
-
-unsigned int vfs_write32(int value, unsigned int offset, unsigned int count, void *buffer)
-{
-
-    if (offset >= 4)
-        return 0;
-
-    if (count + offset >= 4)
-        count = 4 - offset;
-
-    memory_copy(buffer, ((char *)&value) + offset, count);
+    memory_copy(((char *)value) + offset, buffer, count);
 
     return count;
 
