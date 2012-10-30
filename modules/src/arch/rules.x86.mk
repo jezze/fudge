db3179d9226bf79d06881cb5d9511e3a54f975d3
Jezze <jens.nyberg@gmail.com>
2012-10-31 00:27:28 +0100

    moved multi module to arch

--- modules/src/arch/rules.x86.mk
+++ modules/src/arch/rules.x86.mk
@@ -7,6 +7,7 @@ MODULES_FPU=modules/src/arch/x86/fpu/fpu.ko
 MODULES_I915=modules/src/arch/x86/i915/i915.ko
 MODULES_IO=modules/src/arch/x86/io/io.ko
 MODULES_MSR=modules/src/arch/x86/msr/msr.ko
+MODULES_MULTI=modules/src/arch/x86/multi/multi.ko
 MODULES_PCI=modules/src/arch/x86/pci/pci.ko
 MODULES_PIT=modules/src/arch/x86/pit/pit.ko
 MODULES_PS2=modules/src/arch/x86/ps2/ps2.ko
@@ -25,6 +26,7 @@ MODULES_OBJECTS_FPU=modules/src/arch/x86/fpu/main.o modules/src/arch/x86/fpu/fpu
 MODULES_OBJECTS_I915=modules/src/arch/x86/i915/main.o modules/src/arch/x86/i915/driver.o
 MODULES_OBJECTS_IO=modules/src/arch/x86/io/main.o modules/src/arch/x86/io/io.o
 MODULES_OBJECTS_MSR=modules/src/arch/x86/msr/main.o modules/src/arch/x86/msr/msr.o
+MODULES_OBJECTS_MULTI=modules/src/arch/x86/multi/main.o
 MODULES_OBJECTS_PCI=modules/src/arch/x86/pci/main.o modules/src/arch/x86/pci/bus.o modules/src/arch/x86/pci/device.o
 MODULES_OBJECTS_PIT=modules/src/arch/x86/pit/main.o modules/src/arch/x86/pit/driver.o modules/src/arch/x86/pit/device.o
 MODULES_OBJECTS_PS2=modules/src/arch/x86/ps2/main.o modules/src/arch/x86/ps2/device.o modules/src/arch/x86/ps2/kbd_driver.o modules/src/arch/x86/ps2/mouse_driver.o
@@ -34,8 +36,8 @@ MODULES_OBJECTS_SMP=modules/src/arch/x86/smp/main.o
 MODULES_OBJECTS_UART=modules/src/arch/x86/uart/main.o modules/src/arch/x86/uart/driver.o modules/src/arch/x86/uart/device.o
 MODULES_OBJECTS_VGA=modules/src/arch/x86/vga/main.o modules/src/arch/x86/vga/driver.o
 
-MODULES+=$(MODULES_ACPI) $(MODULES_APIC) $(MODULES_ATA) $(MODULES_BGA) $(MODULES_CPUID) $(MODULES_FPU) $(MODULES_I915) $(MODULES_IO) $(MODULES_MSR) $(MODULES_PCI) $(MODULES_PIT) $(MODULES_PS2) $(MODULES_RTC) $(MODULES_RTL8139) $(MODULES_SMP) $(MODULES_UART) $(MODULES_VGA)
-MODULES_OBJECTS+=$(MODULES_OBJECTS_ACPI) $(MODULES_OBJECTS_APIC) $(MODULES_OBJECTS_ATA) $(MODULES_OBJECTS_BGA) $(MODULES_OBJECTS_CPUID) $(MODULES_OBJECTS_FPU) $(MODULES_OBJECTS_I915) $(MODULES_OBJECTS_IO) $(MODULES_OBJECTS_MSR) $(MODULES_OBJECTS_PCI) $(MODULES_OBJECTS_PIT) $(MODULES_OBJECTS_PS2) $(MODULES_OBJECTS_RTC) $(MODULES_OBJECTS_RTL8139) $(MODULES_OBJECTS_SMP) $(MODULES_OBJECTS_UART) $(MODULES_OBJECTS_VGA)
+MODULES+=$(MODULES_ACPI) $(MODULES_APIC) $(MODULES_ATA) $(MODULES_BGA) $(MODULES_CPUID) $(MODULES_FPU) $(MODULES_I915) $(MODULES_IO) $(MODULES_MSR) $(MODULES_MULTI) $(MODULES_PCI) $(MODULES_PIT) $(MODULES_PS2) $(MODULES_RTC) $(MODULES_RTL8139) $(MODULES_SMP) $(MODULES_UART) $(MODULES_VGA)
+MODULES_OBJECTS+=$(MODULES_OBJECTS_ACPI) $(MODULES_OBJECTS_APIC) $(MODULES_OBJECTS_ATA) $(MODULES_OBJECTS_BGA) $(MODULES_OBJECTS_CPUID) $(MODULES_OBJECTS_FPU) $(MODULES_OBJECTS_I915) $(MODULES_OBJECTS_IO) $(MODULES_OBJECTS_MSR) $(MODULES_OBJECTS_MULTI) $(MODULES_OBJECTS_PCI) $(MODULES_OBJECTS_PIT) $(MODULES_OBJECTS_PS2) $(MODULES_OBJECTS_RTC) $(MODULES_OBJECTS_RTL8139) $(MODULES_OBJECTS_SMP) $(MODULES_OBJECTS_UART) $(MODULES_OBJECTS_VGA)
 
 $(MODULES_ACPI): lib/src/memory.o lib/src/string.o $(MODULES_OBJECTS_ACPI)
 	$(LD) $(LDFLAGS) -o $@ $^
@@ -62,6 +64,9 @@ $(MODULES_IO): lib/src/memory.o lib/src/string.o $(MODULES_OBJECTS_IO)
 	$(LD) $(LDFLAGS) -o $@ $^
 
 $(MODULES_MSR): lib/src/memory.o lib/src/string.o $(MODULES_OBJECTS_MSR)
+	$(LD) $(LDFLAGS) -o $@ $^
+
+$(MODULES_MULTI): lib/src/memory.o lib/src/string.o $(MODULES_OBJECTS_MULTI)
 	$(LD) $(LDFLAGS) -o $@ $^
 
 $(MODULES_PCI): lib/src/memory.o lib/src/string.o $(MODULES_OBJECTS_PCI)
