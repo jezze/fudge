c25e9ab657ff9457863c7b80c28f7d445b68358a
Jezze <jens.nyberg@gmail.com>
2012-11-24 03:14:12 +0100

    some fixes

--- modules/include/arch/x86/multi/multi.h
+++ modules/include/arch/x86/multi/multi.h
@@ -1,5 +1,15 @@
 #define MULTI_TASK_SLOTS                8
 
+struct multi_task
+{
+
+    struct runtime_task base;
+    struct mmu_directory *directory;
+    struct mmu_table *ktable;
+    struct mmu_table *utable;
+
+};
+
 struct multi_spawn_args
 {
 
--- modules/src/arch/x86/multi/main.c
+++ modules/src/arch/x86/multi/main.c
@@ -8,9 +8,9 @@
 #include <arch/x86/mmu.h>
 #include <arch/x86/multi/multi.h>
 
-static struct runtime_task tasks[MULTI_TASK_SLOTS];
+static struct multi_task tasks[MULTI_TASK_SLOTS];
 
-static struct runtime_task *get_task(unsigned int index)
+static struct multi_task *get_task(unsigned int index)
 {
 
     if (!index || index >= MULTI_TASK_SLOTS)
@@ -28,7 +28,7 @@ static unsigned int get_task_slot()
     for (i = 1; i < MULTI_TASK_SLOTS; i++)
     {
 
-        if (!tasks[i].status.used)
+        if (!tasks[i].base.status.used)
             return i;
 
     }
@@ -41,22 +41,18 @@ static void schedule()
 {
 
     unsigned int i;
-    struct mmu_directory *directory;
 
     for (i = MULTI_TASK_SLOTS - 1; i > 0; i--)
     {
 
-        if (!tasks[i].status.used)
+        if (!tasks[i].base.status.used)
             continue;
 
-        if (tasks[i].status.idle)
+        if (tasks[i].base.status.idle)
             continue;
 
-        isr_set_task(&tasks[i]);
-
-        directory = (struct mmu_directory *)(MMU_ADDRESS_DIRECTORIES + MMU_PAGE_SIZE * i);
-
-        mmu_load_memory(directory);
+        mmu_load_memory(tasks[i].directory);
+        isr_set_task(&tasks[i].base);
 
         break;
 
@@ -72,17 +68,17 @@ static void notify_interrupt(struct runtime_task *task, unsigned int index)
     for (i = 1; i < MULTI_TASK_SLOTS - 1; i++)
     {
 
-        if (!tasks[i].status.used)
+        if (!tasks[i].base.status.used)
             continue;
 
-        if (!tasks[i].status.idle)
+        if (!tasks[i].base.status.idle)
             continue;
 
-        if (!tasks[i].events[index].callback)
+        if (!tasks[i].base.events[index].callback)
             continue;
 
-        tasks[i].status.idle = 0;
-        runtime_init_registers(&tasks[i].registers, tasks[i].events[index].callback, RUNTIME_TASK_VADDRESS_BASE + RUNTIME_TASK_ADDRESS_SIZE, RUNTIME_TASK_VADDRESS_BASE + RUNTIME_TASK_ADDRESS_SIZE, tasks[i].registers.status);
+        runtime_init_registers(&tasks[i].base.registers, tasks[i].base.events[index].callback, RUNTIME_TASK_VADDRESS_BASE + RUNTIME_TASK_ADDRESS_SIZE, RUNTIME_TASK_VADDRESS_BASE + RUNTIME_TASK_ADDRESS_SIZE, tasks[i].base.registers.status);
+        tasks[i].base.status.idle = 0;
 
     }
 
@@ -101,10 +97,7 @@ static unsigned int spawn(struct runtime_task *task, void *stack)
     struct multi_spawn_args *args = stack;
     struct runtime_descriptor *descriptor = runtime_get_task_descriptor(task, args->index);
     unsigned int index = get_task_slot();
-    struct runtime_task *ntask = get_task(index);
-    struct mmu_directory *directory = (struct mmu_directory *)(MMU_ADDRESS_DIRECTORIES + MMU_PAGE_SIZE * index);
-    struct mmu_table *ktable = (struct mmu_table *)(MMU_ADDRESS_KTABLES + MMU_PAGE_SIZE * index);
-    struct mmu_table *utable = (struct mmu_table *)(MMU_ADDRESS_UTABLES + MMU_PAGE_SIZE * index);
+    struct multi_task *ntask = get_task(index);
     unsigned int entry;
 
     if (!descriptor || !descriptor->interface->read)
@@ -113,13 +106,17 @@ static unsigned int spawn(struct runtime_task *task, void *stack)
     if (!ntask)
         return 0;
 
-    memory_clear(directory, sizeof (struct mmu_directory));
-    memory_clear(ktable, sizeof (struct mmu_table));
-    memory_clear(utable, sizeof (struct mmu_table));
+    ntask->directory = (struct mmu_directory *)(MMU_ADDRESS_DIRECTORIES + MMU_PAGE_SIZE * index);
+    ntask->ktable = (struct mmu_table *)(MMU_ADDRESS_KTABLES + MMU_PAGE_SIZE * index);
+    ntask->utable = (struct mmu_table *)(MMU_ADDRESS_UTABLES + MMU_PAGE_SIZE * index);
 
-    mmu_map_memory(directory, ktable, ARCH_KERNEL_BASE, ARCH_KERNEL_BASE, ARCH_KERNEL_SIZE, MMU_TABLE_FLAG_PRESENT | MMU_TABLE_FLAG_WRITEABLE, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE);
-    mmu_map_memory(directory, utable, RUNTIME_TASK_PADDRESS_BASE + index * RUNTIME_TASK_ADDRESS_SIZE, RUNTIME_TASK_VADDRESS_BASE, RUNTIME_TASK_ADDRESS_SIZE, MMU_TABLE_FLAG_PRESENT | MMU_TABLE_FLAG_WRITEABLE | MMU_TABLE_FLAG_USERMODE, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE | MMU_PAGE_FLAG_USERMODE);
-    mmu_load_memory(directory);
+    memory_clear(ntask->directory, sizeof (struct mmu_directory));
+    memory_clear(ntask->ktable, sizeof (struct mmu_table));
+    memory_clear(ntask->utable, sizeof (struct mmu_table));
+
+    mmu_map_memory(ntask->directory, ntask->ktable, ARCH_KERNEL_BASE, ARCH_KERNEL_BASE, ARCH_KERNEL_SIZE, MMU_TABLE_FLAG_PRESENT | MMU_TABLE_FLAG_WRITEABLE, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE);
+    mmu_map_memory(ntask->directory, ntask->utable, RUNTIME_TASK_PADDRESS_BASE + index * RUNTIME_TASK_ADDRESS_SIZE, RUNTIME_TASK_VADDRESS_BASE, RUNTIME_TASK_ADDRESS_SIZE, MMU_TABLE_FLAG_PRESENT | MMU_TABLE_FLAG_WRITEABLE | MMU_TABLE_FLAG_USERMODE, MMU_PAGE_FLAG_PRESENT | MMU_PAGE_FLAG_WRITEABLE | MMU_PAGE_FLAG_USERMODE);
+    mmu_load_memory(ntask->directory);
 
     entry = binary_copy_program(descriptor->interface, descriptor->id);
 
@@ -128,10 +125,10 @@ static unsigned int spawn(struct runtime_task *task, void *stack)
 
     memory_copy(ntask, task, sizeof (struct runtime_task));
 
-    runtime_init_registers(&ntask->registers, entry, RUNTIME_TASK_VADDRESS_BASE + RUNTIME_TASK_ADDRESS_SIZE, RUNTIME_TASK_VADDRESS_BASE + RUNTIME_TASK_ADDRESS_SIZE, index);
+    runtime_init_registers(&ntask->base.registers, entry, RUNTIME_TASK_VADDRESS_BASE + RUNTIME_TASK_ADDRESS_SIZE, RUNTIME_TASK_VADDRESS_BASE + RUNTIME_TASK_ADDRESS_SIZE, index);
 
-    ntask->notify_interrupt = notify_interrupt;
-    ntask->notify_complete = notify_complete;
+    ntask->base.notify_interrupt = notify_interrupt;
+    ntask->base.notify_complete = notify_complete;
 
     schedule();
 
