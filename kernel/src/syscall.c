57b642f150d495507961a96a1c7e96ac2e5c527d
Jezze <jens.nyberg@gmail.com>
2012-12-02 19:52:53 +0100

    added some security

--- kernel/src/syscall.c
+++ kernel/src/syscall.c
@@ -118,7 +118,10 @@ static unsigned int open(struct runtime_task *task, void *stack)
     struct syscall_open_args *args = stack;
     struct runtime_mount *mount = runtime_get_task_mount(task, 1);
     struct runtime_descriptor *descriptor = runtime_get_task_descriptor(task, args->index);
-    
+   
+    if (!mount || !descriptor)
+        return 0;
+
     runtime_init_descriptor(descriptor, mount->parent.interface, mount->parent.id);
 
     if (!runtime_update_task_descriptor(task, descriptor, args->count - 1, args->path + 1))
