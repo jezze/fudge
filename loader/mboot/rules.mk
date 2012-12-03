6500d24a5fc41f18d076aeee1ab4d65bf2f0c910
Jezze <jens.nyberg@gmail.com>
2012-12-03 01:28:11 +0100

    minor cleanup

--- loader/mboot/rules.mk
+++ loader/mboot/rules.mk
@@ -3,7 +3,7 @@ LOADER+=loader/libmboot.a
 LOADER_OBJECTS+=loader/mboot/src/init.o
 LOADER_OBJECTS+=loader/mboot/src/mboot.o
 
-$(LOADER): $(LOADER_OBJECTS)
+$(LOADER): $(LIB) $(LOADER_OBJECTS)
 	$(AR) $(ARFLAGS) $@ $^
 
 loader/mboot/%.o: loader/mboot/%.c
