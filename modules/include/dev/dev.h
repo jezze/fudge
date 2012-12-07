4bf753305caac30673edab3e495923a6f917c15f
Jezze <jens.nyberg@gmail.com>
2012-12-07 03:48:24 +0100

    more cleanups

--- modules/include/dev/dev.h
+++ modules/include/dev/dev.h
@@ -2,10 +2,6 @@ struct dev_filesystem
 {
 
     struct vfs_interface base;
-    struct dev_interface *interfaces[8];
-    unsigned int interfacesCount;
-    struct dev_protocol *protocols[16];
-    unsigned int protocolsCount;
 
 };
 
