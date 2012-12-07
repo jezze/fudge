809252e79d928a880c37a1156ffcb2fbb76a9829
Jezze <jens.nyberg@gmail.com>
2012-12-07 02:02:15 +0100

    some cleanups

--- packages/base/initm/initm.c
+++ packages/base/initm/initm.c
@@ -5,39 +5,51 @@ void load_modules_core(unsigned int id)
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/dev.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 22, "/ramdisk/mod/nodefs.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 21, "/ramdisk/mod/video.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/net.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 21, "/ramdisk/mod/block.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 18, "/ramdisk/mod/io.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 21, "/ramdisk/mod/cpuid.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/msr.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/pic.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/vga.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/ps2.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/tty.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
 }
 
@@ -47,28 +59,36 @@ void load_modules_extra(unsigned int id)
 /*
     call_open(FILE_STDIN, 20, "/ramdisk/mod/acpi.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 */
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/fpu.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/pci.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/pit.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/rtc.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 20, "/ramdisk/mod/uart.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 19, "/ramdisk/mod/arp.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     call_open(FILE_STDIN, 20, "/ramdisk/mod/ipv4.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
 }
 
@@ -78,18 +98,22 @@ void load_modules_testing(unsigned int id)
     /* Experimental */
     call_open(FILE_STDIN, 19, "/ramdisk/mod/ata.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     /* Experimental */
     call_open(FILE_STDIN, 20, "/ramdisk/mod/ext2.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     /* Does not work if device is non-existing */
     call_open(FILE_STDIN, 23, "/ramdisk/mod/rtl8139.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
     /* Does not work if device is non-existing */
     call_open(FILE_STDIN, 19, "/ramdisk/mod/bga.ko");
     call_spawn(id);
+    call_close(FILE_STDIN);
 
 }
 
@@ -103,21 +127,34 @@ void load_modules()
 
     call_close(3);
 
+}
+
+void mount_filesystems()
+{
+
     call_open(3, 5, "/dev/");
     call_open(4, 19, "/ramdisk/mod/dev.ko");
     call_mount(3, 3, 4);
+    call_close(3);
+    call_close(4);
 
     call_open(3, 9, "/dev/tty/");
     call_open(4, 19, "/ramdisk/mod/tty.ko");
     call_mount(4, 3, 4);
+    call_close(3);
+    call_close(4);
 
     call_open(3, 12, "/dev/nodefs/");
     call_open(4, 22, "/ramdisk/mod/nodefs.ko");
     call_mount(5, 3, 4);
+    call_close(3);
+    call_close(4);
 
     call_open(3, 11, "/dev/video/");
     call_open(4, 21, "/ramdisk/mod/video.ko");
     call_mount(6, 3, 4);
+    call_close(3);
+    call_close(4);
 
 }
 
@@ -137,6 +174,7 @@ void start_shell()
 
     call_open(3, 18, "/ramdisk/bin/shell");
     call_execute(3);
+    call_close(3);
 
 }
 
@@ -144,6 +182,7 @@ void main()
 {
 
     load_modules();
+    mount_filesystems();
     start_shell();
 
 }
