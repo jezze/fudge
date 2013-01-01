0427760e4de28da62d7cf3eb11ab9dc9a3f3da53
Jens Nyberg <jens.nyberg@gmail.com>
2013-01-01 22:06:52 +0100

    added system slot define

--- modules/system/filesystem.h
+++ modules/system/filesystem.h
@@ -1,10 +1,12 @@
+#define SYSTEM_FILESYSTEM_SLOTS         32
+
 struct system_filesystem
 {
 
     struct vfs_interface base;
     struct system_group root;
-    unsigned int (*readers[32])(struct system_node *node, unsigned int offset, unsigned int count, void *buffer);
-    unsigned int (*writers[32])(struct system_node *node, unsigned int offset, unsigned int count, void *buffer);
+    unsigned int (*readers[SYSTEM_FILESYSTEM_SLOTS])(struct system_node *node, unsigned int offset, unsigned int count, void *buffer);
+    unsigned int (*writers[SYSTEM_FILESYSTEM_SLOTS])(struct system_node *node, unsigned int offset, unsigned int count, void *buffer);
 
 };
 
