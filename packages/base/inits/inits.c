db09396a87481d2fbaf929133d14b3233511ed0d
Jens Nyberg <jens.nyberg@gmail.com>
2013-01-01 22:45:50 +0100

    moved base module load

--- packages/base/inits/inits.c
+++ packages/base/inits/inits.c
@@ -135,11 +135,6 @@ void main()
 
     call_read(3, kHeader.shoffset, kHeader.shsize * kHeader.shcount, kSectionTable);
 
-    call_open(FUDGE_IN, FUDGE_ROOT, 19, "ramdisk/mod/base.ko");
-
-    if (resolve())
-        call_load(FUDGE_IN);
-
     call_open(FUDGE_IN, FUDGE_ROOT, 20, "ramdisk/mod/multi.ko");
 
     if (resolve())
