dc581fd8e2b84d3c11e9de6c8074ad04ed782e6e
Jens Nyberg <jens.nyberg@gmail.com>
2013-01-01 19:02:34 +0100

    updated net module

--- modules/net/main.c
+++ modules/net/main.c
@@ -1,5 +1,9 @@
+#include <fudge/memory.h>
 #include <base/base.h>
+#include <system/system.h>
 #include "net.h"
+
+static struct system_group root;
 
 void net_register_interface(struct net_interface *interface)
 {
@@ -21,13 +25,39 @@ void net_unregister_protocol(unsigned short index)
 
 }
 
+void net_init_interface(struct net_interface *interface, struct base_driver *driver, unsigned int (*send)(struct net_interface *self, unsigned int count, void *buffer))
+{
+
+    memory_clear(interface, sizeof (struct net_interface));
+
+    interface->driver = driver;
+    interface->send = send;
+
+}
+
+void net_init_protocol(struct net_protocol *protocol, char *name, unsigned int (*read)(struct net_interface *interface, unsigned int offset, unsigned int count, void *buffer), unsigned int (*write)(struct net_interface *interface, unsigned int offset, unsigned int count, void *buffer))
+{
+
+    memory_clear(protocol, sizeof (struct net_protocol));
+
+    protocol->name = name;
+    protocol->read = read;
+    protocol->write = write;
+
+}
+
 void init()
 {
+
+    system_init_group(&root, "net");
+    system_register_node(&root.base);
 
 }
 
 void destroy()
 {
+
+    system_unregister_node(&root.base);
 
 }
 
