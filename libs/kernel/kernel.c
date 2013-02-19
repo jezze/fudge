de5019fee02e17eb1ecd093e8d88c6792634ded3
Jens Nyberg <jens.nyberg@gmail.com>
2013-02-19 18:43:51 +0100

    minor cleanups

--- libs/kernel/kernel.c
+++ libs/kernel/kernel.c
@@ -28,14 +28,15 @@ static void setup_container()
 {
 
     struct vfs_interface *root = vfs_setup();
-    unsigned int rid = root->walk(root, root->rootid, 8, "ramdisk/");
+    unsigned int id = root->walk(root, root->rootid, 8, "ramdisk/");
 
+    error_assert(id != 0, "Ramdisk mountpoint not found", __FILE__, __LINE__);
     runtime_init_container(&icontainer);
 
     icontainer.mounts[1].child.interface = root;
     icontainer.mounts[1].child.id = root->rootid;
     icontainer.mounts[2].parent.interface = root;
-    icontainer.mounts[2].parent.id = rid;
+    icontainer.mounts[2].parent.id = id;
     icontainer.mounts[2].child.interface = &iramdisk.interface;
     icontainer.mounts[2].child.id = iramdisk.interface.rootid;
 
@@ -46,14 +47,12 @@ static void setup_task(struct binary_format *format)
 
     struct vfs_interface *root = icontainer.mounts[1].child.interface;
     struct vfs_interface *ramdisk = icontainer.mounts[2].child.interface;
-    unsigned int rid = root->walk(root, root->rootid, 8, "ramdisk/");
-    unsigned int iid = ramdisk->walk(ramdisk, ramdisk->rootid, 9, "bin/inits");
+    unsigned int id = ramdisk->walk(ramdisk, ramdisk->rootid, 9, "bin/inits");
 
-    error_assert(rid != 0, "Ramdisk mountpoint not found", __FILE__, __LINE__);
-    error_assert(iid != 0, "Init program not found", __FILE__, __LINE__);
+    error_assert(id != 0, "Init program not found", __FILE__, __LINE__);
     runtime_init_task(&itask, &icontainer);
 
-    itask.registers.ip = format->copy_program(ramdisk, iid);
+    itask.registers.ip = format->copy_program(ramdisk, id);
     itask.registers.sp = RUNTIME_STACKADDRESS_VIRTUAL;
     itask.registers.fp = RUNTIME_STACKADDRESS_VIRTUAL;
 
