9e93401f8b90407070c65b4b8ed33e9414d82499
Jens Nyberg <jens.nyberg@gmail.com>
2013-03-08 04:54:29 +0100

    updated complete

--- packages/base/complete/complete.c
+++ packages/base/complete/complete.c
@@ -5,31 +5,13 @@ void main()
 
     char buffer[FUDGE_BSIZE];
     char buffer2[FUDGE_BSIZE];
+    unsigned int start = 0;
     unsigned int count;
     unsigned int count2;
     unsigned int i;
-    unsigned int start = 0;
 
     count = call_read(FUDGE_IN, 0, FUDGE_BSIZE, buffer);
     count2 = call_read(FUDGE_CWD, 0, FUDGE_BSIZE, buffer2);
-
-    if (!count)
-    {
-
-        call_write(FUDGE_OUT, 0, count2, buffer2);
-
-        return;
-
-    }
-
-    if (count < 4 && memory_match(buffer, "../", count))
-    {
-
-        call_write(FUDGE_OUT, 0, 4, "../\n");
-
-        return;
-
-    }
 
     for (i = 0; i < count2; i++)
     {
@@ -37,16 +19,10 @@ void main()
         if (buffer2[i] != '\n')
             continue;
 
-        if (start)
-        {
-
+        if (memory_match(buffer2 + start, buffer, count))
             call_write(FUDGE_OUT, 0, i - start + 1, buffer2 + start);
-            start = 0;
 
-        }
-
-        if (memory_match(buffer2 + i + 1, buffer, count))
-            start = i + 1;
+        start = i + 1;
 
     }
 
