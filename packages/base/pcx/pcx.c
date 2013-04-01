0e22e7b2b555ee4fae69105d8ec837da4ddda29c
Jens Nyberg <jens.nyberg@gmail.com>
2013-04-01 02:08:01 +0200

    more cleanups

--- packages/base/pcx/pcx.c
+++ packages/base/pcx/pcx.c
@@ -6,30 +6,38 @@
 static struct pcx_surface pcx;
 static struct vga_surface vga;
 
-static void render(struct pcx_surface *surface)
+static void prepare()
 {
 
     unsigned char colormap[768];
-    unsigned int offset = 128;
-    unsigned int row;
 
-    gfx_convert_colormap(colormap, surface->colormap, 768);
+    gfx_convert_colormap(colormap, pcx.colormap, 768);
+
     call_open(3, FUDGE_ROOT, 27, "system/video/vga/resolution");
     call_write(3, 0, 9, "320x200x8");
     call_close(3);
     call_open(3, FUDGE_ROOT, 25, "system/video/vga/colormap");
     call_write(3, 0, 768, colormap);
     call_close(3);
+
+}
+
+static void render(struct gfx_surface *surface)
+{
+
+    unsigned int offset = 128;
+    unsigned int row;
+
     call_open(3, FUDGE_ROOT, 21, "system/video/vga/data");
 
-    for (row = 0; row < surface->base.height; row++)
+    for (row = 0; row < surface->height; row++)
     {
 
         char buffer[FUDGE_BSIZE];
 
-        offset += surface->base.read(&surface->base, offset, FUDGE_BSIZE, buffer);
+        offset += surface->read(surface, offset, FUDGE_BSIZE, buffer);
 
-        call_write(3, row * surface->base.width, surface->base.width, buffer);
+        call_write(3, row * surface->width, surface->width, buffer);
 
     }
 
@@ -44,7 +52,8 @@ void main()
     pcx_load(&pcx);
     vga_init_surface(&vga);
     vga_load(&vga);
-    render(&pcx);
+    prepare();
+    render(&pcx.base);
 
 }
 
