32cb7cbd8a408eff57b7bf127d5d9a2f12c2b5c5
Jens Nyberg <jens.nyberg@gmail.com>
2013-04-07 21:53:29 +0200

    add mouse node

--- modules/mouse/main.c
+++ modules/mouse/main.c
@@ -3,10 +3,46 @@
 #include <system/system.h>
 #include "mouse.h"
 
+struct mouse_interface_group
+{
+
+    struct system_group base;
+    struct mouse_interface *interface;
+    struct system_stream data;
+
+};
+
 static struct system_group root;
+static struct mouse_interface_group interfaces[32];
+static unsigned int ninterfaces;
+
+unsigned int data_read(struct system_stream *self, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    return 0;
+
+}
+
+unsigned int data_write(struct system_stream *self, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    return 0;
+
+}
 
 void mouse_register_interface(struct mouse_interface *interface)
 {
+
+    struct mouse_interface_group *group = &interfaces[ninterfaces];
+
+    group->interface = interface;
+
+    system_init_group(&group->base, interface->driver->module.name);
+    system_group_add(&root, &group->base.node);
+    system_init_stream(&group->data, "data", data_read, data_write);
+    system_group_add(&group->base, &group->data.node);
+
+    ninterfaces++;
 
 }
 
@@ -45,6 +81,8 @@ void mouse_init_protocol(struct mouse_protocol *protocol, char *name)
 
 void init()
 {
+
+    ninterfaces = 0;
 
     system_init_group(&root, "mouse");
     system_register_node(&root.node);
