8b5ebd17322db6d2c271e0342807b73a400d35aa
Jens Nyberg <jens.nyberg@gmail.com>
2013-04-09 12:36:14 +0200

    some multi updates

--- modules/arch/x86/multi/main.c
+++ modules/arch/x86/multi/main.c
@@ -34,7 +34,16 @@ static struct multi_task *create_task()
 
 }
 
-static void schedule(struct runtime_container *container)
+static void activate(struct runtime_container *container, struct multi_task *task)
+{
+
+    mmu_load_memory(&task->directory);
+
+    container->running = &task->base;
+
+}
+
+static struct multi_task *schedule()
 {
 
     unsigned int i;
@@ -54,34 +63,42 @@ static void schedule(struct runtime_container *container)
 
         }
 
-        mmu_load_memory(&tasks[i]->directory);
-
-        container->running = &tasks[i]->base;
-
-        break;
+        return tasks[i];
 
     }
+
+    return 0;
 
 }
 
 static void notify_pagefault(struct runtime_container *self, unsigned int address)
 {
 
+    struct multi_task *task;
+
     self->running->state = 0;
 
-    schedule(self);
+    task = schedule();
+
+    if (task)
+        activate(self, task);
 
 }
 
 static void notify_syscall(struct runtime_container *self, unsigned int index, unsigned int ip, unsigned int sp, unsigned int fp)
 {
 
+    struct multi_task *task;
+
     self->running->registers.ip = ip;
     self->running->registers.sp = sp;
     self->running->registers.fp = fp;
     self->running->registers.status = syscall_raise(index, self->running);
 
-    schedule(self);
+    task = schedule();
+
+    if (task)
+        activate(self, task);
 
 }
 
@@ -94,18 +111,27 @@ static unsigned int spawn(struct runtime_task *task, void *stack)
     if (!ntask)
         return 0;
 
-    task->container->notify_pagefault = notify_pagefault;
-    task->container->notify_syscall = notify_syscall;
-
     memory_copy(&ntask->base, task, sizeof (struct runtime_task));
     memory_copy(&nargs, args, sizeof (struct parameters));
 
     ntask->base.registers.sp = (unsigned int)&nargs;
     ntask->base.registers.fp = (unsigned int)&nargs;
 
-    schedule(task->container);
+    activate(ntask->base.container, ntask);
 
     return syscall_raise(SYSCALL_INDEX_EXECUTE, &ntask->base);
+
+}
+
+static unsigned int spawn_first(struct runtime_task *task, void *stack)
+{
+
+    task->container->notify_pagefault = notify_pagefault;
+    task->container->notify_syscall = notify_syscall;
+
+    syscall_set_routine(SYSCALL_INDEX_SPAWN, spawn);
+
+    return spawn(task, stack);
 
 }
 
@@ -113,7 +139,7 @@ void init()
 {
 
     memory_clear(tasks, sizeof (struct multi_task *) * MULTI_TASK_SLOTS);
-    syscall_set_routine(SYSCALL_INDEX_SPAWN, spawn);
+    syscall_set_routine(SYSCALL_INDEX_SPAWN, spawn_first);
 
 }
 
