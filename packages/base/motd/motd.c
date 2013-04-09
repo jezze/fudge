89cbb699cef61e024e852af296fd6ce6d821ace5
Jens Nyberg <jens.nyberg@gmail.com>
2013-04-09 19:16:23 +0200

    some cleanups

--- packages/base/motd/motd.c
+++ packages/base/motd/motd.c
@@ -3,13 +3,15 @@
 void main()
 {
 
-    call_write(FUDGE_OUT, 0, 23, "FUDGE OPERATING SYSTEM\n");
-    call_write(FUDGE_OUT, 0, 12, "Build date: ");
-    call_write(FUDGE_OUT, 0, 11, __DATE__);
-    call_write(FUDGE_OUT, 0, 1, " ");
-    call_write(FUDGE_OUT, 0, 8, __TIME__);
-    call_write(FUDGE_OUT, 0, 2, "\n\n");
-    call_write(FUDGE_OUT, 0, 43, "Write `help` for a short list if commands\n\n");
+    unsigned int offset = 0;
+
+    offset += call_write(FUDGE_OUT, offset, 23, "FUDGE OPERATING SYSTEM\n");
+    offset += call_write(FUDGE_OUT, offset, 12, "Build date: ");
+    offset += call_write(FUDGE_OUT, offset, 11, __DATE__);
+    offset += call_write(FUDGE_OUT, offset, 1, " ");
+    offset += call_write(FUDGE_OUT, offset, 8, __TIME__);
+    offset += call_write(FUDGE_OUT, offset, 2, "\n\n");
+    offset += call_write(FUDGE_OUT, offset, 43, "Write `help` for a short list if commands\n\n");
 
 }
 
