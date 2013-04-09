c5fed1c431c4ec52eb50261c5313514197fdf5f7
Jens Nyberg <jens.nyberg@gmail.com>
2013-04-09 21:14:29 +0200

    more cleanups

--- libs/kernel/kernel.c
+++ libs/kernel/kernel.c
@@ -8,42 +8,35 @@
 #include "binary_elf.h"
 #include "syscall.h"
 
-static struct runtime_container icontainer;
-static struct runtime_task itask;
+static struct runtime_container container;
+static struct runtime_task task;
 
-static void setup_container(void *ramdiskaddress)
+static void setup_container(struct vfs_interface *ramdisk)
 {
 
-    struct vfs_interface *ramdisk = vfs_ramdisk_setup(ramdiskaddress);
+    runtime_init_container(&container);
 
-    error_assert(ramdisk != 0, "Ramdisk not found", __FILE__, __LINE__);
-    runtime_init_container(&icontainer);
-
-    icontainer.running = &itask;
-    icontainer.mounts[1].child.interface = ramdisk;
-    icontainer.mounts[1].child.id = ramdisk->rootid;
+    container.running = &task;
+    container.mounts[1].child.interface = ramdisk;
+    container.mounts[1].child.id = ramdisk->rootid;
 
 }
 
-static void setup_task(struct binary_format *format)
+static void setup_task(struct vfs_interface *ramdisk, struct binary_format *format)
 {
 
-    struct vfs_interface *ramdisk = icontainer.mounts[1].child.interface;
-    unsigned int id = ramdisk->walk(ramdisk, ramdisk->rootid, 8, "bin/init");
+    runtime_init_task(&task, &container);
 
-    error_assert(id != 0, "Init program not found", __FILE__, __LINE__);
-    runtime_init_task(&itask, &icontainer);
+    task.registers.ip = format->copy_program(ramdisk, ramdisk->walk(ramdisk, ramdisk->rootid, 8, "bin/init"));
+    task.registers.sp = RUNTIME_STACKADDRESS_VIRTUAL;
+    task.registers.fp = RUNTIME_STACKADDRESS_VIRTUAL;
 
-    itask.registers.ip = format->copy_program(ramdisk, id);
-    itask.registers.sp = RUNTIME_STACKADDRESS_VIRTUAL;
-    itask.registers.fp = RUNTIME_STACKADDRESS_VIRTUAL;
+    error_assert(task.registers.ip != 0, "Init program entry point not found", __FILE__, __LINE__);
 
-    error_assert(itask.registers.ip != 0, "Init program entry point not found", __FILE__, __LINE__);
-
-    itask.descriptors[8].interface = ramdisk;
-    itask.descriptors[8].id = ramdisk->rootid;
-    itask.descriptors[9].interface = ramdisk;
-    itask.descriptors[9].id = ramdisk->rootid;
+    task.descriptors[8].interface = ramdisk;
+    task.descriptors[8].id = ramdisk->rootid;
+    task.descriptors[9].interface = ramdisk;
+    task.descriptors[9].id = ramdisk->rootid;
 
 }
 
@@ -51,14 +44,16 @@ struct runtime_container *kernel_setup(unsigned int modulesc, void **modulesv)
 {
 
     struct binary_format *elf = binary_elf_setup();
+    struct vfs_interface *ramdisk = vfs_ramdisk_setup(modulesv[0]);
 
+    error_assert(ramdisk != 0, "Ramdisk not found", __FILE__, __LINE__);
     binary_setup();
     binary_register_format(elf);
-    setup_container(modulesv[0]);
-    setup_task(elf);
+    setup_container(ramdisk);
+    setup_task(ramdisk, elf);
     syscall_setup();
 
-    return &icontainer;
+    return &container;
 
 }
 
