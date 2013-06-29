2c75648f717f92ec97b86ad8c7b2b5395192d668
Jens Nyberg <jens.nyberg@gmail.com>
2013-06-29 04:36:28 +0200

    more cleanups

--- libs/kernel/kernel.c
+++ libs/kernel/kernel.c
@@ -12,53 +12,55 @@
 static struct runtime_container container;
 static struct runtime_task task;
 
-static void setup(struct vfs_backend *backend, struct vfs_protocol *vprotocol, struct binary_protocol *bprotocol, unsigned int id)
+static void setup(struct vfs_backend *backend, struct vfs_protocol *protocol, unsigned int ip)
 {
 
     runtime_init_container(&container);
 
     container.running = &task;
     container.mounts[0x01].child.backend = backend;
-    container.mounts[0x01].child.protocol = vprotocol;
-    container.mounts[0x01].child.id = vprotocol->rootid;
+    container.mounts[0x01].child.protocol = protocol;
+    container.mounts[0x01].child.id = protocol->rootid;
 
     runtime_init_task(&task, &container);
 
-    task.registers.ip = bprotocol->copy_program(vprotocol, backend, id);
+    task.registers.ip = ip;
     task.registers.sp = RUNTIME_STACKADDRESS_VIRTUAL;
     task.registers.fp = RUNTIME_STACKADDRESS_VIRTUAL;
-
-    error_assert(task.registers.ip != 0, "Init program entry point not found", __FILE__, __LINE__);
-
     task.descriptors[0x0E].backend = backend;
-    task.descriptors[0x0E].protocol = vprotocol;
-    task.descriptors[0x0E].id = vprotocol->rootid;
+    task.descriptors[0x0E].protocol = protocol;
+    task.descriptors[0x0E].id = protocol->rootid;
     task.descriptors[0x0F].backend = backend;
-    task.descriptors[0x0F].protocol = vprotocol;
-    task.descriptors[0x0F].id = vprotocol->rootid;
+    task.descriptors[0x0F].protocol = protocol;
+    task.descriptors[0x0F].id = protocol->rootid;
 
 }
 
-static void detect(struct vfs_backend *backend)
+static void detect(struct vfs_backend *vbackend)
 {
 
     struct vfs_protocol *vprotocol;
     struct binary_protocol *bprotocol;
     unsigned int id;
+    unsigned int ip;
 
-    vprotocol = vfs_get_protocol(backend);
+    vprotocol = vfs_get_protocol(vbackend);
 
     error_assert(vprotocol != 0, "Filesystem protocol not recognized", __FILE__, __LINE__);
 
-    id = vprotocol->walk(backend, vprotocol->rootid, 8, "bin/init");
+    id = vprotocol->walk(vbackend, vprotocol->rootid, 8, "bin/init");
 
     error_assert(id != 0, "Init program not found", __FILE__, __LINE__);
 
-    bprotocol = binary_get_protocol(vprotocol, backend, id);
+    bprotocol = binary_get_protocol(vprotocol, vbackend, id);
 
     error_assert(bprotocol != 0, "Binary protocol not recognized", __FILE__, __LINE__);
 
-    setup(backend, vprotocol, bprotocol, id);
+    ip = bprotocol->copy_program(vprotocol, vbackend, id);
+
+    error_assert(ip != 0, "Init entry not found", __FILE__, __LINE__);
+
+    setup(vbackend, vprotocol, ip);
 
 }
 
