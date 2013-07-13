39d8d8efd68a185856cba296980e0fb187df323e
Jens Nyberg <jens.nyberg@gmail.com>
2013-07-13 15:37:03 +0200

    found better implementation of md5

--- packages/md5/md5.c
+++ packages/md5/md5.c
@@ -9,11 +9,15 @@ struct md5
 
 };
 
-#define F(x, y, z)                      (z ^ (x & (y ^ z)))
-#define G(x, y, z)                      (y ^ (z & (x ^ y)))
-#define H(x, y, z)                      (x ^ y ^ z)
-#define I(x, y, z)                      (y ^ (x | ~z))
-#define STEP(f, a, b, c, d, t, x, s)    a += f(b, c, d) + x + t; a = ((a << s) | (a >> (32 - s))); a += b;
+static unsigned int rol(unsigned int n, int k) { return (n << k) | (n >> (32-k)); }
+#define F(x,y,z) (z ^ (x & (y ^ z)))
+#define G(x,y,z) (y ^ (z & (y ^ x)))
+#define H(x,y,z) (x ^ y ^ z)
+#define I(x,y,z) (y ^ (x | ~z))
+#define FF(a,b,c,d,w,s,t) a += F(b,c,d) + w + t; a = rol(a,s) + b
+#define GG(a,b,c,d,w,s,t) a += G(b,c,d) + w + t; a = rol(a,s) + b
+#define HH(a,b,c,d,w,s,t) a += H(b,c,d) + w + t; a = rol(a,s) + b
+#define II(a,b,c,d,w,s,t) a += I(b,c,d) + w + t; a = rol(a,s) + b
 
 static const unsigned int tab[64] =
 {
@@ -29,110 +33,94 @@ static const unsigned int tab[64] =
 
 };
 
-static void *body(struct md5 *s, unsigned int count, void *buffer)
+static void processblock(struct md5 *s, const unsigned char *buffer)
 {
 
-    unsigned int *current = buffer;
-    unsigned int a, b, c, d;
-    unsigned int sa, sb, sc, sd;
+	unsigned int i, W[16], a, b, c, d;
 
-    a = s->a;
-    b = s->b;
-    c = s->c;
-    d = s->d;
+	for (i = 0; i < 16; i++) {
+		W[i] = buffer[4*i];
+		W[i] |= (unsigned int)buffer[4*i+1]<<8;
+		W[i] |= (unsigned int)buffer[4*i+2]<<16;
+		W[i] |= (unsigned int)buffer[4*i+3]<<24;
+	}
 
-    do {
+	a = s->a;
+	b = s->b;
+	c = s->c;
+	d = s->d;
 
-        sa = a;
-        sb = b;
-        sc = c;
-        sd = d;
+	i = 0;
+	while (i < 16) {
+		FF(a,b,c,d, W[i],  7, tab[i]); i++;
+		FF(d,a,b,c, W[i], 12, tab[i]); i++;
+		FF(c,d,a,b, W[i], 17, tab[i]); i++;
+		FF(b,c,d,a, W[i], 22, tab[i]); i++;
+	}
+	while (i < 32) {
+		GG(a,b,c,d, W[(5*i+1)%16],  5, tab[i]); i++;
+		GG(d,a,b,c, W[(5*i+1)%16],  9, tab[i]); i++;
+		GG(c,d,a,b, W[(5*i+1)%16], 14, tab[i]); i++;
+		GG(b,c,d,a, W[(5*i+1)%16], 20, tab[i]); i++;
+	}
+	while (i < 48) {
+		HH(a,b,c,d, W[(3*i+5)%16],  4, tab[i]); i++;
+		HH(d,a,b,c, W[(3*i+5)%16], 11, tab[i]); i++;
+		HH(c,d,a,b, W[(3*i+5)%16], 16, tab[i]); i++;
+		HH(b,c,d,a, W[(3*i+5)%16], 23, tab[i]); i++;
+	}
+	while (i < 64) {
+		II(a,b,c,d, W[7*i%16],  6, tab[i]); i++;
+		II(d,a,b,c, W[7*i%16], 10, tab[i]); i++;
+		II(c,d,a,b, W[7*i%16], 15, tab[i]); i++;
+		II(b,c,d,a, W[7*i%16], 21, tab[i]); i++;
+	}
 
-        STEP(F, a, b, c, d, tab[0x00], current[0], 7)
-        STEP(F, d, a, b, c, tab[0x01], current[1], 12)
-        STEP(F, c, d, a, b, tab[0x02], current[2], 17)
-        STEP(F, b, c, d, a, tab[0x03], current[3], 22)
-        STEP(F, a, b, c, d, tab[0x04], current[4], 7)
-        STEP(F, d, a, b, c, tab[0x05], current[5], 12)
-        STEP(F, c, d, a, b, tab[0x06], current[6], 17)
-        STEP(F, b, c, d, a, tab[0x07], current[7], 22)
-        STEP(F, a, b, c, d, tab[0x08], current[8], 7)
-        STEP(F, d, a, b, c, tab[0x09], current[9], 12)
-        STEP(F, c, d, a, b, tab[0x0A], current[10], 17)
-        STEP(F, b, c, d, a, tab[0x0B], current[11], 22)
-        STEP(F, a, b, c, d, tab[0x0C], current[12], 7)
-        STEP(F, d, a, b, c, tab[0x0D], current[13], 12)
-        STEP(F, c, d, a, b, tab[0x0E], current[14], 17)
-        STEP(F, b, c, d, a, tab[0x0F], current[15], 22)
-        STEP(G, a, b, c, d, tab[0x10], current[1], 5)
-        STEP(G, d, a, b, c, tab[0x11], current[6], 9)
-        STEP(G, c, d, a, b, tab[0x12], current[11], 14)
-        STEP(G, b, c, d, a, tab[0x13], current[0], 20)
-        STEP(G, a, b, c, d, tab[0x14], current[5], 5)
-        STEP(G, d, a, b, c, tab[0x15], current[10], 9)
-        STEP(G, c, d, a, b, tab[0x16], current[15], 14)
-        STEP(G, b, c, d, a, tab[0x17], current[4], 20)
-        STEP(G, a, b, c, d, tab[0x18], current[9], 5)
-        STEP(G, d, a, b, c, tab[0x19], current[14], 9)
-        STEP(G, c, d, a, b, tab[0x1A], current[3], 14)
-        STEP(G, b, c, d, a, tab[0x1B], current[8], 20)
-        STEP(G, a, b, c, d, tab[0x1C], current[13], 5)
-        STEP(G, d, a, b, c, tab[0x1D], current[2], 9)
-        STEP(G, c, d, a, b, tab[0x1E], current[7], 14)
-        STEP(G, b, c, d, a, tab[0x1F], current[12], 20)
-        STEP(H, a, b, c, d, tab[0x20], current[5], 4)
-        STEP(H, d, a, b, c, tab[0x21], current[8], 11)
-        STEP(H, c, d, a, b, tab[0x22], current[11], 16)
-        STEP(H, b, c, d, a, tab[0x23], current[14], 23)
-        STEP(H, a, b, c, d, tab[0x24], current[1], 4)
-        STEP(H, d, a, b, c, tab[0x25], current[4], 11)
-        STEP(H, c, d, a, b, tab[0x26], current[7], 16)
-        STEP(H, b, c, d, a, tab[0x27], current[10], 23)
-        STEP(H, a, b, c, d, tab[0x28], current[13], 4)
-        STEP(H, d, a, b, c, tab[0x29], current[0], 11)
-        STEP(H, c, d, a, b, tab[0x2A], current[3], 16)
-        STEP(H, b, c, d, a, tab[0x2B], current[6], 23)
-        STEP(H, a, b, c, d, tab[0x2C], current[9], 4)
-        STEP(H, d, a, b, c, tab[0x2D], current[12], 11)
-        STEP(H, c, d, a, b, tab[0x2E], current[15], 16)
-        STEP(H, b, c, d, a, tab[0x2F], current[2], 23)
-        STEP(I, a, b, c, d, tab[0x30], current[0], 6)
-        STEP(I, d, a, b, c, tab[0x31], current[7], 10)
-        STEP(I, c, d, a, b, tab[0x32], current[14], 15)
-        STEP(I, b, c, d, a, tab[0x33], current[5], 21)
-        STEP(I, a, b, c, d, tab[0x34], current[12], 6)
-        STEP(I, d, a, b, c, tab[0x35], current[3], 10)
-        STEP(I, c, d, a, b, tab[0x36], current[10], 15)
-        STEP(I, b, c, d, a, tab[0x37], current[1], 21)
-        STEP(I, a, b, c, d, tab[0x38], current[8], 6)
-        STEP(I, d, a, b, c, tab[0x39], current[15], 10)
-        STEP(I, c, d, a, b, tab[0x3A], current[6], 15)
-        STEP(I, b, c, d, a, tab[0x3B], current[13], 21)
-        STEP(I, a, b, c, d, tab[0x3C], current[4], 6)
-        STEP(I, d, a, b, c, tab[0x3D], current[11], 10)
-        STEP(I, c, d, a, b, tab[0x3E], current[2], 15)
-        STEP(I, b, c, d, a, tab[0x3F], current[9], 21)
+	s->a += a;
+	s->b += b;
+	s->c += c;
+	s->d += d;
 
-        a += sa;
-        b += sb;
-        c += sc;
-        d += sd;
-        current += 16;
+}
 
-    } while (count -= 64);
+static void pad(struct md5 *s)
+{
 
-    s->a = a;
-    s->b = b;
-    s->c = c;
-    s->d = d;
+    unsigned r = s->lo % 64;
 
-    return current;
+    s->buffer[r++] = 0x80;
+
+    if (r > 56)
+    {
+
+        memory_clear(s->buffer + r, 64 - r);
+
+        r = 0;
+
+        processblock(s, s->buffer);
+
+    }
+
+    memory_clear(s->buffer + r, 56 - r);
+
+    s->lo *= 8;
+    s->buffer[56] = s->lo;
+    s->buffer[57] = s->lo >> 8;
+    s->buffer[58] = s->lo >> 16;
+    s->buffer[59] = s->lo >> 24;
+    s->buffer[60] = s->hi;
+    s->buffer[61] = s->hi >> 8;
+    s->buffer[62] = s->hi >> 16;
+    s->buffer[63] = s->hi >> 24;
+
+    processblock(s, s->buffer);
 
 }
 
 static void md5_read(struct md5 *s, unsigned int count, void *buffer)
 {
 
+    unsigned char *p = buffer;
     unsigned int lo = s->lo;
     unsigned int r = s->lo & 0x3f;
 
@@ -147,61 +135,32 @@ static void md5_read(struct md5 *s, unsigned int count, void *buffer)
         if (count < 64 - r)
         {
 
-            memory_copy(s->buffer + r, buffer, count);
+            memory_copy(s->buffer + r, p, count);
 
             return;
 
         }
 
-        memory_copy(s->buffer + r, buffer, 64 - r);
-        buffer = (unsigned char *)buffer + 64 - r;
+        memory_copy(s->buffer + r, p, 64 - r);
+
+        p += 64 - r;
         count -= 64 - r;
-        body(s, 64, s->buffer);
+
+        processblock(s, s->buffer);
 
     }
 
-    if (count >= 64)
-    {
+    for (; count >= 64; count -= 64, p += 64)
+        processblock(s, p);
 
-        buffer = body(s, count & ~0x3f, buffer);
-        count &= 0x3f;
-
-    }
-
-    memory_copy(s->buffer, buffer, count);
+    memory_copy(s->buffer, p, count);
 
 }
 
 static void md5_write(struct md5 *s, unsigned char digest[16])
 {
 
-    unsigned int r = s->lo & 0x3f;
-
-    s->buffer[r++] = 0x80;
-
-    if (r > 56)
-    {
-
-        memory_clear(s->buffer + r, 64 - r);
-        body(s, 64, s->buffer);
-
-        r = 0;
-
-    }
-
-    memory_clear(s->buffer + r, 56 - r);
-
-    s->lo <<= 3;
-    s->buffer[56] = s->lo;
-    s->buffer[57] = s->lo >> 8;
-    s->buffer[58] = s->lo >> 16;
-    s->buffer[59] = s->lo >> 24;
-    s->buffer[60] = s->hi;
-    s->buffer[61] = s->hi >> 8;
-    s->buffer[62] = s->hi >> 16;
-    s->buffer[63] = s->hi >> 24;
-
-    body(s, 64, s->buffer);
+    pad(s);
 
     digest[0] = s->a;
     digest[1] = s->a >> 8;
