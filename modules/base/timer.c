ae19a480bfc3990ed253f8c579b46b5364a06445
Jens Nyberg <jens.nyberg@gmail.com>
2013-09-14 01:56:41 +0200

    more timer work

--- modules/base/timer.c
+++ modules/base/timer.c
@@ -17,10 +17,75 @@ static struct
 
 } timers[8];
 
-static unsigned int clone_open(struct system_node *self)
+static unsigned int control_open(struct system_node *self)
 {
 
     return (unsigned int)self;
+
+}
+
+static unsigned int control_close(struct system_node *self)
+{
+
+    return (unsigned int)self;
+
+}
+
+static unsigned int control_read(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    return memory_read(buffer, count, "control", 7, offset);
+
+}
+
+static unsigned int control_write(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    return 0;
+
+}
+
+static unsigned int ticks_open(struct system_node *self)
+{
+
+    return (unsigned int)self;
+
+}
+
+static unsigned int ticks_close(struct system_node *self)
+{
+
+    return (unsigned int)self;
+
+}
+
+static unsigned int ticks_read(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    return memory_read(buffer, count, "ticks", 5, offset);
+
+}
+
+static unsigned int ticks_write(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    return 0;
+
+}
+
+static unsigned int clone_open(struct system_node *self)
+{
+
+    ntimers++;
+
+    system_init_group(&timers[ntimers].index, "0");
+    system_group_add(&timer, &timers[ntimers].index.node);
+    system_init_stream(&timers[ntimers].control, "control", control_open, control_close, control_read, control_write);
+    system_group_add(&timers[ntimers].index, &timers[ntimers].control.node);
+    system_init_stream(&timers[ntimers].ticks, "ticks", ticks_open, ticks_close, ticks_read, ticks_write);
+    system_group_add(&timers[ntimers].index, &timers[ntimers].ticks.node);
+
+    return (unsigned int)&timers[ntimers].control;
 
 }
 
@@ -41,12 +106,7 @@ static unsigned int clone_read(struct system_node *self, unsigned int offset, un
 static unsigned int clone_write(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
 {
 
-    ntimers++;
-
-    system_init_group(&timers[ntimers].index, "0");
-    system_group_add(&timer, &timers[ntimers].index.node);
-
-    return ntimers;
+    return 0;
 
 }
 
