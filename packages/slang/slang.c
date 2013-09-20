9c7f7a87b19d65930735c5604693234626b0e24f
Jens Nyberg <jens.nyberg@gmail.com>
2013-09-20 21:16:25 +0200

    more slang work

--- packages/slang/slang.c
+++ packages/slang/slang.c
@@ -31,6 +31,65 @@ static void open_pipe(unsigned int index, unsigned int index0, unsigned int inde
 
 }
 
+static void execute_command(struct command *command, char *buffer)
+{
+
+    if (command->in0.path.count)
+    {
+
+        open_path(CALL_I0, command->in0.path.count, buffer + command->in0.path.index);
+
+    }
+
+    else if (command->in0.data.count)
+    {
+
+        call_open(CALL_L6, CALL_DR, 17, "system/pipe/clone");
+        open_pipe(CALL_L6, CALL_L5, CALL_I0);
+        call_write(CALL_L5, 0, command->in0.data.count, buffer + command->in0.data.index);
+
+    }
+
+    if (command->in1.path.count)
+    {
+
+        open_path(CALL_I1, command->in1.path.count, buffer + command->in1.path.index);
+
+    }
+
+    else if (command->in1.data.count)
+    {
+
+        call_open(CALL_L7, CALL_DR, 17, "system/pipe/clone");
+        open_pipe(CALL_L7, CALL_L5, CALL_I1);
+        call_write(CALL_L5, 0, command->in1.data.count, buffer + command->in1.data.index);
+
+    }
+
+    if (command->out0.path.count)
+        open_path(CALL_O0, command->out0.path.count, buffer + command->out0.path.index);
+
+    call_open(CALL_DW, CALL_DR, 4, "bin/");
+    open_path(CALL_E0, command->binary.count, buffer + command->binary.index);
+    call_open(CALL_DW, CALL_L3, 0, 0);
+    call_spawn(CALL_E0);
+    call_close(CALL_E0);
+
+    if (command->in0.path.count)
+        call_close(CALL_I0);
+    else if (command->in0.data.count)
+        call_close(CALL_L6);
+
+    if (command->in1.path.count)
+        call_close(CALL_I1);
+    else if (command->in1.data.count)
+        call_close(CALL_L7);
+
+    if (command->out0.path.count)
+        call_close(CALL_O0);
+
+}
+
 static void execute(struct token_state *state, struct expression *expression)
 {
 
@@ -50,76 +109,20 @@ static void execute(struct token_state *state, struct expression *expression)
         for (cindex = 0; cindex < pipe->count - 1; cindex++)
             call_open(CALL_P0 + cindex, CALL_DR, 17, "system/pipe/clone");
 
-        for (cindex = 0; cindex < pipe->count; cindex++)
+        for (cindex = 0; cindex < pipe->count - 1; cindex++)
         {
 
-            struct command *command = &pipe->commands[cindex];
-
-            if (cindex == pipe->count - 1)
-                call_open(CALL_O0, CALL_L1, 0, 0);
-            else
-                open_pipe(CALL_P0 + cindex, CALL_O0, 0);
-
-            if (command->in0.path.count)
-            {
-
-                open_path(CALL_I0, command->in0.path.count, state->buffer + command->in0.path.index);
-
-            }
-
-            else if (command->in0.data.count)
-            {
-
-                call_open(CALL_L6, CALL_DR, 17, "system/pipe/clone");
-                open_pipe(CALL_L6, CALL_L5, CALL_I0);
-                call_write(CALL_L5, 0, command->in0.data.count, state->buffer + command->in0.data.index);
-
-            }
-
-            if (command->in1.path.count)
-            {
-
-                open_path(CALL_I1, command->in1.path.count, state->buffer + command->in1.path.index);
-
-            }
-
-            else if (command->in1.data.count)
-            {
-
-                call_open(CALL_L7, CALL_DR, 17, "system/pipe/clone");
-                open_pipe(CALL_L7, CALL_L5, CALL_I1);
-                call_write(CALL_L5, 0, command->in1.data.count, state->buffer + command->in1.data.index);
-
-            }
-
-            if (command->out0.path.count)
-                open_path(CALL_O0, command->out0.path.count, state->buffer + command->out0.path.index);
-
-            call_open(CALL_DW, CALL_DR, 4, "bin/");
-            open_path(CALL_E0, command->binary.count, state->buffer + command->binary.index);
-            call_open(CALL_DW, CALL_L3, 0, 0);
-            call_spawn(CALL_E0);
-            call_close(CALL_E0);
-
-            if (command->in0.path.count)
-                call_close(CALL_I0);
-            else if (command->in0.data.count)
-                call_close(CALL_L6);
-
-            if (command->in1.path.count)
-                call_close(CALL_I1);
-            else if (command->in1.data.count)
-                call_close(CALL_L7);
-
-            if (command->out0.path.count)
-                call_close(CALL_O0);
-
+            open_pipe(CALL_P0 + cindex, CALL_O0, 0);
+            execute_command(&pipe->commands[cindex], state->buffer);
             open_pipe(CALL_P0 + cindex, 0, CALL_I0);
 
         }
 
         for (cindex = 0; cindex < pipe->count - 1; cindex++)
             call_close(CALL_P0 + cindex);
+
+        call_open(CALL_O0, CALL_L1, 0, 0);
+        execute_command(&pipe->commands[pipe->count - 1], state->buffer);
 
     }
 
