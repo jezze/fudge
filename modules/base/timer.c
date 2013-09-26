f9290ae2de19eabcb2c5afc7c214e2b1d9267d01
Jens Nyberg <jens.nyberg@gmail.com>
2013-09-27 00:32:20 +0200

    more timer work

--- modules/base/timer.c
+++ modules/base/timer.c
@@ -13,8 +13,75 @@ static struct timer_node
 
 } node[8];
 
+static struct timer_session
+{
+
+    struct system_group base;
+    struct system_stream control;
+    struct system_stream sleep;
+    char name[8];
+
+} session[8];
+
 static struct system_group root;
 static struct system_group dev;
+static struct system_stream clone;
+
+static unsigned int control_read(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    struct timer_session *session = (struct timer_session *)self->parent;
+
+    return memory_read(buffer, count, session->name, string_length(session->name), offset);
+
+}
+
+static unsigned int find_session()
+{
+
+    unsigned int i;
+
+    for (i = 1; i < 8; i++)
+    {
+
+        if (!session[i].base.node.parent)
+            return i;
+
+    }
+
+    return 0;
+
+}
+
+static void init_session(struct timer_session *session, unsigned int id)
+{
+
+    memory_clear(session, sizeof (struct timer_session));
+    memory_write_number(session->name, 8, id, 10, 0);
+    system_init_group(&session->base, session->name);
+    system_init_stream(&session->control, "control");
+    system_init_stream(&session->sleep, "sleep");
+
+    session->control.node.read = control_read;
+
+}
+
+static unsigned int clone_open(struct system_node *self)
+{
+
+    unsigned int index = find_session();
+
+    if (!index)
+        return 0;
+
+    init_session(&session[index], index);
+    system_group_add(&root, &session[index].base.node);
+    system_group_add(&session[index].base, &session[index].control.node);
+    system_group_add(&session[index].base, &session[index].sleep.node);
+
+    return (unsigned int)&session[index].control;
+
+}
 
 static unsigned int find_node()
 {
@@ -71,10 +138,15 @@ void base_timer_setup()
 {
 
     memory_clear(node, sizeof (struct timer_node) * 8);
+    memory_clear(session, sizeof (struct timer_session) * 8);
     system_init_group(&root, "timer");
     system_register_node(&root.node);
     system_init_group(&dev, "dev");
     system_group_add(&root, &dev.node);
+    system_init_stream(&clone, "clone");
+    system_group_add(&root, &clone.node);
+
+    clone.node.open = clone_open;
 
 }
 
