ef58a4d7edd2b03eb3132bd8d85628bf4238f758
Jens Nyberg <jens.nyberg@gmail.com>
2013-09-28 17:21:59 +0200

    added some basic timestamp

--- modules/base/clock.c
+++ modules/base/clock.c
@@ -18,13 +18,50 @@ static struct clock_node
 
 static struct system_group root;
 static struct system_group dev;
+static unsigned int dotm365[13] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+static unsigned int dotm366[13] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 365};
+
+static unsigned int isleapyear(unsigned short year)
+{
+
+    if ((year % 4) != 0)
+        return 0;
+
+    if ((year % 100) == 0)
+        return ((year % 400) == 0);
+
+    return 1;
+
+}
+
+static unsigned int dateticks(unsigned int year, unsigned int month, unsigned int day)
+{
+
+    unsigned int dyear = 365 * (year - 1970);
+    unsigned int dmonth = isleapyear(year) ? dotm366[month - 1] : dotm365[month - 1];
+
+    return (dyear + dmonth + day) * 86400;
+
+}
+
+static unsigned int timeticks(unsigned int hour, unsigned int minute, unsigned int second)
+{
+
+    return ((hour * 3600) + (minute * 60) + second);
+
+}
 
 static unsigned int timestamp_read(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
 {
 
+    struct clock_node *node = (struct clock_node *)self->parent;
     char num[32];
+    unsigned int timestamp = 0;
+    
+    timestamp += dateticks(node->interface->get_year(node->device), node->interface->get_month(node->device), node->interface->get_day(node->device));
+    timestamp += timeticks(node->interface->get_hours(node->device), node->interface->get_minutes(node->device), node->interface->get_seconds(node->device));
 
-    return memory_read(buffer, count, num, memory_write_number(num, 32, 0, 10, 0), offset);
+    return memory_read(buffer, count, num, memory_write_number(num, 32, timestamp, 10, 0), offset);
 
 }
 
