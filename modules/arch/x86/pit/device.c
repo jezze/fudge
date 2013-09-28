77ba5dee047a4a694f0a97808ae5be5f1156109e
Jens Nyberg <jens.nyberg@gmail.com>
2013-09-28 23:04:44 +0200

    removed potential bug

--- modules/arch/x86/pit/device.c
+++ modules/arch/x86/pit/device.c
@@ -10,7 +10,6 @@ void pit_init_device(struct pit_device *device, unsigned int irq)
 
     memory_clear(device, sizeof (struct pit_device));
     base_init_device(&device->base, PIT_DEVICE_TYPE, irq, "pit", 0);
-    memory_write_paddednumber(device->base.module.name, 13, irq, 16, 4, 9);
 
 }
 
