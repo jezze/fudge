5be17ae7ada6b193f6379af6992dd8b0a18adfb7
Jens Nyberg <jens.nyberg@gmail.com>
2014-02-02 18:42:58 +0100

    minor cleanups

--- libs/rules.mk
+++ libs/rules.mk
@@ -3,6 +3,6 @@ $(LIBS_PATH)/%.o: CFLAGS+=-I$(LIBS_PATH)
 include $(LIBS_PATH)/fudge/rules.mk
 include $(LIBS_PATH)/kernel/rules.mk
 include $(LIBS_PATH)/posix/rules.mk
-include $(LIBS_ARCH_PATH)/arch/rules.mk
-include $(LIBS_ARCH_PATH)/abi/rules.mk
-include $(LIBS_ARCH_PATH)/$(LOADER)/rules.mk
+include $(LIBS_PATH)/$(ARCH)/arch/rules.mk
+include $(LIBS_PATH)/$(ARCH)/abi/rules.mk
+include $(LIBS_PATH)/$(ARCH)/$(LOADER)/rules.mk
