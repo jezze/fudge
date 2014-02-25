1bb7f1f0e679c8ffbb49dc389d3863606e30829e
Jens Nyberg <jens.nyberg@gmail.com>
2014-02-25 01:25:32 +0100

    add lorem text

--- packages/share/rules.mk
+++ packages/share/rules.mk
@@ -1,3 +1,3 @@
-SHARES:=$(PACKAGES_PATH)/share/design.png $(PACKAGES_PATH)/share/executive-en.png $(PACKAGES_PATH)/share/font2.png $(PACKAGES_PATH)/share/fontall.png $(PACKAGES_PATH)/share/font.png $(PACKAGES_PATH)/share/help.txt $(PACKAGES_PATH)/share/icons.pcx $(PACKAGES_PATH)/share/mi.pcx $(PACKAGES_PATH)/share/motd.txt $(PACKAGES_PATH)/share/test.slang $(PACKAGES_PATH)/share/vancouver-en.png
+SHARES:=$(PACKAGES_PATH)/share/design.png $(PACKAGES_PATH)/share/executive-en.png $(PACKAGES_PATH)/share/font2.png $(PACKAGES_PATH)/share/fontall.png $(PACKAGES_PATH)/share/font.png $(PACKAGES_PATH)/share/help.txt $(PACKAGES_PATH)/share/icons.pcx $(PACKAGES_PATH)/share/lorem.txt $(PACKAGES_PATH)/share/mi.pcx $(PACKAGES_PATH)/share/motd.txt $(PACKAGES_PATH)/share/test.slang $(PACKAGES_PATH)/share/vancouver-en.png
 
 PACKAGES_SHARES+=$(SHARES)
