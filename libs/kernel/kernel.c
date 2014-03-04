1f179279939b3f214c749c381f3b3933024c04f1
Jens Nyberg <jens.nyberg@gmail.com>
2014-03-04 16:30:44 +0100

    some cleanups

--- libs/kernel/kernel.c
+++ libs/kernel/kernel.c
@@ -21,61 +21,73 @@ static struct
 
 } state;
 
+static unsigned int kernel_find_init(struct vfs_channel *channel)
+{
+
+    unsigned int id = channel->protocol->root(channel->backend);
+    struct binary_protocol *protocol;
+
+    if (!id)
+        return 0;
+
+    id = channel->protocol->walk(channel->backend, id, 8, "bin/init");
+
+    if (!id)
+        return 0;
+
+    protocol = binary_find_protocol(channel, id);
+
+    if (!protocol)
+        return 0;
+
+    return protocol->copy_program(channel, id);
+
+}
+
 void kernel_setup_modules(struct container *container, struct task *task, unsigned int count, struct kernel_module *modules)
 {
 
-    struct vfs_channel *channel = &container->channels[0x01];
+    struct vfs_channel *channel1 = &container->channels[0x01];
+    struct vfs_channel *channel2 = &container->channels[0x02];
+    struct vfs_descriptor *descriptor = &task->descriptors[0x01];
+    struct vfs_mount *mount1 = &container->mounts[0x01];
+    struct vfs_mount *mount4 = &container->mounts[0x04];
+    unsigned int entry;
     unsigned int i;
+
+    channel1->backend = &state.vfs.backends[0];
+    channel1->protocol = &state.vfs.protocols[0];
 
     for (i = 0; i < count; i++)
     {
 
-        struct binary_protocol *protocol;
-        unsigned int id;
+        channel2->backend = &modules[i].base;
+        channel2->protocol = vfs_find_protocol(channel2->backend);
 
-        channel->backend = &modules[i].base;
-        channel->protocol = vfs_find_protocol(channel->backend);
-
-        if (!channel->protocol)
+        if (!channel2->protocol)
             continue;
 
-        id = channel->protocol->root(channel->backend);
+        entry = kernel_find_init(channel2);
 
-        if (!id)
-            continue;
-
-        id = channel->protocol->walk(channel->backend, id, 8, "bin/init");
-
-        if (!id)
-            continue;
-
-        protocol = binary_find_protocol(channel, id);
-
-        if (!protocol)
-            continue;
-
-        container->mounts[0x01].parent.channel = channel;
-        container->mounts[0x01].parent.id = channel->protocol->root(channel->backend);
-        container->mounts[0x01].child.channel = channel;
-        container->mounts[0x01].child.id = channel->protocol->root(channel->backend);
-        task->descriptors[0x01].channel = channel;
-        task->descriptors[0x01].id = channel->protocol->root(channel->backend);
-        task->registers.ip = protocol->copy_program(channel, id);
-
-        error_assert(task->registers.ip != 0, "Failed to locate entry point", __FILE__, __LINE__);
-
-        container->mounts[0x04].parent.channel = channel;
-        container->mounts[0x04].parent.id = channel->protocol->walk(channel->backend, channel->protocol->root(channel->backend), 7, "kernel/");
-        container->mounts[0x04].child.channel = &container->channels[0x02];
-        container->mounts[0x04].child.channel->backend = &state.vfs.backends[0];
-        container->mounts[0x04].child.channel->protocol = &state.vfs.protocols[0];
-        container->mounts[0x04].child.id = container->mounts[0x04].child.channel->protocol->root(channel->backend);
-
-        return;
+        if (entry)
+            break;
 
     }
 
-    error_panic("Failed to locate init", __FILE__, __LINE__);
+    task->registers.ip = entry;
+
+    error_assert(task->registers.ip != 0, "Failed to locate entry point", __FILE__, __LINE__);
+
+    descriptor->channel = channel2;
+    descriptor->id = channel2->protocol->root(channel2->backend);
+    mount1->parent.channel = channel2;
+    mount1->parent.id = channel2->protocol->root(channel2->backend);
+    mount1->child.channel = channel2;
+    mount1->child.id = channel2->protocol->root(channel2->backend);
+    mount4->parent.channel = channel2;
+    mount4->parent.id = channel2->protocol->walk(channel2->backend, channel2->protocol->root(channel2->backend), 7, "kernel/");
+    mount4->child.channel = channel1;
+    mount4->child.id = channel1->protocol->root(channel1->backend);
 
 }
 
