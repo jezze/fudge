b88e6e40694d6e873d5823ab74c3fa035d5cd22c
Jens Nyberg <jens.nyberg@gmail.com>
2014-04-19 16:17:44 +0200

    rewrote mmu handling

--- libs/x86/arch/arch.c
+++ libs/x86/arch/arch.c
@@ -22,29 +22,29 @@
 #define ARCH_KSTACK_BASE                0x00280000
 #define ARCH_KSTACK_LIMIT               0x00300000
 #define ARCH_KSTACK_SIZE                (ARCH_KSTACK_LIMIT - ARCH_KSTACK_BASE)
-#define ARCH_DIRECTORY_BASE             0x00300000
-#define ARCH_DIRECTORY_LIMIT            0x00340000
-#define ARCH_DIRECTORY_SIZE             (ARCH_DIRECTORY_LIMIT - ARCH_DIRECTORY_BASE)
-#define ARCH_TABLE_KCODE_BASE           0x00340000
-#define ARCH_TABLE_KCODE_LIMIT          0x00380000
+#define ARCH_DIRECTORY_KCODE_BASE       0x00300000
+#define ARCH_DIRECTORY_KCODE_LIMIT      0x00310000
+#define ARCH_DIRECTORY_KCODE_SIZE       (ARCH_DIRECTORY_KCODE_LIMIT - ARCH_DIRECTORY_KCODE_BASE)
+#define ARCH_TABLE_KCODE_BASE           0x00310000
+#define ARCH_TABLE_KCODE_LIMIT          0x00340000
 #define ARCH_TABLE_KCODE_SIZE           (ARCH_TABLE_KCODE_LIMIT - ARCH_TABLE_KCODE_BASE)
+#define ARCH_DIRECTORY_UCODE_BASE       0x00340000
+#define ARCH_DIRECTORY_UCODE_LIMIT      0x00380000
+#define ARCH_DIRECTORY_UCODE_SIZE       (ARCH_DIRECTORY_UCODE_LIMIT - ARCH_DIRECTORY_UCODE_BASE)
 #define ARCH_TABLE_UCODE_BASE           0x00380000
-#define ARCH_TABLE_UCODE_LIMIT          0x003C0000
+#define ARCH_TABLE_UCODE_LIMIT          0x00400000
 #define ARCH_TABLE_UCODE_SIZE           (ARCH_TABLE_UCODE_LIMIT - ARCH_TABLE_UCODE_BASE)
-#define ARCH_TABLE_USTACK_BASE          0x003C0000
-#define ARCH_TABLE_USTACK_LIMIT         0x00400000
-#define ARCH_TABLE_USTACK_SIZE          (ARCH_TABLE_USTACK_LIMIT - ARCH_TABLE_USTACK_BASE)
-#define ARCH_UCODE_BASE                 0x00800000
-#define ARCH_UCODE_LIMIT                0x00C00000
+#define ARCH_UCODE_BASE                 0x01000000
+#define ARCH_UCODE_LIMIT                0x01400000
 #define ARCH_UCODE_SIZE                 (ARCH_UCODE_LIMIT - ARCH_UCODE_BASE)
-#define ARCH_USTACK_BASE                0x00C00000
-#define ARCH_USTACK_LIMIT               0x01000000
+#define ARCH_USTACK_BASE                0x01400000
+#define ARCH_USTACK_LIMIT               0x01800000
 #define ARCH_USTACK_SIZE                (ARCH_USTACK_LIMIT - ARCH_USTACK_BASE)
 #define ARCH_GDT_DESCRIPTORS            6
 #define ARCH_IDT_DESCRIPTORS            256
 #define ARCH_TSS_DESCRIPTORS            1
-#define ARCH_CONTAINERS                 8
-#define ARCH_CONTAINER_MAPPINGS         2
+#define ARCH_CONTAINERS                 16
+#define ARCH_CONTAINER_MAPPINGS         3
 #define ARCH_TASKS                      64
 #define ARCH_TASK_MAPPINGS              2
 #define ARCH_TASK_CODESIZE              (ARCH_UCODE_SIZE / ARCH_TASKS)
@@ -52,21 +52,12 @@
 #define ARCH_TASK_STACKLIMIT            0x80000000
 #define ARCH_TASK_STACKBASE             (ARCH_TASK_STACKLIMIT - ARCH_TASK_STACKSIZE)
 
-struct arch_mapping
-{
-
-    struct mmu_table *table;
-    unsigned int paddress;
-    unsigned int vaddress;
-    unsigned int size;
-
-};
-
 struct arch_container
 {
 
     struct container base;
-    struct arch_mapping mapping[ARCH_CONTAINER_MAPPINGS];
+    struct mmu_directory *directory;
+    struct mmu_table *table;
 
 };
 
@@ -76,7 +67,8 @@ struct arch_task
     struct task base;
     struct cpu_general general;
     struct mmu_directory *directory;
-    struct arch_mapping mapping[ARCH_TASK_MAPPINGS];
+    struct mmu_table *table;
+    unsigned int mapping[ARCH_TASK_MAPPINGS];
 
 };
 
@@ -104,8 +96,7 @@ static void activate_task(struct container *c, struct task *t)
     struct arch_container *container = (struct arch_container *)c;
     struct arch_task *task = (struct arch_task *)t;
 
-    memory_clear(task->directory, sizeof (struct mmu_directory));
-    mmu_map(task->directory, container->mapping[0].table, container->mapping[0].paddress, container->mapping[0].vaddress, container->mapping[0].size, MMU_TFLAG_PRESENT | MMU_TFLAG_WRITEABLE, MMU_PFLAG_PRESENT | MMU_PFLAG_WRITEABLE);
+    memory_copy(task->directory, container->directory, sizeof (struct mmu_directory));
     mmu_load(task->directory);
 
 }
@@ -223,11 +214,10 @@ unsigned short arch_pagefault(void *stack)
     {
 
         struct arch_task *task = (struct arch_task *)scheduler_find_used_task();
+        unsigned int address = cpu_get_cr2();
 
-        task->mapping[0].vaddress = cpu_get_cr2();
-
-        mmu_map(task->directory, task->mapping[0].table, task->mapping[0].paddress, task->mapping[0].vaddress, task->mapping[0].size, MMU_TFLAG_PRESENT | MMU_TFLAG_WRITEABLE | MMU_TFLAG_USERMODE, MMU_PFLAG_PRESENT | MMU_PFLAG_WRITEABLE | MMU_PFLAG_USERMODE);
-        mmu_map(task->directory, task->mapping[1].table, task->mapping[1].paddress, task->mapping[1].vaddress, task->mapping[1].size, MMU_TFLAG_PRESENT | MMU_TFLAG_WRITEABLE | MMU_TFLAG_USERMODE, MMU_PFLAG_PRESENT | MMU_PFLAG_WRITEABLE | MMU_PFLAG_USERMODE);
+        mmu_map(task->directory, &task->table[0], task->mapping[0], address, ARCH_TASK_CODESIZE, MMU_TFLAG_PRESENT | MMU_TFLAG_WRITEABLE | MMU_TFLAG_USERMODE, MMU_PFLAG_PRESENT | MMU_PFLAG_WRITEABLE | MMU_PFLAG_USERMODE);
+        mmu_map(task->directory, &task->table[1], task->mapping[1], ARCH_TASK_STACKBASE, ARCH_TASK_STACKSIZE, MMU_TFLAG_PRESENT | MMU_TFLAG_WRITEABLE | MMU_TFLAG_USERMODE, MMU_PFLAG_PRESENT | MMU_PFLAG_WRITEABLE | MMU_PFLAG_USERMODE);
 
         return state.kdata;
 
@@ -251,43 +241,40 @@ unsigned short arch_syscall(void *stack)
 static void arch_setup_entities()
 {
 
-    struct mmu_directory *directories = (struct mmu_directory *)ARCH_DIRECTORY_BASE;
-    struct mmu_table *ktables = (struct mmu_table *)ARCH_TABLE_KCODE_BASE;
-    struct mmu_table *ucodetables = (struct mmu_table *)ARCH_TABLE_UCODE_BASE;
-    struct mmu_table *ustacktables = (struct mmu_table *)ARCH_TABLE_USTACK_BASE;
     unsigned int i;
 
     for (i = 0; i < ARCH_CONTAINERS; i++)
     {
+
+        struct mmu_directory *directories = (struct mmu_directory *)ARCH_DIRECTORY_KCODE_BASE;
+        struct mmu_table *tables = (struct mmu_table *)ARCH_TABLE_KCODE_BASE;
 
         container_init(&state.containers[i].base);
         resource_register_item(&state.containers[i].base.resource);
 
         state.containers[i].base.calls[CONTAINER_CALL_SPAWN] = spawn;
         state.containers[i].base.calls[CONTAINER_CALL_EXIT] = exit;
-        state.containers[i].mapping[0].table = &ktables[i];
-        state.containers[i].mapping[0].paddress = ARCH_BIOS_BASE;
-        state.containers[i].mapping[0].vaddress = ARCH_BIOS_BASE;
-        state.containers[i].mapping[0].size = ARCH_TABLE_USTACK_LIMIT - ARCH_BIOS_BASE;
+        state.containers[i].directory = directories + i;
+        state.containers[i].table = tables + i * ARCH_CONTAINER_MAPPINGS;
+
+        mmu_map(state.containers[i].directory, &state.containers[i].table[0], ARCH_BIOS_BASE, ARCH_BIOS_BASE, ARCH_TABLE_UCODE_LIMIT - ARCH_BIOS_BASE, MMU_TFLAG_PRESENT | MMU_TFLAG_WRITEABLE, MMU_PFLAG_PRESENT | MMU_PFLAG_WRITEABLE);
 
     }
 
     for (i = 0; i < ARCH_TASKS; i++)
     {
 
+        struct mmu_directory *directories = (struct mmu_directory *)ARCH_DIRECTORY_UCODE_BASE;
+        struct mmu_table *tables = (struct mmu_table *)ARCH_TABLE_UCODE_BASE;
+
         task_init(&state.tasks[i].base, 0, ARCH_TASK_STACKLIMIT);
         resource_register_item(&state.tasks[i].base.resource);
         scheduler_register_task(&state.tasks[i].base);
 
-        state.tasks[i].directory = &directories[i];
-        state.tasks[i].mapping[0].table = &ucodetables[i];
-        state.tasks[i].mapping[0].paddress = ARCH_UCODE_BASE + ARCH_TASK_CODESIZE * i;
-        state.tasks[i].mapping[0].vaddress = 0;
-        state.tasks[i].mapping[0].size = ARCH_TASK_CODESIZE;
-        state.tasks[i].mapping[1].table = &ustacktables[i];
-        state.tasks[i].mapping[1].paddress = ARCH_USTACK_BASE + ARCH_TASK_STACKSIZE * i;
-        state.tasks[i].mapping[1].vaddress = ARCH_TASK_STACKBASE;
-        state.tasks[i].mapping[1].size = ARCH_TASK_STACKSIZE;
+        state.tasks[i].directory = directories + i;
+        state.tasks[i].table = tables + i * ARCH_TASK_MAPPINGS;
+        state.tasks[i].mapping[0] = ARCH_UCODE_BASE + ARCH_TASK_CODESIZE * i;
+        state.tasks[i].mapping[1] = ARCH_USTACK_BASE + ARCH_TASK_STACKSIZE * i;
 
     }
 
