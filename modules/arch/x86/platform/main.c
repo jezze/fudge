05c9e537b5eb661f69077b432191eaed975748a4
Jens Nyberg <jens.nyberg@gmail.com>
2014-08-30 15:16:42 +0200

    more cleanups
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- modules/arch/x86/platform/main.c
+++ modules/arch/x86/platform/main.c
@@ -8,10 +8,10 @@ enum platform_register
 
     PLATFORM_REGISTER_PIT               = 0x0040,
     PLATFORM_REGISTER_RTC               = 0x0070,
-    PLATFORM_REGISTER_UART1             = 0x03F8,
+    PLATFORM_REGISTER_UART4             = 0x02E8,
     PLATFORM_REGISTER_UART2             = 0x02F8,
     PLATFORM_REGISTER_UART3             = 0x03E8,
-    PLATFORM_REGISTER_UART4             = 0x02E8
+    PLATFORM_REGISTER_UART1             = 0x03F8
 
 };
 
@@ -19,41 +19,36 @@ enum platform_irq
 {
 
     PLATFORM_IRQ_PIT                    = 0x00,
-    PLATFORM_IRQ_RTC                    = 0x08,
+    PLATFORM_IRQ_UART2                  = 0x03,
     PLATFORM_IRQ_UART1                  = 0x04,
-    PLATFORM_IRQ_UART2                  = 0x03
+    PLATFORM_IRQ_RTC                    = 0x08
+
+};
+
+struct device
+{
+
+    unsigned short base;
+    unsigned short irq;
 
 };
 
 static struct base_bus bus;
 
-unsigned short platform_bus_get_base(struct base_bus *bus, unsigned int type)
+static struct device devices[] = {
+    {0, 0},
+    {PLATFORM_REGISTER_PIT, PLATFORM_IRQ_PIT},
+    {PLATFORM_REGISTER_RTC, PLATFORM_IRQ_RTC},
+    {PLATFORM_REGISTER_UART1, PLATFORM_IRQ_UART1},
+    {PLATFORM_REGISTER_UART2, PLATFORM_IRQ_UART2},
+    {PLATFORM_REGISTER_UART3, PLATFORM_IRQ_UART1},
+    {PLATFORM_REGISTER_UART4, PLATFORM_IRQ_UART2}
+};
+
+unsigned short platform_bus_get_base(struct base_bus *bus, unsigned int id)
 {
 
-    switch (type)
-    {
-
-        case PLATFORM_PIT_DEVICE_TYPE:
-            return PLATFORM_REGISTER_PIT;
-
-        case PLATFORM_RTC_DEVICE_TYPE:
-            return PLATFORM_REGISTER_RTC;
-
-        case PLATFORM_UART1_DEVICE_TYPE:
-            return PLATFORM_REGISTER_UART1;
-
-        case PLATFORM_UART2_DEVICE_TYPE:
-            return PLATFORM_REGISTER_UART2;
-
-        case PLATFORM_UART3_DEVICE_TYPE:
-            return PLATFORM_REGISTER_UART3;
-
-        case PLATFORM_UART4_DEVICE_TYPE:
-            return PLATFORM_REGISTER_UART4;
-
-    }
-
-    return 0xFFFF;
+    return (id < 7) ? devices[id].base : 0;
 
 }
 
@@ -65,60 +60,14 @@ static void setup(struct base_bus *self)
 static unsigned int device_next(struct base_bus *self, unsigned int id)
 {
 
-    switch (id)
-    {
-
-        case 0:
-            return PLATFORM_PIT_DEVICE_TYPE;
-
-        case PLATFORM_PIT_DEVICE_TYPE:
-            return PLATFORM_RTC_DEVICE_TYPE;
-
-        case PLATFORM_RTC_DEVICE_TYPE:
-            return PLATFORM_UART1_DEVICE_TYPE;
-
-        case PLATFORM_UART1_DEVICE_TYPE:
-            return PLATFORM_UART2_DEVICE_TYPE;
-
-        case PLATFORM_UART2_DEVICE_TYPE:
-            return PLATFORM_UART3_DEVICE_TYPE;
-
-        case PLATFORM_UART3_DEVICE_TYPE:
-            return PLATFORM_UART4_DEVICE_TYPE;
-
-    }
-
-    return 0;
+    return (id < 6) ? id + 1 : 0;
 
 }
 
 static unsigned short device_irq(struct base_bus *self, unsigned int id)
 {
 
-    switch (id)
-    {
-
-        case PLATFORM_PIT_DEVICE_TYPE:
-            return PLATFORM_IRQ_PIT;
-
-        case PLATFORM_RTC_DEVICE_TYPE:
-            return PLATFORM_IRQ_RTC;
-
-        case PLATFORM_UART1_DEVICE_TYPE:
-            return PLATFORM_IRQ_UART1;
-
-        case PLATFORM_UART2_DEVICE_TYPE:
-            return PLATFORM_IRQ_UART2;
-
-        case PLATFORM_UART3_DEVICE_TYPE:
-            return PLATFORM_IRQ_UART1;
-
-        case PLATFORM_UART4_DEVICE_TYPE:
-            return PLATFORM_IRQ_UART2;
-
-    }
-
-    return 0xFFFF;
+    return (id < 7) ? devices[id].irq : 0xFFFF;
 
 }
 
--- modules/arch/x86/platform/platform.h
+++ modules/arch/x86/platform/platform.h
@@ -1,9 +1,9 @@
 #define PLATFORM_BUS_TYPE               0x0001
-#define PLATFORM_PIT_DEVICE_TYPE        0x8601
-#define PLATFORM_RTC_DEVICE_TYPE        0x8602
-#define PLATFORM_UART1_DEVICE_TYPE      0x8603
-#define PLATFORM_UART2_DEVICE_TYPE      0x8604
-#define PLATFORM_UART3_DEVICE_TYPE      0x8605
-#define PLATFORM_UART4_DEVICE_TYPE      0x8606
+#define PLATFORM_PIT_DEVICE_TYPE        0x0001
+#define PLATFORM_RTC_DEVICE_TYPE        0x0002
+#define PLATFORM_UART1_DEVICE_TYPE      0x0003
+#define PLATFORM_UART2_DEVICE_TYPE      0x0004
+#define PLATFORM_UART3_DEVICE_TYPE      0x0005
+#define PLATFORM_UART4_DEVICE_TYPE      0x0006
 
-unsigned short platform_bus_get_base(struct base_bus *bus, unsigned int type);
+unsigned short platform_bus_get_base(struct base_bus *bus, unsigned int id);
