9c34b3c54feec42a013ca70d0f296c44c2abe214
Jens Nyberg <jens.nyberg@gmail.com>
2014-09-02 23:05:54 +0200

    some pcx fixes
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- libs/gfx/pcx.c
+++ libs/gfx/pcx.c
@@ -68,7 +68,7 @@ void pcx_load(struct pcx_surface *surface)
 
     unsigned char magic;
 
-    call_read(CALL_I0, 0, sizeof (struct pcx_header), &surface->header);
+    call_read(surface->id, 0, sizeof (struct pcx_header), &surface->header);
 
     surface->base.width = surface->header.xend - surface->header.xstart + 1;
     surface->base.height = surface->header.yend - surface->header.ystart + 1;
--- packages/pcx/pcxcmap.c
+++ packages/pcx/pcxcmap.c
@@ -8,12 +8,12 @@ void main()
 {
 
     call_open(CALL_I0);
-    pcx_initsurface(&pcx, CALL_I0);
-    call_close(CALL_I0);
-    pcx_load(&pcx);
     call_open(CALL_O0);
+    pcx_initsurface(&pcx, CALL_I0);
+    pcx_load(&pcx);
     call_write(CALL_O0, 0, 768, pcx.colormap);
     call_close(CALL_O0);
+    call_close(CALL_I0);
 
 }
 
