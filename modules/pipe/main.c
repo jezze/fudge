f34dc3361e3035cf463638b39f68c1ff67e1ebbb
Jens Nyberg <jens.nyberg@gmail.com>
2014-09-14 00:54:43 +0200

    minor pipe fix
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- modules/pipe/main.c
+++ modules/pipe/main.c
@@ -35,12 +35,12 @@ static unsigned int pipe0_read(struct system_node *self, unsigned int offset, un
 static unsigned int pipe0_write(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
 {
 
-    count = buffer_wcfifo(&pipe0.cfifo, count, buffer);
+    unsigned int c = buffer_wcfifo(&pipe0.cfifo, count, buffer);
 
-    scheduler_rendezvous_sleep(&pipe0.wdata, !count);
+    scheduler_rendezvous_sleep(&pipe0.wdata, !c && count);
     scheduler_rendezvous_unsleep(&pipe1.rdata);
 
-    return count;
+    return c;
 
 }
 
@@ -59,12 +59,12 @@ static unsigned int pipe1_read(struct system_node *self, unsigned int offset, un
 static unsigned int pipe1_write(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
 {
 
-    count = buffer_wcfifo(&pipe1.cfifo, count, buffer);
+    unsigned int c = buffer_wcfifo(&pipe1.cfifo, count, buffer);
 
-    scheduler_rendezvous_sleep(&pipe1.wdata, !count);
+    scheduler_rendezvous_sleep(&pipe1.wdata, !c && count);
     scheduler_rendezvous_unsleep(&pipe0.rdata);
 
-    return count;
+    return c;
 
 }
 
