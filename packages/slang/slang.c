14787810a12d42b126cbc660a6ca2c1927feeef5
Jens Nyberg <jens.nyberg@gmail.com>
2014-10-27 19:18:44 +0100

    minor cleanups
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- packages/slang/slang.c
+++ packages/slang/slang.c
@@ -300,6 +300,7 @@ static void parse(struct tokenlist *postfix, struct tokenlist *stack)
             call_walk(CALL_O1, CALL_L1, 1, "0");
             call_spawn(CALL_P0);
             call_walk(CALL_I1, CALL_L1, 1, "1");
+            call_walk(CALL_O1, CALL_O0, 0, 0);
             call_close(CALL_L1);
 
             break;
@@ -313,9 +314,9 @@ static void parse(struct tokenlist *postfix, struct tokenlist *stack)
             if (!walk_path(CALL_P0, CALL_L0, ascii_length(t->str), t->str))
                 return;
 
-            call_walk(CALL_O1, CALL_O0, 0, 0);
             call_spawn(CALL_P0);
             call_walk(CALL_I1, CALL_I0, 0, 0);
+            call_walk(CALL_O1, CALL_O0, 0, 0);
 
             break;
 
