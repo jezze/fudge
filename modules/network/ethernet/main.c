94e7eadcd7186ef3a307126297ec5b26e7c111e4
Jens Nyberg <jens.nyberg@gmail.com>
2014-12-08 18:58:47 +0100

    more ethernet work
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- modules/network/ethernet/main.c
+++ modules/network/ethernet/main.c
@@ -1,11 +1,16 @@
 #include <module.h>
+#include <net/ethernet.h>
 #include <kernel/resource.h>
+#include <kernel/scheduler.h>
 #include <system/system.h>
 #include <base/base.h>
 #include <network/network.h>
 
 static struct network_channel channel;
 static struct network_channelnode channelnode;
+static unsigned char buffer[4096];
+static struct buffer cfifo;
+static struct scheduler_rendezvous rdata;
 
 static unsigned int channel_match(struct network_interface *interface, void *packet, unsigned int count)
 {
@@ -17,12 +22,26 @@ static unsigned int channel_match(struct network_interface *interface, void *pac
 static void channel_notify(struct network_interface *interface, void *packet, unsigned int count)
 {
 
+    struct ethernet_header *header = packet;
+
+    if (header->type[0] == 0x08)
+    {
+
+        count = buffer_wcfifo(&cfifo, count, packet);
+        scheduler_rendezvous_unsleep(&rdata);
+
+    }
+
 }
 
 static unsigned int channel_rdata(unsigned int offset, unsigned int count, void *buffer)
 {
 
-    return 0;
+    count = buffer_rcfifo(&cfifo, count, buffer);
+
+    scheduler_rendezvous_sleep(&rdata, !count);
+
+    return count;
 
 }
 
@@ -36,6 +55,7 @@ static unsigned int channel_wdata(unsigned int offset, unsigned int count, void 
 void init()
 {
 
+    buffer_init(&cfifo, 1, 4096, buffer);
     network_initchannel(&channel, channel_match, channel_notify, channel_rdata, channel_wdata);
     network_registerchannel(&channel);
     network_initchannelnode(&channelnode, &channel);
