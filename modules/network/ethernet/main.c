03729d88938be07023e90271a58f6d3d692d5bb9
Jens Nyberg <jens.nyberg@gmail.com>
2015-01-22 22:12:37 +0100

    removed more rendezvous
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- modules/network/ethernet/main.c
+++ modules/network/ethernet/main.c
@@ -1,14 +1,10 @@
 #include <fudge.h>
 #include <kernel.h>
-#include <net/ethernet.h>
 #include <system/system.h>
 #include <base/base.h>
 #include <network/network.h>
 
 static struct network_channel channel;
-static unsigned char buffer[4096];
-static struct buffer cfifo;
-static struct scheduler_rendezvous rdata;
 
 static unsigned int channel_match(struct network_interface *interface, void *packet, unsigned int count)
 {
@@ -20,27 +16,12 @@ static unsigned int channel_match(struct network_interface *interface, void *pac
 static void channel_notify(struct network_interface *interface, void *packet, unsigned int count)
 {
 
-    struct ethernet_header *header = packet;
-
-    if (header->type[0] == 0x08)
-    {
-
-        count = buffer_wcfifo(&cfifo, count, packet);
-        scheduler_rendezvous_unsleep(&rdata);
-
-    }
-
 }
 
 static unsigned int channel_rdata(unsigned int offset, unsigned int count, void *buffer)
 {
 
-    count = buffer_rcfifo(&cfifo, count, buffer);
-
-    if (!count)
-        scheduler_rendezvous_sleep(&rdata);
-
-    return count;
+    return 0;
 
 }
 
@@ -54,7 +35,6 @@ static unsigned int channel_wdata(unsigned int offset, unsigned int count, void 
 void init()
 {
 
-    buffer_init(&cfifo, 1, 4096, buffer);
     network_initchannel(&channel, channel_match, channel_notify, channel_rdata, channel_wdata);
     network_registerchannel(&channel);
 
