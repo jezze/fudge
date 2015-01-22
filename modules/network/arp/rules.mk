43804853b5a658ab9597eabb6bff3a384d34e05e
Jens Nyberg <jens.nyberg@gmail.com>
2015-01-23 00:43:17 +0100

    removed some network crap
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- modules/network/arp/rules.mk
+++
@@ -1,8 +0,0 @@
-MOD_$(DIR):=$(BUILD_MODULE)/arp.ko.0
-OBJ_$(DIR):=$(DIR)/main.o
-
-$(MOD_$(DIR)): $(OBJ_$(DIR))
-	$(LD) -o $@ $(LDFLAGS) $^ $(MODULES_LIBS)
-
-MODULES:=$(MODULES) $(MOD_$(DIR))
-CLEAN:=$(CLEAN) $(MOD_$(DIR)) $(OBJ_$(DIR))
