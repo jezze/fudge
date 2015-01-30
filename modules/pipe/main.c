eba9895eeae10b10d7fa853270928485528332cc
Jens Nyberg <jens.nyberg@gmail.com>
2015-01-30 03:16:57 +0100

    more work
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- modules/pipe/main.c
+++ modules/pipe/main.c
@@ -6,28 +6,45 @@
 struct pipe_endpoint
 {
 
+    struct list mailboxes;
     struct system_node node;
 
 };
 
-static struct list mailboxes;
 static struct pipe_endpoint endpoint0;
 static struct pipe_endpoint endpoint1;
 static struct system_node root;
 
+static unsigned int endpoint0_open(struct system_node *self)
+{
+
+    scheduler_mailboxes_addactive(&endpoint0.mailboxes);
+
+    return system_open(self);
+
+}
+
 static unsigned int endpoint0_close(struct system_node *self)
 {
 
-    scheduler_mailboxes_removeall(&mailboxes);
+    scheduler_mailboxes_removeactive(&endpoint0.mailboxes);
+    scheduler_mailboxes_removeall(&endpoint1.mailboxes);
 
     return system_close(self);
+
+}
+
+static unsigned int endpoint0_read(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    return scheduler_mailboxes_readactive(&endpoint0.mailboxes, count, buffer);
 
 }
 
 static unsigned int endpoint0_write(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
 {
 
-    scheduler_mailboxes_send(&mailboxes, count, buffer);
+    scheduler_mailboxes_send(&endpoint1.mailboxes, count, buffer);
 
     return count;
 
@@ -36,7 +53,7 @@ static unsigned int endpoint0_write(struct system_node *self, unsigned int offse
 static unsigned int endpoint1_open(struct system_node *self)
 {
 
-    scheduler_mailboxes_addactive(&mailboxes);
+    scheduler_mailboxes_addactive(&endpoint1.mailboxes);
 
     return system_open(self);
 
@@ -45,7 +62,8 @@ static unsigned int endpoint1_open(struct system_node *self)
 static unsigned int endpoint1_close(struct system_node *self)
 {
 
-    scheduler_mailboxes_removeactive(&mailboxes);
+    scheduler_mailboxes_removeactive(&endpoint1.mailboxes);
+    scheduler_mailboxes_removeall(&endpoint0.mailboxes);
 
     return system_close(self);
 
@@ -54,24 +72,37 @@ static unsigned int endpoint1_close(struct system_node *self)
 static unsigned int endpoint1_read(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
 {
 
-    return scheduler_mailboxes_readactive(&mailboxes, count, buffer);
+    return scheduler_mailboxes_readactive(&endpoint1.mailboxes, count, buffer);
+
+}
+
+static unsigned int endpoint1_write(struct system_node *self, unsigned int offset, unsigned int count, void *buffer)
+{
+
+    scheduler_mailboxes_send(&endpoint0.mailboxes, count, buffer);
+
+    return count;
 
 }
 
 void init()
 {
 
-    list_init(&mailboxes);
+    list_init(&endpoint0.mailboxes);
     system_initnode(&endpoint0.node, SYSTEM_NODETYPE_NORMAL, "0");
 
+    endpoint0.node.open = endpoint0_open;
     endpoint0.node.close = endpoint0_close;
+    endpoint0.node.read = endpoint0_read;
     endpoint0.node.write = endpoint0_write;
 
+    list_init(&endpoint1.mailboxes);
     system_initnode(&endpoint1.node, SYSTEM_NODETYPE_NORMAL, "1");
 
     endpoint1.node.open = endpoint1_open;
     endpoint1.node.close = endpoint1_close;
     endpoint1.node.read = endpoint1_read;
+    endpoint1.node.write = endpoint1_write;
 
     system_initnode(&root, SYSTEM_NODETYPE_GROUP | SYSTEM_NODETYPE_MULTI, "pipe");
     system_addchild(&root, &endpoint0.node);
