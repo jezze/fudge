40c7e3ca40e4f8a9b9c25f850e25baa17408dce0
Jens Nyberg <jens.nyberg@gmail.com>
2015-03-23 22:11:09 +0100

    allow negative box values
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- packages/wm/box.h
+++ packages/wm/box.h
@@ -1,11 +1,11 @@
 struct box
 {
 
-    unsigned int x;
-    unsigned int y;
-    unsigned int w;
-    unsigned int h;
+    int x;
+    int y;
+    int w;
+    int h;
 
 };
 
-void box_setsize(struct box *box, unsigned int x, unsigned int y, unsigned int w, unsigned int h);
+void box_setsize(struct box *box, int x, int y, int w, int h);
