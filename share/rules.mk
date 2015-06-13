8969a7fc7b5d334d1e4f0123f771c94881683d6c
Jens Nyberg <jens.nyberg@gmail.com>
2015-06-13 03:31:28 +0200

    remove leftover file
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- share/rules.mk
+++
@@ -1,3 +0,0 @@
-SHARES_$(DIR):=$(DIR)/design.png $(DIR)/executive-en.png $(DIR)/font2.png $(DIR)/fontall.png $(DIR)/font.png $(DIR)/help.txt $(DIR)/icons.pcx $(DIR)/lorem.txt $(DIR)/mi.pcx $(DIR)/motd.txt $(DIR)/test.slang $(DIR)/vancouver-en.png
-
-SHARES:=$(SHARES) $(SHARES_$(DIR))
