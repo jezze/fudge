c5dd2c56835b8ce6e6294715a0acf8258402c274
Jens Nyberg <jens.nyberg@gmail.com>
2015-08-16 14:33:26 +0200

    make reproducable builds
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/motd.c
+++ src/utils/motd.c
@@ -4,17 +4,8 @@
 void main(void)
 {
 
-    unsigned int woff = 0;
-
     call_open(CALL_PO);
-
-    woff += call_write(CALL_PO, woff, 23, 1, "FUDGE OPERATING SYSTEM\n");
-    woff += call_write(CALL_PO, woff, 12, 1, "Build date: ");
-    woff += call_write(CALL_PO, woff, 11, 1, __DATE__);
-    woff += call_write(CALL_PO, woff, 1, 1, " ");
-    woff += call_write(CALL_PO, woff, 8, 1, __TIME__);
-    woff += call_write(CALL_PO, woff, 2, 1, "\n\n");
-
+    call_write(CALL_PO, 0, 24, 1, "FUDGE OPERATING SYSTEM\n\n");
     call_close(CALL_PO);
 
     if (!call_walk(CALL_CP, CALL_PR, 8, "bin/echo"))
