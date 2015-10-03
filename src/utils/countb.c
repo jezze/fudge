0c12f82fdcdf273e5acc93de649c50e1ae08093d
Jens Nyberg <jens.nyberg@gmail.com>
2015-10-03 04:02:04 +0200

    fixed broken functions
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/countb.c
+++ src/utils/countb.c
@@ -3,18 +3,18 @@
 
 void main(void)
 {
-/*
+
     unsigned char buffer[FUDGE_BSIZE];
-    unsigned int count, roff;
+    unsigned int total, count;
 
     call_open(CALL_P0);
 
-    for (roff = 0; (count = call_read(CALL_P0, roff, 1, FUDGE_BSIZE, buffer)); roff += count);
+    for (total = 0; (count = call_read(CALL_P0, FUDGE_BSIZE, buffer)); total += count);
 
     call_close(CALL_P0);
     call_open(CALL_PO);
-    call_write(CALL_PO, 0, 1, ascii_fromint(buffer, FUDGE_BSIZE, roff + count, 10), buffer);
+    call_write(CALL_PO, ascii_fromint(buffer, FUDGE_BSIZE, total + count, 10), buffer);
     call_close(CALL_PO);
-*/
+
 }
 
--- src/utils/countl.c
+++ src/utils/countl.c
@@ -3,25 +3,31 @@
 
 void main(void)
 {
-/*
+
     unsigned char buffer[FUDGE_BSIZE];
-    unsigned int count0, count1, roff0, roff1;
-    unsigned int lines = 0;
+    unsigned int count, lines = 0;
 
     call_open(CALL_P0);
 
-    for (roff0 = 0; (count0 = call_read(CALL_P0, roff0, 1, FUDGE_BSIZE, buffer)); roff0 += roff1)
+    while ((count = call_read(CALL_P0, FUDGE_BSIZE, buffer)))
     {
 
-        for (roff1 = 0; (count1 = memory_findbyte(buffer + roff1, count0 - roff1, '\n')); roff1 += count1)
-            lines++;
+        unsigned int i;
+
+        for (i = 0; i < count; i++)
+        {
+
+            if (buffer[i] == '\n')
+                lines++;
+
+        }
 
     }
 
     call_close(CALL_P0);
     call_open(CALL_PO);
-    call_write(CALL_PO, 0, ascii_fromint(buffer, FUDGE_BSIZE, lines, 10), 1, buffer);
+    call_write(CALL_PO, ascii_fromint(buffer, FUDGE_BSIZE, lines, 10), buffer);
     call_close(CALL_PO);
-*/
+
 }
 
