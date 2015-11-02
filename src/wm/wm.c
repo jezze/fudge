c630122fa3250b572872f7accf05a16573166536
Jens Nyberg <jens.nyberg@gmail.com>
2015-11-02 22:08:50 +0100

    more cleanups
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/wm/wm.c
+++ src/wm/wm.c
@@ -571,7 +571,7 @@ static void onwmresize(union event *event)
 
     box_setsize(&screen, event->wmresize.x, event->wmresize.y, event->wmresize.w, event->wmresize.h);
     box_setsize(&menu, screen.x, screen.y, screen.w, 32);
-    box_setsize(&body, screen.x, screen.y + 32, screen.w, screen.h - 32);
+    box_setsize(&body, screen.x, screen.y + menu.h, screen.w, screen.h - menu.h);
     box_setsize(&background.size, screen.x, screen.y, screen.w, screen.h);
     writefill(event->header.destination, 1, &background);
 
@@ -582,7 +582,7 @@ static void onwmresize(union event *event)
 
         box_setsize(&view[i].panel.size, menu.x + i * menu.w / VIEWS, menu.y, menu.w / VIEWS, menu.h);
         writepanel(event->header.destination, 1, &view[i].panel);
-        box_setsize(&view[i].number.size, view[i].panel.size.x + 8, view[i].panel.size.y + 8, view[i].panel.size.w, 18);
+        box_setsize(&view[i].number.size, view[i].panel.size.x + menu.h / 4, view[i].panel.size.y + menu.h / 4, view[i].panel.size.w - menu.h / 2, view[i].panel.size.h - menu.h / 2);
         writetext(event->header.destination, 1, &view[i].number, 1, view[i].numberstring);
         arrangeclients(event->header.destination, &view[i]);
 
