8c0e70d174c3acfa02898c0c61bc71b99e75af51
Jens Nyberg <jens.nyberg@gmail.com>
2016-08-05 02:21:58 +0200

    adding event nodes
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/event/main.c
+++ src/modules/event/main.c
@@ -5,6 +5,17 @@
 
 static struct system_node root;
 static struct system_node poll;
+static struct system_node keypress;
+static struct system_node keyrelease;
+static struct system_node mousepress;
+static struct system_node mouserelease;
+static struct system_node mousemove;
+static struct system_node wmmap;
+static struct system_node wmunmap;
+static struct system_node wmresize;
+static struct system_node wmshow;
+static struct system_node wmhide;
+static struct system_node tick;
 
 static unsigned int notify(void *buffer)
 {
@@ -128,11 +139,33 @@ void module_init(void)
 {
 
     system_initnode(&poll, SYSTEM_NODETYPE_MAILBOX, "poll");
+    system_initnode(&keypress, SYSTEM_NODETYPE_MAILBOX, "keypress");
+    system_initnode(&keyrelease, SYSTEM_NODETYPE_MAILBOX, "keyrelease");
+    system_initnode(&mousepress, SYSTEM_NODETYPE_MAILBOX, "mousepress");
+    system_initnode(&mouserelease, SYSTEM_NODETYPE_MAILBOX, "mouserelease");
+    system_initnode(&mousemove, SYSTEM_NODETYPE_MAILBOX, "mousemove");
+    system_initnode(&wmmap, SYSTEM_NODETYPE_MAILBOX, "wmmap");
+    system_initnode(&wmunmap, SYSTEM_NODETYPE_MAILBOX, "wmunmap");
+    system_initnode(&wmresize, SYSTEM_NODETYPE_MAILBOX, "wmresize");
+    system_initnode(&wmshow, SYSTEM_NODETYPE_MAILBOX, "wmshow");
+    system_initnode(&wmhide, SYSTEM_NODETYPE_MAILBOX, "wmhide");
+    system_initnode(&tick, SYSTEM_NODETYPE_MAILBOX, "tick");
 
     poll.write = poll_write;
 
     system_initnode(&root, SYSTEM_NODETYPE_GROUP, "event");
     system_addchild(&root, &poll);
+    system_addchild(&root, &keypress);
+    system_addchild(&root, &keyrelease);
+    system_addchild(&root, &mousepress);
+    system_addchild(&root, &mouserelease);
+    system_addchild(&root, &mousemove);
+    system_addchild(&root, &wmmap);
+    system_addchild(&root, &wmunmap);
+    system_addchild(&root, &wmresize);
+    system_addchild(&root, &wmshow);
+    system_addchild(&root, &wmhide);
+    system_addchild(&root, &tick);
 
 }
 
