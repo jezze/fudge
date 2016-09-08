cb77d0dcf46763469f5bdedfd100aee3a6537478
Jens Nyberg <jens.nyberg@gmail.com>
2016-09-08 22:27:29 +0200

    more cleanups
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/ipserver.c
+++ src/utils/ipserver.c
@@ -6,23 +6,14 @@ void main(void)
 
     struct ctrl_consettings settings;
 
-    file_write(CALL_PO, "hej\n", 4);
+    ctrl_setconsettings(&settings, 0, 0x11);
 
     if (!file_walk(CALL_L1, "ctrl"))
         return;
 
-    file_write(CALL_PO, "hej\n", 4);
-
     file_open(CALL_L1);
-    file_readall(CALL_L1, &settings, sizeof (struct ctrl_consettings));
-
-    settings.interface = 0;
-    settings.protocol = 0x11;
-
     file_writeall(CALL_L1, &settings, sizeof (struct ctrl_consettings));
     file_close(CALL_L1);
-
-    file_write(CALL_PO, "hej\n", 4);
 
 }
 
