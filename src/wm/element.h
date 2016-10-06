cf54f367f302f3db932a06d2a68bf4d9bde71622
Jens Nyberg <jens.nyberg@gmail.com>
2016-10-06 02:14:53 +0200

    more cleanups
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/wm/element.h
+++ src/wm/element.h
@@ -6,11 +6,11 @@
 #define ELEMENT_TYPE_TEXT               2
 #define ELEMENT_TYPE_PANEL              3
 #define ELEMENT_TYPE_MOUSE              4
-#define ELEMENT_TEXTTYPE_NORMAL         0x00
-#define ELEMENT_TEXTTYPE_HIGHLIGHT      0x01
-#define ELEMENT_TEXTFLOW_NORMAL         0x00
-#define ELEMENT_TEXTFLOW_BOTTOM         0x01
-#define ELEMENT_TEXTFLOW_INPUT          0x02
+#define ELEMENT_TEXTTYPE_NORMAL         0
+#define ELEMENT_TEXTTYPE_HIGHLIGHT      1
+#define ELEMENT_TEXTFLOW_NORMAL         0
+#define ELEMENT_TEXTFLOW_BOTTOM         1
+#define ELEMENT_TEXTFLOW_INPUT          2
 
 struct element
 {
