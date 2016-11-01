e9db89f9c8370e6ec36ef6044a8a696602a57343
Jens Nyberg <jens.nyberg@gmail.com>
2016-11-02 00:18:51 +0100

    minor disksetup changes
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/disksetup.c
+++ src/utils/disksetup.c
@@ -1,19 +1,44 @@
 #include <abi.h>
 #include <fudge.h>
 
-void main(void)
+static void setpart0()
 {
 
     struct ctrl_partsettings settings;
 
     ctrl_setpartsettings(&settings, 0, 2048 * 512, 40000 * 512);
 
-    if (!file_walk(CALL_L0, "/system/part/clone/ctrl"))
+    if (!file_walk(CALL_L0, "/system/part/part:0/ctrl"))
         return;
 
     file_open(CALL_L0);
     file_write(CALL_L0, &settings, sizeof (struct ctrl_partsettings));
     file_close(CALL_L0);
+
+
+}
+
+static void setpart1()
+{
+
+    struct ctrl_partsettings settings;
+
+    ctrl_setpartsettings(&settings, 0, 40960 * 512, 1048575 * 512);
+
+    if (!file_walk(CALL_L0, "/system/part/part:1/ctrl"))
+        return;
+
+    file_open(CALL_L0);
+    file_write(CALL_L0, &settings, sizeof (struct ctrl_partsettings));
+    file_close(CALL_L0);
+
+}
+
+void main(void)
+{
+
+    setpart0();
+    setpart1();
 
 }
 
