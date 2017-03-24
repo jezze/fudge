5dc3d26ac11e4518df0c88acfac3b1f88693d7fd
Jens Nyberg <jens.nyberg@gmail.com>
2017-03-24 20:12:07 +0100

    more work
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/con/node.c
+++ src/modules/con/node.c
@@ -5,10 +5,11 @@
 #include <modules/udp/udp.h>
 #include "con.h"
 
+#define UDPTABLESIZE                    64
+
 static struct con con;
 static struct udp_hook hook;
-static struct ipv4_header *tempipv4header;
-static struct udp_header *tempudpheader;
+static struct udp_session sessions[UDPTABLESIZE];
 
 static void *writedata(void *buffer, void *payload, unsigned int count)
 {
@@ -18,19 +19,6 @@ static void *writedata(void *buffer, void *payload, unsigned int count)
     memory_copy(buffer, payload, count);
 
     return data + count;
-
-}
-
-static void send(unsigned char *sip, unsigned char *sp, unsigned char *tip, unsigned char *tp, void *buffer, unsigned int count)
-{
-
-    unsigned char response[FUDGE_BSIZE];
-    unsigned char *current = response;
-
-    current = udp_writehead(current, sip, sp, tip, tp, count);
-    current = writedata(current, buffer, count);
-
-    udp_send(response, current - response);
 
 }
 
@@ -51,7 +39,14 @@ static void con_close(void)
 static unsigned int con_write(void *buffer, unsigned int count)
 {
 
-    send(tempipv4header->tip, tempudpheader->tp, tempipv4header->sip, tempudpheader->sp, buffer, count);
+    struct udp_session *session = &sessions[0];
+    unsigned char response[FUDGE_BSIZE];
+    unsigned char *current = response;
+
+    current = udp_writehead(current, session->sip, session->sp, session->tip, session->tp, count);
+    current = writedata(current, buffer, count);
+
+    udp_send(response, current - response);
 
     return count;
 
@@ -67,8 +62,20 @@ static unsigned int hook_match(unsigned int port)
 static void hook_notify(struct ipv4_header *ipv4header, struct udp_header *udpheader, void *buffer, unsigned int count)
 {
 
-    tempipv4header = ipv4header;
-    tempudpheader = udpheader;
+    struct udp_session *session = &sessions[0];
+
+    session->sip[0] = ipv4header->tip[0];
+    session->sip[1] = ipv4header->tip[1];
+    session->sip[2] = ipv4header->tip[2];
+    session->sip[3] = ipv4header->tip[3];
+    session->tip[0] = ipv4header->sip[0];
+    session->tip[1] = ipv4header->sip[1];
+    session->tip[2] = ipv4header->sip[2];
+    session->tip[3] = ipv4header->sip[3];
+    session->sp[0] = udpheader->tp[0];
+    session->sp[1] = udpheader->tp[1];
+    session->tp[0] = udpheader->sp[0];
+    session->tp[1] = udpheader->sp[1];
 
     kernel_multicast(&con.links, buffer, count);
 
--- src/modules/udp/udp.h
+++ src/modules/udp/udp.h
@@ -8,6 +8,16 @@ struct udp_header
 
 };
 
+struct udp_session
+{
+
+    unsigned char sip[IPV4_ADDRSIZE];
+    unsigned char tip[IPV4_ADDRSIZE];
+    unsigned char sp[2];
+    unsigned char tp[2];
+
+};
+
 struct udp_hook
 {
 
