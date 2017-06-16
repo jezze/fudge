62d801ab48dbdac6fc5503e389fba6c011001f99
Jens Nyberg <jens.nyberg@gmail.com>
2017-06-16 20:55:48 +0200

    remove leftover flag
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- mk/arch.x86.mk
+++ mk/arch.x86.mk
@@ -1,6 +1,6 @@
 AR=i386-unknown-elf-ar rcs
 AS=i386-unknown-elf-as -c -o
-CC=i386-unknown-elf-cc -msoft-float -Wall -Werror -ffreestanding -nostdlib -nostdinc -std=c89 -pedantic -O2 -c -S -o
+CC=i386-unknown-elf-cc -msoft-float -Wall -Werror -ffreestanding -nostdlib -nostdinc -std=c89 -pedantic -O2 -S -o
 LD=i386-unknown-elf-ld -static -o
 PP=i386-unknown-elf-cc -msoft-float -Wall -Werror -ffreestanding -nostdlib -nostdinc -std=c89 -pedantic -O2 -E -I$(DIR_INCLUDE) -I$(DIR_SRC) -I$(DIR_LIB) -o
 
