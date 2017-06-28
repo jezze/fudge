21e93665847b5d3c9bdbb36f5351dd701f370350
Jens Nyberg <jens.nyberg@gmail.com>
2017-06-28 22:45:28 +0200

    more cleanups
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- mk/arch.x86.mk
+++ mk/arch.x86.mk
@@ -1,8 +1,8 @@
 AR=i386-unknown-elf-ar rcs
 AS=i386-unknown-elf-as -c -o
-CC=i386-unknown-elf-cc -msoft-float -Wall -Werror -ffreestanding -fno-asynchronous-unwind-tables -nostdlib -nostdinc -std=c89 -pedantic -O2 -S -o
-LD=i386-unknown-elf-ld -static -o
-PP=i386-unknown-elf-cc -msoft-float -Wall -Werror -ffreestanding -nostdlib -nostdinc -std=c89 -pedantic -O2 -E -I$(DIR_INCLUDE) -I$(DIR_SRC) -I$(DIR_LIB) -o
+CC=i386-unknown-elf-cc -Wall -Werror -msoft-float -ffreestanding -fno-asynchronous-unwind-tables -std=c89 -pedantic -O2 -S -o
+LD=i386-unknown-elf-ld -static -nostdlib -o
+PP=i386-unknown-elf-cc -Wall -Werror -nostdinc -std=c89 -pedantic -E -I$(DIR_INCLUDE) -I$(DIR_SRC) -I$(DIR_LIB) -o
 
 %.i: %.c
 	@echo PP $@
