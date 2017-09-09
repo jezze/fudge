7681f013bd673aa024de08789e4a1eca65070869
Jens Nyberg <jens.nyberg@gmail.com>
2017-09-09 03:10:05 +0200

    add more stuff
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/arch/x86/smp/init.s
+++ src/modules/arch/x86/smp/init.s
@@ -18,17 +18,10 @@ init:
     movw %ax, %gs
     movw %ax, %ss
 
-    # print red thing
-#    movw $0xB800, %ax
-#    movw %ax, %es
-#    xorw %bx, %bx
-#    movw $0x4020, %ax
-#    movw %ax, %es:(%bx)
-
     # setup stack
-#    movw $0x8800, %ax
-#    movw %ax, %sp
-#    movw %ax, %bp
+    movw $0x8800, %ax
+    movw %ax, %ss
+    xorl %esp, %esp
 
     # load gdt
     movl $0x1000, %eax
@@ -40,12 +33,12 @@ init:
     movl %eax, %cr0
 
     # long jump
-#    ljmp $0x08, $0x9000
+    ljmp $0x08, $0x9000
 
     # long ret
-#    pushl $0x08
-#    pushl $0x9000
-#    lret
+    #pushl $0x08
+    #pushl $0x9000
+    #lret
 
     hlt
 
@@ -58,21 +51,16 @@ smp_end16:
 smp_begin32:
 
 smp_init32:
-#    movl $0xB8000, %eax
-#    movl $0x4121, (%eax)
-
     movw $0x10, %ax
     movw %ax, %ds
     movw %ax, %es
     movw %ax, %fs
     movw %ax, %gs
     movw %ax, %ss
-
     movl $0xB8000, %eax
     movl $0x4121, (%eax)
-
-    jmp .
-    call smp_setup
+    hlt
+#    call smp_setup
 
 .global smp_end32
 smp_end32:
