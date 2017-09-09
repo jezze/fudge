2a9f09a7aaad3ee40af098497315c97c06b199d5
Jens Nyberg <jens.nyberg@gmail.com>
2017-09-09 02:49:11 +0200

    more work
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/arch/x86/smp/init.s
+++ src/modules/arch/x86/smp/init.s
@@ -18,26 +18,26 @@ init:
     movw %ax, %gs
     movw %ax, %ss
 
+    # print red thing
+#    movw $0xB800, %ax
+#    movw %ax, %es
+#    xorw %bx, %bx
+#    movw $0x4020, %ax
+#    movw %ax, %es:(%bx)
+
     # setup stack
-    movw $0x8800, %ax
-    movw %ax, %sp
-    movw %ax, %bp
+#    movw $0x8800, %ax
+#    movw %ax, %sp
+#    movw %ax, %bp
 
     # load gdt
     movl $0x1000, %eax
     lgdt (%eax)
 
     # protected mode
-#    movl %cr0, %eax
-#    orl $1, %eax
-#    movl %eax, %cr0
-
-    # print red thing
-    movw $0xB800, %ax
-    movw %ax, %es
-    xorw %bx, %bx
-    movw $0x4020, %ax
-    movw %ax, %es:(%bx)
+    movl %cr0, %eax
+    orl $1, %eax
+    movl %eax, %cr0
 
     # long jump
 #    ljmp $0x08, $0x9000
@@ -58,8 +58,8 @@ smp_end16:
 smp_begin32:
 
 smp_init32:
-    movl $0xB8000, %eax
-    movl $0x4121, (%eax)
+#    movl $0xB8000, %eax
+#    movl $0x4121, (%eax)
 
     movw $0x10, %ax
     movw %ax, %ds
