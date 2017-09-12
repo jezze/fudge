9841a684e29702ddb10ad29c0832492ab16e9e9f
Jens Nyberg <jens.nyberg@gmail.com>
2017-09-13 00:31:53 +0200

    more fixes
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/arch/x86/smp/main.c
+++ src/modules/arch/x86/smp/main.c
@@ -127,7 +127,7 @@ void module_init(void)
     if (!madt)
         return;
 
-    smp_prep(0x00380000);
+    smp_prep(0x00384000);
     copytrampoline16();
     copytrampoline32();
     detect(madt);
