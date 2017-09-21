88e6a4f2758f080d24d37a04aabeb161a47bb9f4
Jens Nyberg <jens.nyberg@gmail.com>
2017-09-21 21:32:04 +0200

    add exact push size
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/arch/x86/pic/pic.s
+++ src/modules/arch/x86/pic/pic.s
@@ -6,98 +6,98 @@
 
 .global pic_routine00
 pic_routine00:
-    push $0
-    push $0x00
+    pushl $0
+    pushl $0x00
     jmp pic_common
 
 .global pic_routine01
 pic_routine01:
-    push $0
-    push $0x01
+    pushl $0
+    pushl $0x01
     jmp pic_common
 
 .global pic_routine02
 pic_routine02:
-    push $0
-    push $0x02
+    pushl $0
+    pushl $0x02
     jmp pic_common
 
 .global pic_routine03
 pic_routine03:
-    push $0
-    push $0x03
+    pushl $0
+    pushl $0x03
     jmp pic_common
 
 .global pic_routine04
 pic_routine04:
-    push $0
-    push $0x04
+    pushl $0
+    pushl $0x04
     jmp pic_common
 
 .global pic_routine05
 pic_routine05:
-    push $0
-    push $0x05
+    pushl $0
+    pushl $0x05
     jmp pic_common
 
 .global pic_routine06
 pic_routine06:
-    push $0
-    push $0x06
+    pushl $0
+    pushl $0x06
     jmp pic_common
 
 .global pic_routine07
 pic_routine07:
-    push $0
-    push $0x07
+    pushl $0
+    pushl $0x07
     jmp pic_common
 
 .global pic_routine08
 pic_routine08:
-    push $1
-    push $0x08
+    pushl $1
+    pushl $0x08
     jmp pic_common
 
 .global pic_routine09
 pic_routine09:
-    push $1
-    push $0x09
+    pushl $1
+    pushl $0x09
     jmp pic_common
 
 .global pic_routine0A
 pic_routine0A:
-    push $1
-    push $0x0A
+    pushl $1
+    pushl $0x0A
     jmp pic_common
 
 .global pic_routine0B
 pic_routine0B:
-    push $1
-    push $0x0B
+    pushl $1
+    pushl $0x0B
     jmp pic_common
 
 .global pic_routine0C
 pic_routine0C:
-    push $1
-    push $0x0C
+    pushl $1
+    pushl $0x0C
     jmp pic_common
 
 .global pic_routine0D
 pic_routine0D:
-    push $1
-    push $0x0D
+    pushl $1
+    pushl $0x0D
     jmp pic_common
 
 .global pic_routine0E
 pic_routine0E:
-    push $1
-    push $0x0E
+    pushl $1
+    pushl $0x0E
     jmp pic_common
 
 .global pic_routine0F
 pic_routine0F:
-    push $1
-    push $0x0F
+    pushl $1
+    pushl $0x0F
     jmp pic_common
 
 pic_common:
