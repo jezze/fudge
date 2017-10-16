4a3a6df1fd00029835d743d844a73e827200c3b1
Jens Nyberg <jens.nyberg@gmail.com>
2017-10-17 00:41:02 +0200

    remove spinlocks from uart module
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/arch/x86/uart/main.c
+++ src/modules/arch/x86/uart/main.c
@@ -101,30 +101,18 @@ static unsigned short io;
 static unsigned char read(void)
 {
 
-    unsigned char value;
-
-    spinlock_acquire(&consoleinterface.rspinlock);
-
     while (!(io_inb(io + REGISTERLSR) & LSRREADY));
 
-    value = io_inb(io);
-
-    spinlock_release(&consoleinterface.rspinlock);
-
-    return value;
+    return io_inb(io);
 
 }
 
 static void write(unsigned char c)
 {
 
-    spinlock_acquire(&consoleinterface.wspinlock);
-
     while (!(io_inb(io + REGISTERLSR) & LSRTRANSMIT));
 
     io_outb(io, c);
-
-    spinlock_release(&consoleinterface.wspinlock);
 
 }
 
