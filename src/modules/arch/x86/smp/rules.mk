6023f619164b4a276b9563d113453f96697d1c2f
Jens Nyberg <jens.nyberg@gmail.com>
2018-05-09 00:02:29 +0200

    remove test module from smp
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/arch/x86/smp/rules.mk
+++ src/modules/arch/x86/smp/rules.mk
@@ -9,14 +9,3 @@ L:=\
     $(DIR_LIB)/fudge/fudge.a \
 
 include $(DIR_MK)/kmod.mk
-
-M:=\
-    $(DIR_SRC)/modules/arch/x86/smp/test.ko \
-
-O:=\
-    $(DIR_SRC)/modules/arch/x86/smp/test.o \
-
-L:=\
-    $(DIR_LIB)/fudge/fudge.a \
-
-include $(DIR_MK)/kmod.mk
