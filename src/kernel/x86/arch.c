1af610b8092287057d0aeced6146692dafcc1d83
Jens Nyberg <jens.nyberg@gmail.com>
2018-09-14 00:40:38 +0200

    reset buffer during spawn
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/kernel/x86/arch.c
+++ src/kernel/x86/arch.c
@@ -63,6 +63,7 @@ static unsigned int spawn(struct task *task, void *stack)
 
         memory_copy(gettaskdirectory(next->id), getkerneldirectory(), sizeof (struct mmu_directory));
         kernel_usetask(next);
+        ring_reset(&next->mailbox.ring);
 
         return next->id;
 
