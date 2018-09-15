56efb73ecb9cb661e445abdfd1b4f096f423cda5
Jens Nyberg <jens.nyberg@gmail.com>
2018-09-15 14:12:46 +0200

    more cleanups
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/reboot.c
+++ src/utils/reboot.c
@@ -7,9 +7,6 @@ void main(void)
     if (!file_walk(FILE_L0, "/system/reset"))
         return;
 
-    file_open(FILE_PO);
-    file_writeall(FILE_PO, "System is rebooting...\n", 23);
-    file_close(FILE_PO);
     file_open(FILE_L0);
     file_writeall(FILE_L0, "1", 1);
     file_close(FILE_L0);
