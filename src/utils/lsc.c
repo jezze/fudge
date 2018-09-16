5260efb57cfc84ec42ea3108b72cc1b2acc51cba
Jens Nyberg <jens.nyberg@gmail.com>
2018-09-16 12:05:57 +0200

    minor cleanup
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/lsc.c
+++ src/utils/lsc.c
@@ -8,12 +8,13 @@ static void list(struct event_header *header, unsigned int descriptor)
 {
 
     struct record record;
-    char num[FUDGE_NSIZE];
 
     file_open(descriptor);
 
     while (file_readall(descriptor, &record, sizeof (struct record)))
     {
+
+        char num[FUDGE_NSIZE];
 
         event_senddata(FILE_L0, header->destination, header->source, ascii_wzerovalue(num, FUDGE_NSIZE, record.id, 16, 8, 0), num);
         event_senddata(FILE_L0, header->destination, header->source, 1, " ");
