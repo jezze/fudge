6083d1671214d632a2595fd4425cb8b7180ce466
Jens Nyberg <jens.nyberg@gmail.com>
2019-09-12 18:10:46 +0200

    more work
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- lib/abi/channel.c
+++ lib/abi/channel.c
@@ -120,6 +120,7 @@ void channel_init(struct channel *channel)
     for (i = 0; i < EVENTS; i++)
         channel_setsignal(channel, i, ignore);
 
+    event_create(&channel->i, 0, 0);
     channel_setsignal(channel, EVENT_KILL, abort);
     channel_setsignal(channel, EVENT_STOP, abort);
 
