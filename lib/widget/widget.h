0685fbdbb4f9821081c30d6ab9aa86deba470810
Jens Nyberg <jens.nyberg@gmail.com>
2019-10-07 22:38:32 +0200

    cleanups
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- lib/widget/widget.h
+++ lib/widget/widget.h
@@ -85,5 +85,5 @@ void widget_initpanel(struct widget_panel *panel, unsigned int active);
 void widget_inittext(struct widget_text *text, unsigned int type);
 void widget_inittextbox(struct widget_textbox *textbox);
 void widget_initwindow(struct widget_window *window, unsigned int active);
-void widget_update(struct ring *ring, void *item, unsigned int z, unsigned int type, unsigned int count, struct box *box);
+void widget_update(struct ring *ring, void *item, unsigned int z, unsigned int type, unsigned int count, struct box *size);
 void widget_remove(struct ring *ring, void *item, unsigned int z);
