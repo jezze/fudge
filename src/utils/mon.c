e9d461c8a0a8c4788b7decc5c81f8be8051fd82a
Jens Nyberg <jens.nyberg@gmail.com>
2020-02-02 00:48:17 +0100

    more work
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/mon.c
+++ src/utils/mon.c
@@ -22,7 +22,16 @@ struct request
 static char blocks[BLOCKSIZE * 4];
 static unsigned int qp;
 
-static void createrequest(struct request *request, unsigned int type, unsigned int offset, unsigned int count)
+static void request_notifydata(struct request *request, void *data, unsigned int size)
+{
+
+    unsigned int count = memory_write(request->data, BLOCKSIZE * 4, data, size, request->blockreads * BLOCKSIZE);
+
+    request->blockreads += count / BLOCKSIZE;
+
+}
+
+static void request_init(struct request *request, unsigned int type, unsigned int offset, unsigned int count)
 {
 
     request->type = type;
@@ -30,33 +39,19 @@ static void createrequest(struct request *request, unsigned int type, unsigned i
     request->count = count;
     request->blocksector = offset / BLOCKSIZE;
     request->blockoffset = offset % BLOCKSIZE;
-    request->blockcount = 2;
+    request->blockcount = ((BLOCKSIZE - 1) + request->blockoffset + request->count) / BLOCKSIZE;
     request->blockreads = 0;
     request->data = blocks;
 
 }
 
-static void saveblock(struct request *request, void *data, unsigned int size)
-{
-
-    if (request->blockreads < request->blockcount)
-    {
-
-        unsigned int count = memory_write(request->data, BLOCKSIZE * 4, data, size, request->blockreads * BLOCKSIZE);
-
-        request->blockreads += count / BLOCKSIZE;
-
-    }
-
-}
-
-static void sendrequest(struct channel *channel, struct request *request)
+static void sendblockrequest(struct channel *channel, unsigned int sector, unsigned int count)
 {
 
     struct event_blockrequest blockrequest;
 
-    blockrequest.sector = request->blocksector;
-    blockrequest.count = request->blockcount;
+    blockrequest.sector = sector;
+    blockrequest.count = count;
 
     channel_request(channel, EVENT_BLOCKREQUEST);
     channel_append(channel, sizeof (struct event_blockrequest), &blockrequest);
@@ -72,14 +67,17 @@ static void handlefind(struct channel *channel, unsigned int source, struct requ
     if (cpio_validate(header))
     {
 
+        /* need to check if namesize is too big, otherwise request more data */
+        /* this way we can remove the + BLOCKSIZE at the end of request_init calls */
+
         channel_request(channel, EVENT_DATA);
         channel_append(channel, header->namesize - 1, header + 1);
         channel_appendstring(channel, "\n");
         channel_place(channel, source);
 
         /* request next entry */
-        createrequest(request, TYPE_FIND, cpio_next(header, request->offset), sizeof (struct cpio_header));
-        sendrequest(channel, request);
+        request_init(request, TYPE_FIND, cpio_next(header, request->offset), sizeof (struct cpio_header) + BLOCKSIZE);
+        sendblockrequest(channel, request->blocksector, request->blockcount);
 
     }
 
@@ -105,8 +103,8 @@ static void ondone(struct channel *channel, unsigned int source, void *mdata, un
     struct request *request = &requests[qp];
 
     /* request first entry */
-    createrequest(request, TYPE_FIND, 0, sizeof (struct cpio_header));
-    sendrequest(channel, request);
+    request_init(request, TYPE_FIND, 0, sizeof (struct cpio_header) + BLOCKSIZE);
+    sendblockrequest(channel, request->blocksector, request->blockcount);
 
 }
 
@@ -115,7 +113,7 @@ static void ondata(struct channel *channel, unsigned int source, void *mdata, un
 
     struct request *request = &requests[qp];
 
-    saveblock(request, mdata, msize);
+    request_notifydata(request, mdata, msize);
 
     if (request->blockreads < request->blockcount)
         return;
