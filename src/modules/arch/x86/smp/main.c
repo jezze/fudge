e36d0e5a31e9636d9abbbdf67b7018118100511a
Jens Nyberg <jens.nyberg@gmail.com>
2020-11-06 23:03:53 +0100

    temporary fix
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/arch/x86/smp/main.c
+++ src/modules/arch/x86/smp/main.c
@@ -64,19 +64,11 @@ static struct core *coreget(void)
 static void coreassign(struct task *task)
 {
 
-    /* Change from picktail to pickhead for round-robin */
-    struct list_item *current = list_picktail(&corelist);
+    struct list_item *current = corelist.head;
+    struct core *core = current->data;
 
-    if (current)
-    {
-
-        struct core *core = current->data;
-
-        list_add(&core->tasks, &task->item);
-        list_add(&corelist, &core->item);
-        apic_sendint(core->id, APIC_ICR_TYPE_NORMAL | APIC_ICR_MODE_PHYSICAL | APIC_ICR_LEVEL_ASSERT | APIC_ICR_TRIGGER_EDGE | APIC_ICR_TARGET_NORMAL | 0xFE);
-
-    }
+    list_add(&core->tasks, &task->item);
+    apic_sendint(core->id, APIC_ICR_TYPE_NORMAL | APIC_ICR_MODE_PHYSICAL | APIC_ICR_LEVEL_ASSERT | APIC_ICR_TRIGGER_EDGE | APIC_ICR_TARGET_NORMAL | 0xFE);
 
 }
 
