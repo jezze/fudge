fd8c15484ca92425176c4b6ad4877ae567f721ea
Jens Nyberg <jens.nyberg@gmail.com>
2020-11-16 23:10:09 +0100

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/log.c
+++ src/utils/log.c
@@ -7,12 +7,12 @@ static void onmain(struct channel *channel, unsigned int source, void *mdata, un
     struct message_header header;
     struct message_data data;
 
-    file_open(FILE_G0);
+    file_link(FILE_G0);
 
     while (channel_pollsource(channel, 0, &header, &data))
         channel_place(channel, source, EVENT_DATA, message_datasize(&header), &data);
 
-    file_close(FILE_G0);
+    file_unlink(FILE_G0);
     channel_close(channel);
 
 }
