b1cfed8adf2f9e5f0814a1e373026c43947e1881
Jens Nyberg <jens.nyberg@gmail.com>
2020-11-21 23:17:44 +0100

    add t_input prog
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

---
+++ src/test/t_input.c
@@ -0,0 +1,54 @@
+#include <fudge.h>
+#include <abi.h>
+
+static void onmain(struct channel *channel, unsigned int source, void *mdata, unsigned int msize)
+{
+
+    struct message_header header;
+    struct message_data data;
+
+    channel_place(channel, source, EVENT_DATA, message_putstring(&data, "If you press f I will quit\n", 0), &data);
+
+    while (channel_pollsource(channel, source, &header, &data))
+    {
+
+        if (header.event == EVENT_CONSOLEDATA)
+        {
+
+            struct event_consoledata *consoledata = (struct event_consoledata *)data.buffer;
+
+            if (consoledata->data == 'f')
+            {
+
+                channel_place(channel, source, EVENT_DATA, message_putstring(&data, "\nYou clicked f, goodbye!\n", 0), &data);
+                channel_close(channel, source);
+
+            }
+
+            else if (consoledata->data == '\r')
+            {
+ 
+                channel_place(channel, source, EVENT_DATA, message_putstring(&data, "\n", 0), &data);
+
+            }
+
+            else
+            {
+
+                channel_place(channel, source, EVENT_DATA, message_datasize(&header), &data);
+
+            }
+
+        }
+
+    }
+
+}
+
+void init(struct channel *channel)
+{
+
+    channel_setcallback(channel, EVENT_MAIN, onmain);
+
+}
+
