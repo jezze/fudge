99beeba1523bdaff1b2e08cee9d9caec5bb5f8b3
Jens Nyberg <jens.nyberg@gmail.com>
2020-12-12 06:22:28 +0100

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/motd.c
+++ src/utils/motd.c
@@ -12,7 +12,7 @@ static void onmain(struct channel *channel, unsigned int source, void *mdata, un
         struct message_header header;
         struct message_data data;
 
-        job_replyto(channel, id, EVENT_DATA, source);
+        job_replyto(channel, id, EVENT_DATA, (channel->callbacks[EVENT_DATA].redirect) ? channel->callbacks[EVENT_DATA].redirect : source);
         job_replyback(channel, id, EVENT_CLOSE);
         channel_place2(channel, id, EVENT_FILE, message_putstringz(&data, "/data/motd.txt", 0), &data);
         channel_place2(channel, id, EVENT_MAIN, 0, 0);
