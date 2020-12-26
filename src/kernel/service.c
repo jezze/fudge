25f7a9eb0e4ee07f2a5b228af0c25bc78d1508bf
Jens Nyberg <jens.nyberg@gmail.com>
2020-12-26 11:06:28 +0100

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/kernel/service.c
+++ src/kernel/service.c
@@ -30,12 +30,13 @@ void service_initlink(struct service_link *link, unsigned int id)
 
 }
 
-void service_initprotocol(struct service_protocol *protocol, unsigned int id, unsigned int (*root)(void), unsigned int (*parent)(unsigned int id), unsigned int (*child)(unsigned int id, char *path, unsigned int length), unsigned int (*create)(unsigned int id, char *name, unsigned int length), unsigned int (*destroy)(unsigned int id, char *name, unsigned int length), unsigned int (*step)(unsigned int id, unsigned int current), unsigned int (*open)(unsigned int id), unsigned int (*close)(unsigned int id), unsigned int (*read)(unsigned int id, unsigned int current, void *buffer, unsigned int count, unsigned int offset), unsigned int (*write)(unsigned int id, unsigned int current, void *buffer, unsigned int count, unsigned int offset), unsigned int (*seek)(unsigned int id, unsigned int offset), unsigned int (*map)(unsigned int id), unsigned int (*link)(unsigned int id, struct service_link *link), unsigned int (*unlink)(unsigned int id, struct service_link *link), unsigned int (*notify)(unsigned int id, struct service_link *link, struct message_header *header, void *data))
+void service_initprotocol(struct service_protocol *protocol, unsigned int id, char *name, unsigned int (*root)(void), unsigned int (*parent)(unsigned int id), unsigned int (*child)(unsigned int id, char *path, unsigned int length), unsigned int (*create)(unsigned int id, char *name, unsigned int length), unsigned int (*destroy)(unsigned int id, char *name, unsigned int length), unsigned int (*step)(unsigned int id, unsigned int current), unsigned int (*open)(unsigned int id), unsigned int (*close)(unsigned int id), unsigned int (*read)(unsigned int id, unsigned int current, void *buffer, unsigned int count, unsigned int offset), unsigned int (*write)(unsigned int id, unsigned int current, void *buffer, unsigned int count, unsigned int offset), unsigned int (*seek)(unsigned int id, unsigned int offset), unsigned int (*map)(unsigned int id), unsigned int (*link)(unsigned int id, struct service_link *link), unsigned int (*unlink)(unsigned int id, struct service_link *link), unsigned int (*notify)(unsigned int id, struct service_link *link, struct message_header *header, void *data))
 {
 
     resource_init(&protocol->resource, RESOURCE_SERVICEPROTOCOL, protocol);
 
     protocol->id = id;
+    protocol->name = name;
     protocol->root = root;
     protocol->parent = parent;
     protocol->child = child;
