985961a6d81c92fa67d03cdcac0fd3f14c9d22cb
Jens Nyberg <jens.nyberg@gmail.com>
2021-04-30 12:47:07 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/dump.c
+++ src/utils/dump.c
@@ -104,11 +104,34 @@ static void onfile(struct channel *channel, unsigned int source, void *mdata, un
 
 }
 
+static void onpath(struct channel *channel, unsigned int source, void *mdata, unsigned int msize)
+{
+
+    page = 0;
+
+    if (file_walk2(FILE_L0, mdata))
+    {
+
+        struct message_data data;
+        unsigned int count;
+
+        file_open(FILE_L0);
+
+        while ((count = file_read(FILE_L0, &data, sizeof (struct message_data))))
+            print(channel, source, count, &data);
+
+        file_close(FILE_L0);
+
+    }
+
+}
+
 void init(struct channel *channel)
 {
 
     channel_setcallback(channel, EVENT_DATA, ondata);
     channel_setcallback(channel, EVENT_FILE, onfile);
+    channel_setcallback(channel, EVENT_PATH, onpath);
 
 }
 
