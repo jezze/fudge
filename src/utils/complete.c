a37071f5a300f41bf4bc5264af6ad61cda7202c8
Jens Nyberg <jens.nyberg@gmail.com>
2021-05-01 23:08:35 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/complete.c
+++ src/utils/complete.c
@@ -1,45 +1,67 @@
 #include <fudge.h>
 #include <abi.h>
 
-static void print(struct channel *channel, unsigned int source, struct record *record, char *name, unsigned int length)
+static char key[1024];
+static unsigned int keylength;
+
+static void print(struct channel *channel, unsigned int source, struct record *record)
 {
 
-    struct message_data data;
-    unsigned int offset = 0;
+    if (buffer_match(record->name, key, keylength))
+    {
 
-    if (record->length < length || !buffer_match(record->name, name, length))
-        return;
+        struct message_data data;
+        unsigned int offset = 0;
 
-    offset = message_putbuffer(&data, record->length, record->name, offset);
-    offset = message_putstring(&data, "\n", offset);
+        offset = message_putbuffer(&data, record->length, record->name, offset);
+        offset = message_putstring(&data, "\n", offset);
 
-    channel_reply(channel, EVENT_DATA, offset, &data);
+        channel_reply(channel, EVENT_DATA, offset, &data);
+
+    }
 
 }
 
-static void ondata(struct channel *channel, unsigned int source, void *mdata, unsigned int msize)
+static void onoption(struct channel *channel, unsigned int source, void *mdata, unsigned int msize)
 {
 
-    file_open(FILE_PW);
+    char *key = mdata;
+    char *value = key + ascii_lengthz(key);
 
-    do
+    if (ascii_match(key, "key"))
+        keylength = buffer_write(key, 1024, value, ascii_length(value), 0);
+
+}
+
+static void onpath(struct channel *channel, unsigned int source, void *mdata, unsigned int msize)
+{
+
+    if (file_walk2(FILE_L0, mdata))
     {
 
-        struct record record;
+        file_open(FILE_L0);
 
-        if (file_readall(FILE_PW, &record, sizeof (struct record)))
-            print(channel, source, &record, mdata, msize);
+        do
+        {
 
-    } while (file_step(FILE_PW));
+            struct record record;
 
-    file_close(FILE_PW);
+            if (file_readall(FILE_L0, &record, sizeof (struct record)))
+                print(channel, source, &record);
+
+        } while (file_step(FILE_L0));
+
+        file_close(FILE_L0);
+
+    }
 
 }
 
 void init(struct channel *channel)
 {
 
-    channel_setcallback(channel, EVENT_DATA, ondata);
+    channel_setcallback(channel, EVENT_OPTION, onoption);
+    channel_setcallback(channel, EVENT_PATH, onpath);
 
 }
 
