417a9d9ddb155c73ba3952efead35c335ad55283
Jens Nyberg <jens.nyberg@gmail.com>
2021-05-02 17:31:56 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/irc.c
+++ src/utils/irc.c
@@ -134,7 +134,7 @@ void init(struct channel *channel)
 
     unsigned char address1[IPV4_ADDRSIZE] = {10, 0, 5, 1};
     unsigned char port1[TCP_PORTSIZE] = {0x07, 0xD0};
-    unsigned char address2[IPV4_ADDRSIZE] = {130, 239, 18, 119};
+    unsigned char address2[IPV4_ADDRSIZE] = {185, 30, 166, 37};
     unsigned char port2[TCP_PORTSIZE] = {0x1A, 0x0B};
     unsigned char address3[IPV4_ADDRSIZE] = {10, 0, 5, 80};
 
