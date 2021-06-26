1db23c531ccc019872160d403d942c4d45708f8e
Jens Nyberg <jens.nyberg@gmail.com>
2021-06-26 22:37:58 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/touch.c
+++ src/utils/touch.c
@@ -4,7 +4,28 @@
 static void onpath(struct channel *channel, unsigned int source, void *mdata, unsigned int msize)
 {
 
-    file_create(FILE_L0, FILE_PW, mdata);
+    unsigned int lb = buffer_findlastbyte(mdata, msize, '/');
+
+    if (lb)
+    {
+
+        char *buffer = mdata;
+
+        buffer[lb] = '\0';
+
+        if (!file_walk2(FILE_L0, buffer))
+            return;
+
+        file_create(FILE_L1, FILE_L0, buffer + lb + 1);
+
+    }
+
+    else
+    {
+
+        file_create(FILE_L1, FILE_PW, mdata);
+
+    }
 
 }
 
