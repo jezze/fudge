c5fffade897873497bd324e686e6e8b2da274677
Jens Nyberg <jens.nyberg@gmail.com>
2021-08-18 20:40:35 +0200

    add new wm
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/wm/rules.mk
+++ src/wm/rules.mk
@@ -38,6 +38,18 @@ L:=\
 include $(DIR_MK)/bin.mk
 
 B:=\
+    $(DIR_SRC)/wm/wm2 \
+
+O:=\
+    $(DIR_SRC)/wm/wm2.o \
+
+L:=\
+    $(DIR_LIB)/abi/abi.a \
+    $(DIR_LIB)/fudge/fudge.a \
+
+include $(DIR_MK)/bin.mk
+
+B:=\
     $(DIR_SRC)/wm/wshell \
 
 O:=\
