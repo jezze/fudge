daaf3ce6eabc5f57d6c4035ed6794a62daf1a376
Jens Nyberg <jens.nyberg@gmail.com>
2021-10-04 22:53:36 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/test/t_disk2.c
+++ src/test/t_disk2.c
@@ -1,30 +1,88 @@
 #include <fudge.h>
 #include <abi.h>
 
-static void onmain(unsigned int source, void *mdata, unsigned int msize)
+static unsigned int version(void)
 {
 
     struct message message;
+    struct event_p9p *p9p = (struct event_p9p *)message.data.buffer;
 
     p9p_mktversion(&message, 1200, "9P2000");
     file_notify(FILE_G0, EVENT_P9P, message_datasize(&message.header), message.data.buffer);
     channel_pollevent(EVENT_P9P, &message);
 
-    /* check that version is ok */
+    if (p9p_read1(p9p->type) == P9P_RERROR)
+        return 0;
+
+    if (p9p_read1(p9p->type) == P9P_RVERSION)
+        return 1;
+
+    return 0;
+
+}
+
+static unsigned int walk(void)
+{
+
+    struct message message;
+    struct event_p9p *p9p = (struct event_p9p *)message.data.buffer;
 
     p9p_mktwalk(&message, 22445566, 0, "build/data/help.txt");
     file_notify(FILE_G0, EVENT_P9P, message_datasize(&message.header), message.data.buffer);
     channel_pollevent(EVENT_P9P, &message);
-    
-    /* check that walk is ok */
+
+    if (p9p_read1(p9p->type) == P9P_RERROR)
+        return 0;
+
+    if (p9p_read1(p9p->type) == P9P_RWALK)
+        return 1;
+
+    return 0;
+
+}
+
+static unsigned int read(void)
+{
+
+    struct message message;
+    struct event_p9p *p9p = (struct event_p9p *)message.data.buffer;
 
     p9p_mktread(&message, 22445566, 0, 0, 512);
     file_notify(FILE_G0, EVENT_P9P, message_datasize(&message.header), message.data.buffer);
     channel_pollevent(EVENT_P9P, &message);
 
-    /* check that read is ok */
+    if (p9p_read1(p9p->type) == P9P_RERROR)
+        return 0;
+
+    /*
+    if (p9p_read1(p9p->type) == P9P_RREAD)
+        return 0;
+    */
 
     channel_sendbuffer(EVENT_DATA, message_datasize(&message.header) - sizeof (struct event_p9p) - sizeof (struct p9p_rread), message.data.buffer + sizeof (struct event_p9p) + sizeof (struct p9p_rread));
+
+    return 0;
+
+}
+
+static void onmain(unsigned int source, void *mdata, unsigned int msize)
+{
+
+    if (version())
+    {
+
+        if (walk())
+        {
+
+            if (read())
+            {
+
+            }
+
+        }
+
+    }
+
     channel_close();
 
 }
