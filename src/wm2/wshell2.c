b85693532d450a1ea9f068d813fdd50178248659
Jens Nyberg <jens.nyberg@gmail.com>
2022-08-01 18:01:44 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/wm2/wshell2.c
+++ src/wm2/wshell2.c
@@ -23,7 +23,11 @@ static void onwminit(unsigned int source, void *mdata, unsigned int msize)
         "+ window id \"window\" title \"Shell\"\n"
         "+ container id \"base\" in \"window\" layout \"maximize\" padding \"16\"\n"
         "+ textbox id \"output\" in \"base\"\n"
-        "+ text id \"prompt\" in \"output\" wrap \"char\" content \"$   \"\n";
+        "+ text id \"result\" in \"output\" wrap \"char\"\n"
+        "+ text id \"prompt\" in \"output\" wrap \"char\" content \"$ \"\n"
+        "+ text id \"input0\" in \"output\" wrap \"char\"\n"
+        "+ text id \"cursor\" in \"output\" wrap \"char\" content \" \"\n"
+        "+ text id \"input1\" in \"output\" wrap \"char\"\n";
 
     file_notify(FILE_G0, EVENT_WMRENDERDATA, cstring_length(data), data);
 
