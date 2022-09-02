881f3668f46c8bb0c01a5f63de4b21a304e59f6e
Jens Nyberg <jens.nyberg@gmail.com>
2022-09-02 17:58:12 +0200

    kill process if segfault
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/kernel/x86/arch.c
+++ src/kernel/x86/arch.c
@@ -308,7 +308,8 @@ unsigned short arch_pagefault(struct cpu_general general, unsigned int type, str
             mmu_map(directory, ctable, ARCH_TASKCODEPHYSICAL + core->task->id * (ARCH_TASKCODESIZE + ARCH_TASKSTACKSIZE), code, ARCH_TASKCODESIZE, MMU_TFLAG_PRESENT | MMU_TFLAG_WRITEABLE | MMU_TFLAG_USERMODE, MMU_PFLAG_PRESENT | MMU_PFLAG_WRITEABLE | MMU_PFLAG_USERMODE);
             mmu_map(directory, stable, ARCH_TASKCODEPHYSICAL + core->task->id * (ARCH_TASKCODESIZE + ARCH_TASKSTACKSIZE) + ARCH_TASKCODESIZE, ARCH_TASKSTACKVIRTUAL - ARCH_TASKSTACKSIZE, ARCH_TASKSTACKSIZE, MMU_TFLAG_PRESENT | MMU_TFLAG_WRITEABLE | MMU_TFLAG_USERMODE, MMU_PFLAG_PRESENT | MMU_PFLAG_WRITEABLE | MMU_PFLAG_USERMODE);
 
-            core->task->format->copyprogram(&core->task->node);
+            if (!core->task->format->copyprogram(&core->task->node))
+                task_signal(core->task, TASK_SIGNAL_KILL);
 
         }
 
@@ -316,19 +317,14 @@ unsigned short arch_pagefault(struct cpu_general general, unsigned int type, str
         {
 
             struct mmu_directory *kdirectory = getkerneldirectory();
-            unsigned int i;
+            unsigned int index = address >> 22;
 
-            for (i = 4; i < MMU_TABLES; i++)
-            {
-
-                if (kdirectory->tables[i])
-                    directory->tables[i] = kdirectory->tables[i];
-
-            }
+            if (!directory->tables[index] && kdirectory->tables[index])
+                directory->tables[index] = kdirectory->tables[index];
+            else
+                task_signal(core->task, TASK_SIGNAL_KILL);
 
         }
-
-        /* TODO: If address was not mapped, task segfaulted and should be killed. */
 
     }
 
