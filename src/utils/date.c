24303775eb2098d3fdfb84ba9198f53f34cfbf2f
Jens Nyberg <jens.nyberg@gmail.com>
2022-10-01 01:48:33 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/date.c
+++ src/utils/date.c
@@ -1,16 +1,21 @@
 #include <fudge.h>
 #include <abi.h>
 
+static struct option options[32];
+
 static void onmain(unsigned int source, void *mdata, unsigned int msize)
 {
 
-    if (file_walk(FILE_L0, FILE_G0, "ctrl"))
+    if (!file_walk2(FILE_L0, option_getstring(options, "clock")))
+        channel_error("Could not open clock");
+
+    if (file_walk(FILE_L1, FILE_L0, "ctrl"))
     {
 
         struct ctrl_clocksettings settings;
         struct message message;
 
-        file_readall(FILE_L0, &settings, sizeof (struct ctrl_clocksettings));
+        file_readall(FILE_L1, &settings, sizeof (struct ctrl_clocksettings));
         message_init(&message, EVENT_DATA);
         message_putvalue(&message, settings.year, 10, 4);
         message_putstring(&message, "-");
@@ -31,7 +36,7 @@ static void onmain(unsigned int source, void *mdata, unsigned int msize)
     else
     {
 
-        channel_error("Ctrl file not found");
+        channel_error("Clock ctrl file not found");
 
     }
 
@@ -45,15 +50,14 @@ static void onoption(unsigned int source, void *mdata, unsigned int msize)
     char *key = mdata;
     char *value = key + cstring_lengthz(key);
 
-    if (cstring_match(key, "clock"))
-        file_walk2(FILE_G0, value);
+    option_set(options, key, value);
 
 }
 
 void init(void)
 {
 
-    file_walk2(FILE_G0, "system:clock/if:0");
+    option_add(options, "clock", "system:clock/if:0");
     channel_bind(EVENT_MAIN, onmain);
     channel_bind(EVENT_OPTION, onoption);
 
--- src/utils/timestamp.c
+++ src/utils/timestamp.c
@@ -1,6 +1,7 @@
 #include <fudge.h>
 #include <abi.h>
 
+static struct option options[32];
 static unsigned int dotm365[13] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
 static unsigned int dotm366[13] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 365};
 
@@ -31,13 +32,16 @@ static unsigned int gettimestamp(unsigned int year, unsigned int month, unsigned
 static void onmain(unsigned int source, void *mdata, unsigned int msize)
 {
 
-    if (file_walk(FILE_L0, FILE_G0, "ctrl"))
+    if (!file_walk2(FILE_L0, option_getstring(options, "clock")))
+        channel_error("Could not open clock");
+
+    if (file_walk(FILE_L1, FILE_L0, "ctrl"))
     {
 
         struct ctrl_clocksettings settings;
         struct message message;
 
-        file_readall(FILE_L0, &settings, sizeof (struct ctrl_clocksettings));
+        file_readall(FILE_L1, &settings, sizeof (struct ctrl_clocksettings));
         message_init(&message, EVENT_DATA);
         message_putvalue(&message, gettimestamp(settings.year, settings.month, settings.day, settings.hours, settings.minutes, settings.seconds), 10, 0);
         message_putstring(&message, "\n");
@@ -48,7 +52,7 @@ static void onmain(unsigned int source, void *mdata, unsigned int msize)
     else
     {
 
-        channel_error("Ctrl file not found");
+        channel_error("Clock ctrl file not found");
 
     }
 
@@ -62,15 +66,14 @@ static void onoption(unsigned int source, void *mdata, unsigned int msize)
     char *key = mdata;
     char *value = key + cstring_lengthz(key);
 
-    if (cstring_match(key, "clock"))
-        file_walk2(FILE_G0, value);
+    option_set(options, key, value);
 
 }
 
 void init(void)
 {
 
-    file_walk2(FILE_G0, "system:clock/if:0");
+    option_add(options, "clock", "system:clock/if:0");
     channel_bind(EVENT_MAIN, onmain);
     channel_bind(EVENT_OPTION, onoption);
 
