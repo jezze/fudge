09936e387e0f84337baa25dbe01ad883efbeae05
Jens Nyberg <jens.nyberg@gmail.com>
2022-11-20 20:40:37 +0100

    add kill tool
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/rules.mk
+++ src/utils/rules.mk
@@ -172,6 +172,18 @@ L:=\
 include $(DIR_MK)/bin.mk
 
 B:=\
+    $(DIR_SRC)/utils/kill \
+
+O:=\
+    $(DIR_SRC)/utils/kill.o \
+
+L:=\
+    $(DIR_LIB)/abi/abi.a \
+    $(DIR_LIB)/fudge/fudge.a \
+
+include $(DIR_MK)/bin.mk
+
+B:=\
     $(DIR_SRC)/utils/log \
 
 O:=\
