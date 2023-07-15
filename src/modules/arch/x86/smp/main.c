2dc79c414c068099f19b112836765b3280473b51
Jens Nyberg <jens.nyberg@gmail.com>
2023-07-15 19:54:25 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/modules/arch/x86/smp/main.c
+++ src/modules/arch/x86/smp/main.c
@@ -17,14 +17,22 @@
 #define INIT16PHYSICAL                  0x00008000
 #define INIT32PHYSICAL                  0x00008200
 
-static struct corerow {struct arch_tss tss; struct core core; struct list_item item;} corerows[256];
+static struct corerow {unsigned int detected; struct arch_tss tss; struct core core; struct list_item item;} corerows[256];
 static struct list corelist;
 
-static void detect(struct acpi_madt *madt)
+static void detect(void)
 {
 
+    struct acpi_madt *madt = (struct acpi_madt *)acpi_findheader("APIC");
     unsigned int madttable = (unsigned int)madt + sizeof (struct acpi_madt);
     unsigned int madtend = (unsigned int)madt + madt->base.length;
+
+    if (madt->flags & 0x01)
+    {
+
+        /* pic_disable(); */
+
+    }
 
     while (madttable < madtend)
     {
@@ -36,18 +44,50 @@ static void detect(struct acpi_madt *madt)
 
             struct acpi_madt_lapic *lapic = (struct acpi_madt_lapic *)entry;
 
-            if (lapic->id)
+            if ((lapic->flags & 0x01) || (lapic->flags & 0x02))
             {
 
-                apic_sendint(lapic->id, APIC_REG_ICR_TYPE_INIT | APIC_REG_ICR_MODE_PHYSICAL | APIC_REG_ICR_LEVEL_ASSERT | APIC_REG_ICR_TRIGGER_EDGE | APIC_REG_ICR_TARGET_NORMAL | 0x00);
-                pit_wait(10);
-                apic_sendint(lapic->id, APIC_REG_ICR_TYPE_SIPI | APIC_REG_ICR_MODE_PHYSICAL | APIC_REG_ICR_LEVEL_ASSERT | APIC_REG_ICR_TRIGGER_EDGE | APIC_REG_ICR_TARGET_NORMAL | (INIT16PHYSICAL >> 12));
+                corerows[lapic->id].detected = 1;
 
             }
 
         }
 
+        if (entry->type == 1)
+        {
+
+            /*
+            struct acpi_madt_ioapic *ioapic = (struct acpi_madt_ioapic *)entry;
+            */
+
+        }
+
         madttable += entry->length;
+
+    }
+
+}
+
+static void enable(void)
+{
+
+    unsigned int id = apic_getid();
+    unsigned int i;
+
+    for (i = 0; i < 256; i++)
+    {
+
+        if (corerows[i].detected)
+        {
+
+            if (i == id)
+                continue;
+
+            apic_sendint(i, APIC_REG_ICR_TYPE_INIT | APIC_REG_ICR_MODE_PHYSICAL | APIC_REG_ICR_LEVEL_ASSERT | APIC_REG_ICR_TRIGGER_EDGE | APIC_REG_ICR_TARGET_NORMAL | 0x00);
+            pit_wait(10);
+            apic_sendint(i, APIC_REG_ICR_TYPE_SIPI | APIC_REG_ICR_MODE_PHYSICAL | APIC_REG_ICR_LEVEL_ASSERT | APIC_REG_ICR_TRIGGER_EDGE | APIC_REG_ICR_TARGET_NORMAL | (INIT16PHYSICAL >> 12));
+
+        }
 
     }
 
@@ -131,20 +171,16 @@ void smp_setupap(unsigned int stack)
 void module_init(void)
 {
 
-    struct acpi_madt *madt = (struct acpi_madt *)acpi_findheader("APIC");
     struct core *core = kernel_getcore();
 
     list_init(&corelist);
     smp_setupbp(core->sp, &core->tasks);
     kernel_setcallback(coreget, coreassign);
-
-    if (!madt)
-        return;
-
     buffer_copy((void *)INIT16PHYSICAL, (void *)(unsigned int)smp_begin16, (unsigned int)smp_end16 - (unsigned int)smp_begin16);
     buffer_copy((void *)INIT32PHYSICAL, (void *)(unsigned int)smp_begin32, (unsigned int)smp_end32 - (unsigned int)smp_begin32);
     smp_prep(ARCH_KERNELSTACKPHYSICAL + 2 * ARCH_KERNELSTACKSIZE);
-    detect(madt);
+    detect();
+    enable();
 
 }
 
