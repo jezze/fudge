40d2819b6f863a59f760268a76edf07f59c4cc02
Jens Nyberg <jens.nyberg@gmail.com>
2023-07-28 05:25:19 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -1,7 +1,7 @@
 #define KERNEL_TASKS                    16
 #define KERNEL_LINKS                    128
 #define KERNEL_DESCRIPTORS              32
-#define KERNEL_PORTS                    0xFFFF
+#define KERNEL_PORTS                    0x4000
 
 struct core *kernel_getcore(void);
 void kernel_setcallback(struct core *(*get)(void), void (*assign)(struct list_item *item));
