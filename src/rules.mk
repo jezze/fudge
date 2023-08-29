755d1485b8f69f48ed2b1f5c2e0711c17b63500e
Jens Nyberg <jens.nyberg@gmail.com>
2023-08-29 02:32:15 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/rules.mk
+++ src/rules.mk
@@ -4,8 +4,8 @@ include $(DIR_SRC)/gameboy/rules.mk
 endif
 include $(DIR_SRC)/kernel/rules.mk
 include $(DIR_SRC)/modules/rules.mk
+include $(DIR_SRC)/pcx/rules.mk
 include $(DIR_SRC)/test/rules.mk
 include $(DIR_SRC)/utils/rules.mk
 include $(DIR_SRC)/wm/rules.mk
 include $(DIR_SRC)/wutils/rules.mk
-include $(DIR_SRC)/pcx/rules.mk
