d15dde139a40929ec7293116b0feaa5d6ad95d11
Jens Nyberg <jens.nyberg@gmail.com>
2024-04-07 16:39:34 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- lib/abi/fsp.c
+++ lib/abi/fsp.c
@@ -12,97 +12,21 @@ static unsigned int getsession()
 
 }
 
-static unsigned int sendlistrequest(unsigned int target, unsigned int session, unsigned int id, unsigned int cid)
-{
-
-    struct event_listrequest listrequest;
-
-    listrequest.session = session;
-    listrequest.id = id;
-    listrequest.cid = cid;
-
-    return channel_send_buffer(target, EVENT_LISTREQUEST, sizeof (struct event_listrequest), &listrequest);
-
-}
-
-static unsigned int sendlistresponse(unsigned int source, unsigned int session, unsigned int nrecords, struct record *records)
-{
-
-    struct {struct event_listresponse listresponse; struct record records[8];} message;
-
-    message.listresponse.session = session;
-    message.listresponse.nrecords = nrecords;
-
-    buffer_write(message.records, sizeof (struct record) * 8, records, sizeof (struct record) * nrecords, 0);
-
-    return channel_send_buffer(source, EVENT_LISTRESPONSE, sizeof (struct event_listresponse) + sizeof (struct record) * message.listresponse.nrecords, &message);
-
-}
-
-static unsigned int sendreadrequest(unsigned int target, unsigned int session, unsigned int id, unsigned int count, unsigned int offset)
-{
-
-    struct event_readrequest readrequest;
-
-    readrequest.session = session;
-    readrequest.id = id;
-    readrequest.offset = offset;
-    readrequest.count = count;
-
-    return channel_send_buffer(target, EVENT_READREQUEST, sizeof (struct event_readrequest), &readrequest);
-
-}
-
-static unsigned int sendreadresponse(unsigned int source, unsigned int session, unsigned int count, void *buffer)
-{
-
-    struct {struct event_readresponse readresponse; char data[64];} message;
-
-    message.readresponse.session = session;
-    message.readresponse.count = count;
-
-    buffer_write(message.data, 64, buffer, message.readresponse.count, 0);
-
-    return channel_send_buffer(source, EVENT_READRESPONSE, sizeof (struct event_readresponse) + message.readresponse.count, &message);
-
-}
-
-static unsigned int sendwalkrequest(unsigned int target, unsigned int session, unsigned int parent, char *path)
-{
-
-    struct {struct event_walkrequest walkrequest; char path[64];} message;
-
-    message.walkrequest.session = session;
-    message.walkrequest.parent = parent;
-    message.walkrequest.length = cstring_length(path);
-
-    buffer_write(message.path, 64, path, message.walkrequest.length, 0);
-
-    return channel_send_buffer(target, EVENT_WALKREQUEST, sizeof (struct event_walkrequest) + message.walkrequest.length, &message);
-
-}
-
-static unsigned int sendwalkresponse(unsigned int source, unsigned int session, unsigned int id)
-{
-
-    struct event_walkresponse walkresponse;
-
-    walkresponse.session = session;
-    walkresponse.id = id;
-
-    return channel_send_buffer(source, EVENT_WALKRESPONSE, sizeof (struct event_walkresponse), &walkresponse);
-
-}
-
 unsigned int fsp_list(unsigned int target, unsigned int id, unsigned int cid, struct record records[8])
 {
 
     unsigned int session = getsession();
-    struct message message;
-    struct {struct event_listresponse listresponse; struct record records[8];} payload;
+    struct {struct event_listrequest header;} request;
 
-    if (sendlistrequest(target, session, id, cid))
+    request.header.session = session;
+    request.header.id = id;
+    request.header.cid = cid;
+
+    if (channel_send_buffer(target, EVENT_LISTREQUEST, sizeof (struct event_listrequest), &request))
     {
+
+        struct message message;
+        struct {struct event_listresponse listresponse; struct record records[8];} payload;
 
         while (channel_poll_any(EVENT_LISTRESPONSE, &message, &payload))
         {
@@ -127,7 +51,14 @@ unsigned int fsp_list(unsigned int target, unsigned int id, unsigned int cid, st
 unsigned int fsp_listresponse(unsigned int source, unsigned int session, unsigned int nrecords, struct record *records)
 {
 
-    return sendlistresponse(source, session, nrecords, records);
+    struct {struct event_listresponse listresponse; struct record records[8];} message;
+
+    message.listresponse.session = session;
+    message.listresponse.nrecords = nrecords;
+
+    buffer_write(message.records, sizeof (struct record) * 8, records, sizeof (struct record) * nrecords, 0);
+
+    return channel_send_buffer(source, EVENT_LISTRESPONSE, sizeof (struct event_listresponse) + sizeof (struct record) * message.listresponse.nrecords, &message);
 
 }
 
@@ -135,14 +66,21 @@ unsigned int fsp_read(unsigned int target, unsigned int id, unsigned int count, 
 {
 
     unsigned int session = getsession();
-    struct message message;
-    struct {struct event_readresponse readresponse; char data[64];} payload;
+    struct {struct event_readrequest header;} request;
 
     if (!count)
         return 0;
 
-    if (sendreadrequest(target, session, id, count, offset))
+    request.header.session = session;
+    request.header.id = id;
+    request.header.offset = offset;
+    request.header.count = count;
+
+    if (channel_send_buffer(target, EVENT_READREQUEST, sizeof (struct event_readrequest), &request))
     {
+
+        struct message message;
+        struct {struct event_readresponse readresponse; char data[64];} payload;
 
         while (channel_poll_any(EVENT_READRESPONSE, &message, &payload))
         {
@@ -167,19 +105,34 @@ unsigned int fsp_read(unsigned int target, unsigned int id, unsigned int count, 
 unsigned int fsp_readresponse(unsigned int source, unsigned int session, unsigned int count, void *buffer)
 {
 
-    return sendreadresponse(source, session, count, buffer);
+    struct {struct event_readresponse readresponse; char data[64];} message;
+
+    message.readresponse.session = session;
+    message.readresponse.count = count;
+
+    buffer_write(message.data, 64, buffer, message.readresponse.count, 0);
+
+    return channel_send_buffer(source, EVENT_READRESPONSE, sizeof (struct event_readresponse) + message.readresponse.count, &message);
 
 }
 
-unsigned int fsp_walk(unsigned int target, unsigned int id, char *path)
+unsigned int fsp_walk(unsigned int target, unsigned int parent, char *path)
 {
 
     unsigned int session = getsession();
-    struct message message;
-    struct event_walkresponse walkresponse;
+    struct {struct event_walkrequest header; char path[64];} request;
 
-    if (sendwalkrequest(target, session, id, path))
+    request.header.session = session;
+    request.header.parent = parent;
+    request.header.length = cstring_length(path);
+
+    buffer_write(request.path, 64, path, request.header.length, 0);
+
+    if (channel_send_buffer(target, EVENT_WALKREQUEST, sizeof (struct event_walkrequest) + request.header.length, &request))
     {
+
+        struct message message;
+        struct event_walkresponse walkresponse;
 
         while (channel_poll_any(EVENT_WALKRESPONSE, &message, &walkresponse))
         {
@@ -198,7 +151,12 @@ unsigned int fsp_walk(unsigned int target, unsigned int id, char *path)
 unsigned int fsp_walkresponse(unsigned int source, unsigned int session, unsigned int id)
 {
 
-    return sendwalkresponse(source, session, id);
+    struct event_walkresponse walkresponse;
+
+    walkresponse.session = session;
+    walkresponse.id = id;
+
+    return channel_send_buffer(source, EVENT_WALKRESPONSE, sizeof (struct event_walkresponse), &walkresponse);
 
 }
 
--- lib/abi/fsp.h
+++ lib/abi/fsp.h
@@ -2,5 +2,5 @@ unsigned int fsp_list(unsigned int target, unsigned int id, unsigned int cid, st
 unsigned int fsp_listresponse(unsigned int source, unsigned int session, unsigned int nrecords, struct record *records);
 unsigned int fsp_read(unsigned int target, unsigned int id, unsigned int count, unsigned int offset, unsigned int ocount, void *obuffer);
 unsigned int fsp_readresponse(unsigned int source, unsigned int session, unsigned int count, void *buffer);
-unsigned int fsp_walk(unsigned int target, unsigned int id, char *path);
+unsigned int fsp_walk(unsigned int target, unsigned int parent, char *path);
 unsigned int fsp_walkresponse(unsigned int source, unsigned int session, unsigned int id);
