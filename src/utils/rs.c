619e44da90d9dc1c51af20f179133a07e849ba4e
Jens Nyberg <jens.nyberg@gmail.com>
2024-06-29 18:48:31 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/rs.c
+++ src/utils/rs.c
@@ -43,6 +43,38 @@ static void onmain(unsigned int source, void *mdata, unsigned int msize)
     if (root)
     {
 
+        unsigned int id = fsp_walk(service, root, option_getstring("subdir"));
+
+        if (id)
+        {
+
+            struct record records[8];
+            unsigned int nrecords;
+            unsigned int offset;
+
+            for (offset = 0; (nrecords = fsp_list(service, id, offset, records)); offset += nrecords)
+            {
+
+                unsigned int i;
+
+                for (i = 0; i < nrecords; i++)
+                {
+
+                    struct record *record = &records[i];
+
+                    channel_send_fmt3(CHANNEL_DEFAULT, EVENT_DATA, "(%u) %w\n", &record->id, record->name, &record->length);
+
+                }
+
+            }
+
+        }
+
+    }
+
+    if (root)
+    {
+
         unsigned int id = fsp_walk(service, root, option_getstring("file"));
 
         if (id)
@@ -65,6 +97,7 @@ void init(void)
 {
 
     option_add("service", "90");
+    option_add("subdir", "kernel");
     option_add("file", "data/help.txt");
     channel_bind(EVENT_MAIN, onmain);
     channel_bind(EVENT_ERROR, onerror);
