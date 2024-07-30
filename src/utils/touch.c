97a787f6dacc9b6ba024b23a4dabd4aba6052284
Jens Nyberg <jens.nyberg@gmail.com>
2024-07-30 21:20:55 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/touch.c
+++ src/utils/touch.c
@@ -4,40 +4,6 @@
 static void onpath(unsigned int source, void *mdata, unsigned int msize)
 {
 
-    unsigned int offset = buffer_lastbyte(mdata, msize, '/');
-
-    if (offset)
-    {
-
-        char *buffer = mdata;
-
-        buffer[offset - 1] = '\0';
-
-        if (call_walk_absolute(FILE_L0, buffer))
-        {
-
-            if (!call_create(FILE_L1, FILE_L0, buffer + offset, cstring_length(buffer + offset)))
-                channel_send_fmt1(source, EVENT_ERROR, "Create path failed: %s\n", mdata);
-
-        }
-
-        else
-        {
-
-            channel_send_fmt1(source, EVENT_ERROR, "Path not found: %s\n", mdata);
-
-        }
-
-    }
-
-    else
-    {
-
-        if (!call_create(FILE_L1, FILE_PW, mdata, cstring_length(mdata)))
-            channel_send_fmt1(source, EVENT_ERROR, "Create path failed: %s\n", mdata);
-
-    }
-
 }
 
 void init(void)
