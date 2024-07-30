28284b13c4f875d3e5f32c06f7d9410478973def
Jens Nyberg <jens.nyberg@gmail.com>
2024-07-30 17:52:51 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/reboot.c
+++ src/utils/reboot.c
@@ -4,16 +4,17 @@
 static void onmain(unsigned int source, void *mdata, unsigned int msize)
 {
 
-    if (call_walk_absolute(FILE_L0, "system:reset"))
+    if (call_walk_absolute(FILE_L0, option_getstring("reset")))
         call_notify(FILE_L0, EVENT_TERMREQUEST, 0, 0);
     else
-        channel_send_fmt1(source, EVENT_ERROR, "Path not found: %s\n", "system:reset");
+        channel_send_fmt1(source, EVENT_ERROR, "Path not found: %s\n", option_getstring("reset"));
 
 }
 
 void init(void)
 {
 
+    option_add("reset", "system:reset");
     channel_bind(EVENT_MAIN, onmain);
 
 }
