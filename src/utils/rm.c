52d73d04ef9b91c90d1bc1b89620342ae560bd2b
Jens Nyberg <jens.nyberg@gmail.com>
2024-08-31 18:53:29 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/rm.c
+++ src/utils/rm.c
@@ -4,11 +4,6 @@
 static void onpath(unsigned int source, void *mdata, unsigned int msize)
 {
 
-    if (call_walk_absolute(FILE_L0, mdata))
-        call_destroy(FILE_L0);
-    else
-        channel_send_fmt1(source, EVENT_ERROR, "Path not found: %s\n", mdata);
-
 }
 
 void init(void)
