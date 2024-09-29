e2b6459572cfa2b3a92125ee93944ecba11ae66e
Jens Nyberg <jens.nyberg@gmail.com>
2024-09-29 20:37:21 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/kernel/task.c
+++ src/kernel/task.c
@@ -163,6 +163,7 @@ void task_init(struct task *task, unsigned int id)
     task_initsignals(&task->signals);
     task_initthread(&task->thread);
     spinlock_init(&task->spinlock);
+    node_init(&task->node);
 
     task->id = id;
     task->state = TASK_STATE_DEAD;
