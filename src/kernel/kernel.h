ed04cc52f72505462c7341e46a12ea43ebe72ea7
Jens Nyberg <jens.nyberg@gmail.com>
2024-10-06 16:55:43 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -1,7 +1,6 @@
 #define KERNEL_TASKS                    64
 #define KERNEL_LINKS                    1024
 #define KERNEL_MAILBOXES                512
-#define KERNEL_CHANNELS                 4096
 
 struct core *kernel_getcore(void);
 void kernel_setcallback(struct core *(*get)(void), void (*assign)(struct list_item *item));
