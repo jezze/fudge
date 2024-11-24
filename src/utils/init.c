ffd29e422ad720ae398f0dd7f9e4e2329d43c9e1
Jens Nyberg <jens.nyberg@gmail.com>
2024-11-24 09:32:43 +0100

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/utils/init.c
+++ src/utils/init.c
@@ -1,50 +1,33 @@
 #include <fudge.h>
 #include <abi.h>
 
-static void loaddriver(char *path)
+static void loaddriver(unsigned int ichannel, char *path)
 {
 
-    unsigned int target = fs_spawn(0, "initrd:bin/elfload");
+    unsigned int target = fs_spawn(ichannel, "initrd:bin/elfload");
 
     if (target)
     {
 
-        channel_send(0, target, EVENT_MAIN);
-        channel_send_fmt1(0, target, EVENT_PATH, "%s\\0", path);
-        channel_send(0, target, EVENT_END);
-        channel_wait(0, target, EVENT_DONE);
+        channel_send(ichannel, target, EVENT_MAIN);
+        channel_send_fmt1(ichannel, target, EVENT_PATH, "%s\\0", path);
+        channel_send(ichannel, target, EVENT_END);
+        channel_wait(ichannel, target, EVENT_DONE);
 
     }
 
 }
 
-static unsigned int spawnenv(void)
+static unsigned int spawnenv(unsigned int ichannel)
 {
 
-    unsigned int target = fs_spawn(0, "initrd:bin/env");
+    unsigned int target = fs_spawn(ichannel, "initrd:bin/env");
 
     if (target)
     {
 
-        channel_send_fmt1(0, target, EVENT_OPTION, "env\\0%u\\0pwd\\0initrd:\\0", &target);
-        channel_send(0, target, EVENT_MAIN);
-
-    }
-
-    return target;
-
-}
-
-static unsigned int spawnshell(unsigned int env)
-{
-
-    unsigned int target = fs_spawn(0, "initrd:bin/shell");
-
-    if (target)
-    {
-
-        channel_send_fmt1(0, target, EVENT_OPTION, "env\\0%u\\0pwd\\0initrd:\\0", &env);
-        channel_send(0, target, EVENT_MAIN);
+        channel_send_fmt1(ichannel, target, EVENT_OPTION, "env\\0%u\\0pwd\\0initrd:\\0", &target);
+        channel_send(ichannel, target, EVENT_MAIN);
 
     }
 
@@ -52,16 +35,33 @@ static unsigned int spawnshell(unsigned int env)
 
 }
 
-static unsigned int spawnwm(unsigned int env)
+static unsigned int spawnshell(unsigned int ichannel, unsigned int env)
 {
 
-    unsigned int target = fs_spawn(0, "initrd:bin/wm");
+    unsigned int target = fs_spawn(ichannel, "initrd:bin/shell");
 
     if (target)
     {
 
-        channel_send_fmt1(0, target, EVENT_OPTION, "env\\0%u\\0pwd\\0initrd:\\0", &env);
-        channel_send(0, target, EVENT_MAIN);
+        channel_send_fmt1(ichannel, target, EVENT_OPTION, "env\\0%u\\0pwd\\0initrd:\\0", &env);
+        channel_send(ichannel, target, EVENT_MAIN);
+
+    }
+
+    return target;
+
+}
+
+static unsigned int spawnwm(unsigned int ichannel, unsigned int env)
+{
+
+    unsigned int target = fs_spawn(ichannel, "initrd:bin/wm");
+
+    if (target)
+    {
+
+        channel_send_fmt1(ichannel, target, EVENT_OPTION, "env\\0%u\\0pwd\\0initrd:\\0", &env);
+        channel_send(ichannel, target, EVENT_MAIN);
 
     }
 
@@ -74,50 +74,50 @@ static void onmain(unsigned int source, void *mdata, unsigned int msize)
 
     unsigned int env;
 
-    loaddriver("initrd:kernel/base.ko");
-    loaddriver("initrd:kernel/log.ko");
-    loaddriver("initrd:kernel/block.ko");
-    loaddriver("initrd:kernel/clock.ko");
-    loaddriver("initrd:kernel/console.ko");
-    loaddriver("initrd:kernel/keyboard.ko");
-    loaddriver("initrd:kernel/mouse.ko");
-    loaddriver("initrd:kernel/timer.ko");
-    loaddriver("initrd:kernel/audio.ko");
-    loaddriver("initrd:kernel/video.ko");
-    loaddriver("initrd:kernel/ethernet.ko");
-    loaddriver("initrd:kernel/info.ko");
-    loaddriver("initrd:kernel/io.ko");
-    loaddriver("initrd:kernel/cpuid.ko");
-    loaddriver("initrd:kernel/msr.ko");
-    loaddriver("initrd:kernel/pat.ko");
-    loaddriver("initrd:kernel/acpi.ko");
-    loaddriver("initrd:kernel/pic.ko");
-    loaddriver("initrd:kernel/apic.ko");
-    loaddriver("initrd:kernel/platform.ko");
-    loaddriver("initrd:kernel/pci.ko");
-    loaddriver("initrd:kernel/pit.ko");
-    loaddriver("initrd:kernel/rtc.ko");
-    loaddriver("initrd:kernel/ps2.ko");
-    loaddriver("initrd:kernel/ps2-keyboard.ko");
-    loaddriver("initrd:kernel/ps2-mouse.ko");
-    loaddriver("initrd:kernel/bga.ko");
-    loaddriver("initrd:kernel/uart.ko");
-    loaddriver("initrd:kernel/vga.ko");
-    loaddriver("initrd:kernel/rtl8139.ko");
-    loaddriver("initrd:kernel/ide.ko");
-    loaddriver("initrd:kernel/ahci.ko");
-    loaddriver("initrd:kernel/nvme.ko");
-    loaddriver("initrd:kernel/ata.ko");
-    loaddriver("initrd:kernel/virtio-network.ko");
+    loaddriver(1, "initrd:kernel/base.ko");
+    loaddriver(1, "initrd:kernel/log.ko");
+    loaddriver(1, "initrd:kernel/block.ko");
+    loaddriver(1, "initrd:kernel/clock.ko");
+    loaddriver(1, "initrd:kernel/console.ko");
+    loaddriver(1, "initrd:kernel/keyboard.ko");
+    loaddriver(1, "initrd:kernel/mouse.ko");
+    loaddriver(1, "initrd:kernel/timer.ko");
+    loaddriver(1, "initrd:kernel/audio.ko");
+    loaddriver(1, "initrd:kernel/video.ko");
+    loaddriver(1, "initrd:kernel/ethernet.ko");
+    loaddriver(1, "initrd:kernel/info.ko");
+    loaddriver(1, "initrd:kernel/io.ko");
+    loaddriver(1, "initrd:kernel/cpuid.ko");
+    loaddriver(1, "initrd:kernel/msr.ko");
+    loaddriver(1, "initrd:kernel/pat.ko");
+    loaddriver(1, "initrd:kernel/acpi.ko");
+    loaddriver(1, "initrd:kernel/pic.ko");
+    loaddriver(1, "initrd:kernel/apic.ko");
+    loaddriver(1, "initrd:kernel/platform.ko");
+    loaddriver(1, "initrd:kernel/pci.ko");
+    loaddriver(1, "initrd:kernel/pit.ko");
+    loaddriver(1, "initrd:kernel/rtc.ko");
+    loaddriver(1, "initrd:kernel/ps2.ko");
+    loaddriver(1, "initrd:kernel/ps2-keyboard.ko");
+    loaddriver(1, "initrd:kernel/ps2-mouse.ko");
+    loaddriver(1, "initrd:kernel/bga.ko");
+    loaddriver(1, "initrd:kernel/uart.ko");
+    loaddriver(1, "initrd:kernel/vga.ko");
+    loaddriver(1, "initrd:kernel/rtl8139.ko");
+    loaddriver(1, "initrd:kernel/ide.ko");
+    loaddriver(1, "initrd:kernel/ahci.ko");
+    loaddriver(1, "initrd:kernel/nvme.ko");
+    loaddriver(1, "initrd:kernel/ata.ko");
+    loaddriver(1, "initrd:kernel/virtio-network.ko");
 
     /*
     loaddriver("initrd:kernel/smp.ko");
     */
 
-    env = spawnenv();
+    env = spawnenv(1);
 
-    spawnshell(env);
-    spawnwm(env);
+    spawnshell(1, env);
+    spawnwm(1, env);
 
 }
 
