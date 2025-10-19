b531fd43303d0671b1113c3a9e98101817e60b82
Jens Nyberg <jens.nyberg@gmail.com>
2025-10-19 18:38:14 +0200

    more

--- src/kernel/x86/mmu.h
+++ src/kernel/x86/mmu.h
@@ -19,14 +19,14 @@
 #define MMU_PFLAG_DIRTY                 0x0040
 #define MMU_PFLAG_PAT                   0x0080
 #define MMU_PFLAG_GLOBAL                0x0100
-#define MMU_EFLAG_PRESENT               (1 << 0)
-#define MMU_EFLAG_RW                    (1 << 1)
-#define MMU_EFLAG_USER                  (1 << 2)
-#define MMU_EFLAG_RESERVED              (1 << 3)
-#define MMU_EFLAG_INSTRUCTION           (1 << 4)
-#define MMU_EFLAG_PROTECTIONKEY         (1 << 5)
-#define MMU_EFLAG_SHADOWSTACK           (1 << 6)
-#define MMU_EFLAG_SGX                   (1 << 15)
+#define MMU_EFLAG_PRESENT               0x0001
+#define MMU_EFLAG_RW                    0x0002
+#define MMU_EFLAG_USER                  0x0004
+#define MMU_EFLAG_RESERVED              0x0008
+#define MMU_EFLAG_INSTRUCTION           0x0010
+#define MMU_EFLAG_PROTECTIONKEY         0x0020
+#define MMU_EFLAG_SHADOWSTACK           0x0040
+#define MMU_EFLAG_SGX                   0x8000
 
 unsigned int mmu_getdirectory(void);
 unsigned int mmu_gettable(unsigned int directory, unsigned int vaddress);
