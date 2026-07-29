c8f5db537df067f6166064a2db23f8f1b88817c4
Jens Nyberg <jens.nyberg@gmail.com>
2026-07-29 16:43:30 +0200

    fix it again

--- src/kernel/abi.c
+++ src/kernel/abi.c
@@ -28,7 +28,7 @@ static unsigned int checkuserspace(void *address, unsigned int count)
         if (entry->flags & MMAP_FLAG_USERMODE)
         {
 
-            if (value >= entry->vaddress && (value + count) < (entry->vaddress + entry->size))
+            if (value >= entry->vaddress && count <= entry->size && (value - entry->vaddress) <= (entry->size - count))
                 return 1;
 
         }
