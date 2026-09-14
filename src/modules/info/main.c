8608acfa0bff7356f8788addb25da90b8035065c
Jens Nyberg <jens.nyberg@gmail.com>
2026-09-14 13:38:28 +0200

    more

--- src/modules/info/main.c
+++ src/modules/info/main.c
@@ -4,8 +4,94 @@
 static struct node_operands operands;
 static unsigned int inode;
 
-static unsigned int read(unsigned int id)
+static unsigned int readcores(unsigned int id, unsigned int offset, unsigned int count, unsigned int nrecords, struct record *records)
 {
+
+    struct resource *resource = 0;
+    unsigned int i;
+
+    for (i = 0; (resource = resource_foreachtype(resource, RESOURCE_CORE)); i++)
+    {
+
+    }
+
+    return 0;
+
+}
+
+static unsigned int readroot(unsigned int id, unsigned int offset, unsigned int count, unsigned int nrecords, struct record *records)
+{
+
+    unsigned int crecords = 0;
+
+    if (offset < 1)
+    {
+
+        struct record *record = &records[crecords];
+
+        record->id = 2;
+        record->offset = 1;
+        record->type = RECORD_TYPE_DIRECTORY;
+        record->size = 0;
+        record->length = 5;
+
+        buffer_write(record->name, RECORD_NAMESIZE, "cores", record->length, 0);
+
+        crecords++;
+
+    }
+
+    if (offset < 2)
+    {
+
+        struct record *record = &records[crecords];
+
+        record->id = 3;
+        record->offset = 2;
+        record->type = RECORD_TYPE_DIRECTORY;
+        record->size = 0;
+        record->length = 5;
+
+        buffer_write(record->name, RECORD_NAMESIZE, "tasks", record->length, 0);
+
+        crecords++;
+
+    }
+
+    if (offset < 3)
+    {
+
+        struct record *record = &records[crecords];
+
+        record->id = 4;
+        record->offset = 3;
+        record->type = RECORD_TYPE_DIRECTORY;
+        record->size = 0;
+        record->length = 5;
+
+        buffer_write(record->name, RECORD_NAMESIZE, "nodes", record->length, 0);
+
+        crecords++;
+
+    }
+
+    return crecords * sizeof (struct record);
+
+}
+
+static unsigned int read(unsigned int id, unsigned int offset, unsigned int count, unsigned int nrecords, struct record *records)
+{
+
+    switch (id)
+    {
+
+    case 1:
+        return readroot(id, offset, count, nrecords, records);
+
+    case 2:
+        return readcores(id, offset, count, nrecords, records);
+
+    }
 
     return 0;
 
@@ -32,7 +118,7 @@ static unsigned int onreadrequest(unsigned int source, unsigned int count, void 
     struct event_readrequest *request = data;
     struct event_readresponse *response = (struct event_readresponse *)buffer;
 
-    response->count = read(request->id);
+    response->count = read(request->id, request->offset, request->count, (MESSAGE_SIZE - sizeof (struct event_readresponse)) / sizeof (struct record), (struct record *)(response + 1));
 
     return kernel_place(inode, source, EVENT_READRESPONSE, sizeof (struct event_readresponse) + response->count, buffer);
 
